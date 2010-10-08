92d589409ddf18154ec1d469332d47de7c7e6035
Romain d'Alverny <rda@mageia.org>
2010-10-08 13:15:34 +0000

    git-svn-id: svn+ssh://svn.mageia.org/svn/web/www/trunk@4 ecb8f77e-9c09-4d67-9c5c-b96076beb801

---
+++ en/about/values/index.php
@@ -0,0 +1,73 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE html>
+<html dir="ltr" lang="en">
+<head>
+    <meta charset="utf-8" />
+    <title>Mageia Values</title>
+    <meta name="description" content="Mageia is new community-based Linux distribution." />
+    <meta name="keywords" content="mageia, values" />
+    <meta name="author" content="Mageia" />
+    <link rel="stylesheet" type="text/css" href="/g/style/all.css" />
+    <?php include '../../../analytics.php'; ?>
+</head>
+<body>
+    <?php include '../../../langs.php'; ?>
+    
+    <div id="doc" class="yui-t7">
+        <div id="hd" role="banner"><h1>Mageia <span>| Values</span></h1></div>
+        <div id="bd" role="main">
+            <div class="yui-g">
+                <div class="para faq">
+                    <h2>Mageia is Social Software</h2>
+
+                    <ul>
+                        <li>We will, as a Community, Value Our Users. We will always be in touch with our user base, for they are as much part of the community as are makers and organisers.</li>
+                        <li>We understand that our most valuable assets are people and the Community.</li>
+                        <li>We will always be a community distribution, valuing all the community's contributions.</li>
+                        <li>We will be an inclusive distribution, making sure that everyone in the community can make their voice heard.</li>
+                        <li>We esteem friendships highly and will promote social interaction between people.</li>
+                        <li>We value integrity, community, trust and will always adhere to the highest ethical standards.</li>
+                        <li>We will, above all things, enjoy ourselves :-) .</li>
+                        <li>We will strive to maintain the goodwill of both our own and the wider Open Source community.</li>
+                        <li>We will help and support each other. When something isn't right, we will take the appropriate steps to find a better solution and use it.</li>
+                    </ul>
+                    
+                    <h2>Mageia is Powerful Technology</h2>
+                    </ul>
+                        <li>We, as Free Software and as an Operating System, will constantly innovate to remain a premium computing technology choice.</li>
+                        <li>We will stay up-to-date with the latest trends in Open Source, while choosing wisely to balance between modernization and reliability in our releases.</li>
+                        <li>We will maintain the vibrancy within our Community, always aiming to lead the way in collaborative development.</li>
+                        <li>We will encourage our users to be the best they can be on their computers.</li>
+                        <li>We will strive for ease of use, but also offer a holistic set of innovations for novice and power users alike.</li>
+                    </ul>
+                    
+                    <h2>Mageia is Knowledge</h2>
+                    <ul>
+                        <li>Software is an integral part of our daily lives, in almost every interaction at home and around the world, and at its heart is computer Code. We will strive to understand it, and how it works in our daily lives.</li>
+                        <li>We will empower our user base by demystifying advanced technologies.</li>
+                        <li>We recognise that we are human. Failure and inadequacies do happen and context changes. We will take that into account in our software and lives, as well as the pursuit of happiness. We will evolve.</li>
+                        <li>We believe that there is nothing as beautiful as understanding the software you use in daily life. Therefore, we will openly educate people about Free Software, our Community, and our Operating System.</li>
+                    </ul>
+                    
+                    <h2>Mageia is Quality</h2>
+                    <ul>
+                        <li>Mageia will be based on good quality/stability requirements.</li>
+                        <li>Mageia will be compliant with open source standards.</li>
+                        <li>Mageia will always adhere to high security and privacy standards/technologies to protect our users' data.</li>
+                    </ul>
+                    
+                    <h2>Mageia is Open Relations</h2>
+                    <ul>
+                        <li>We will secure the future of the distribution through its community.</li>
+                        <li>We will cooperate with other OSS distributions and core and kernel developers with code contribution.</li>
+                        <li>We will work in collaboration with other open source projects.</li>
+                        <li>We will welcome any Commercial Enterprise as part of our community.</li>
+                        <li>Mageia will always be based upon Open Source Software. However, it will also be open to non-OSS contributions, to provide alternatives for our users' diverse needs and expectations.</li>
+                        <li>Mageia will always be a collective, international and organised endeavor, representative of our pluralistic community.</li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+    </div>
+</body>
+</html>
