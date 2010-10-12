e71ae9c564392102a0aa7c5696424d8e5c03cfa6
Romain d'Alverny <rda@mageia.org>
2010-10-12 15:25:39 +0000

    Estonian tree
    git-svn-id: svn+ssh://svn.mageia.org/svn/web/www/trunk@13 ecb8f77e-9c09-4d67-9c5c-b96076beb801

---
+++ et/about/values/index.php
@@ -0,0 +1,72 @@
+<!DOCTYPE html>
+<html dir="ltr" lang="et">
+<head>
+    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+    <meta charset="utf-8">
+    <title>Mageia väärtused</title>
+    <meta name="description" content="Mageia on uus kogukonna loodav Linuxi distributsioon.">
+    <meta name="keywords" content="mageia, väärtused">
+    <meta name="author" content="Mageia">
+    <link rel="stylesheet" type="text/css" href="/g/style/all.css">
+    <?php include '../../../analytics.php'; ?>
+</head>
+<body>
+    <?php include '../../../langs.php'; ?>
+    <div id="doc" class="yui-t7">
+        <div id="hd" role="banner"><h1>Mageia <span>| Väärtused</span></h1></div>
+        <div id="bd" role="main">
+            <div class="yui-g">
+                <div class="para values">
+                    <h2>Mageia on sotsiaalne tarkvara</h2>
+
+                    <ul>
+                        <li>Me väärtustame kogukonnana oma kasutajaid. Me oleme alati ühenduses oma kasutajatega, sest nemad on kogukonna osa sama palju kui loojad ja korraldajad.</li>
+                        <li>Me mõistame, et meie kõige suurem vara on inimesed ja kogukond.</li>
+                        <li>Me jääme alati kogukonna loodavaks distributsiooniks, hinnates kõrgelt igasugust kogukonna panust.</li>
+                        <li>Me oleme kõiki vastu võttev distributsioon ja anname endast kõik, et iga kogukonna liikme hääl oleks kuulda.</li>
+                        <li>Me hindame kõrgelt sõprust ja õhutame inimesi omavahel sotsiaalselt suhtlema.</li>
+                        <li>Me väärtustame ühtsustunnet, terviklikkust ja usaldust ning järgime alati kõrgeimaid eetilisi standardeid.</li>
+                        <li>Me soovime eelkõige iseendaga rahul olla :-) .</li>
+                        <li>Me püüame saavutada ja alal hoida head suhtumist nii iseendasse kui ka laiemasse vaba tarkvara kogukonda.</li>
+                        <li>Me aitame ja toetame üksteist. Kui midagi on valesti, anname endast kõik, et seda parandada.</li>
+                    </ul>
+                    
+                    <h2>Mageia on võimas tehnoloogia</h2>
+                    <ul>
+                        <li>Me oleme vaba tarkvarana ja operatsioonisüsteemina pidevalt uuendustelainel, et pakkuda kõike paremat arvutimaailmas.</li>
+                        <li>Me võtame kasutusele uusimad trendid vabas tarkvaras, tasakaalustades väljalasetes samas mõistlikult uuenduslikkust ja usaldusväärsust.</li>
+                        <li>Me hoiame oma kogukonna elavana ja elujõulisena, püüdes alati olla esirinnas koostööalti arenduse alal.</li>
+                        <li>Me julgustame oma kasutajaid olema parimad arvutikasutajad.</li>
+                        <li>Me püüdleme kasutamislihtsuse poole, kuid pakume ka terviklikke uudislahendusi nii algajatele kui ka kogenud kasutajatele.</li>
+                    </ul>
+                    
+                    <h2>Mageia on teadmine</h2>
+                    <ul>
+                        <li>Tarkvara on meie igapäevaelu lahutamatu osa, mis avaldub peaaegu kõigis tegevustes nii kodus kui ka maailmas, ning selle keskne osa on arvutikood. Me püüdleme selle mõistmise poole, et paremini aduda, kuidas see mõjutab meie igapäevaelu.</li>
+                        <li>Me muudame oma kasutajad enesekindlamaks, vähendades saladusloori tipptehnoloogia ümber.</li>
+                        <li>Me mööname, et me oleme inimesed. Meil esineb vigu, me ei mõista kõike, maailm meie ümber muutub pidevalt. Me võtame seda arvesse nii tarkvara juures kui ka oma elus, püüdlustes õnne ja täiuse poole. Me areneme.</li>
+                        <li>Me usume, et pole midagi ilusamat kui arusaamine tarkvarast, mida me iga päev kasutame. Seepärast harime me järjekindlalt inimesi vaba tarkvara, meie kogukonna ja meie operatsioonisüsteemi osas.</li>
+                    </ul>
+                    
+                    <h2>Mageia on kvaliteet</h2>
+                    <ul>
+                        <li>Mageia aluseks on korraliku kvaliteedi ja stabiilsuse nõue.</li>
+                        <li>Mageia ühildub avatud lähtekoodi standarditega.</li>
+                        <li>Mageia kasutab alati kõrgeid turbe- ja privaatsusstandardeid ning -tehnoloogiaid, et kaitsta oma kasutajate andmeid.</li>
+                    </ul>
+                    
+                    <h2>Mageia on avatud suhtlemine</h2>
+                    <ul>
+                        <li>Me kindlustame distributsiooni tuleviku kogukonna abiga.</li>
+                        <li>Me teeme koostööd teiste avatud lähtekoodiga distributsioonidega ning kerneli ja tuumikelementide arendajatega, pakkudes neile oma kooditäiendusi.</li>
+                        <li>Me teeme koostööd teiste vaba tarkvara projektidega.</li>
+                        <li>Me võtame oma kogukonda avasüli vastu kõiki äriettevõtteid.</li>
+                        <li>Mageia aluseks jääb alati avatud lähtekoodiga tarkvara. Ometi oleme alati avatud ka muulaadsele panusele, et pakkuda alternatiive, mis vastaksid meie kasutajate mitmekesistele nõudmistele ja vajadustele.</li>
+                        <li>Mageia on alati kollektiivne, rahvusvaheline ja organiseeritud ettevõtmine, esindades meie paljutahulist kogukonda.</li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+    </div>
+
+</body></html>
