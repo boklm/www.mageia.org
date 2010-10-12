b008eddba6f0335f451c8fa6b5c53f9fe8ae626a
Romain d'Alverny <rda@mageia.org>
2010-10-12 12:04:15 +0000

    git-svn-id: svn+ssh://svn.mageia.org/svn/web/www/trunk@12 ecb8f77e-9c09-4d67-9c5c-b96076beb801

---
+++ nl/about/values/index.php
@@ -0,0 +1,71 @@
+<!DOCTYPE html>
+<html dir="ltr" lang="en"><head><meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+    <meta charset="utf-8">
+    <title>Mageia kernwaarden</title>
+    <meta name="description" content="Mageia is new community-based Linux distribution.">
+    <meta name="keywords" content="mageia, values">
+    <meta name="author" content="Mageia">
+    <link rel="stylesheet" type="text/css" href="/g/style/all.css">
+    <?php include '../../../analytics.php'; ?>
+</head>
+<body>
+    <?php include '../../../langs.php'; ?>
+    <div id="doc" class="yui-t7">
+        <div id="hd" role="banner"><h1>Mageia <span>| Onze kernwaarden</span></h1></div>
+        <div id="bd" role="main">
+            <div class="yui-g">
+                <div class="para values">
+                    <h2>Mageia is sociale software</h2>
+
+                    <ul>
+                    		<li>Wij, als gemeenschap, vinden onze gebruikers zeer waardevol. Wij zullen ervoor zorgen dat we voortdurend contact met onze gebruikers onderhouden want zij zijn evenzeer een deel van de gemeenschap als de makers en organisatoren.</li>
+						<li>Wij begrijpen dat de mensen en de gemeenschap onze meest waardevollebezittingen vormen.</li>
+						<li>Wij zullen altijd een gemeenschapsdistributie zijn en hechten grote waarde aan de inbreng van de gemeenschap.</li>
+						<li>Wij zullen een distributie zijn waarvan iedereen deel kan uitmaken, en zullen ervoor zorgen dat iedereen in de gemeenschap zijn stem kan laten horen.</li>
+						<li>Wij beschouwen vriendschappen als zeer belangrijk en zullen de sociale interactie tussen mensen bevorderen.</li>
+						<li>Wij hechten grote waarde aan integriteit, gemeenschap en vertrouwen en zullen altijd werken volgens de hoogste ethische normen.</li>
+						<li>Wij zullen vooral ook veel genieten ;-) .</li>
+						<li>Wij zullen actief werken aan het behoud van onze goede naam, en die van de Open Source gemeenschap in het algemeen. </li>
+						<li>Wij helpen en ondersteunen elkaar. Als er iets mis is zullen we waar nodig stappen ondernemen om tot een goede oplossing te komen en ons daaraan houden.</li>
+                    </ul>
+                    
+                    <h2>Mageia is hoogwaardige technologie</h2>
+                    <ul>
+                    		<li>Wij, als Vrije Software en als besturingssysteem, zullen ons steeds innovatief ontwikkelen om eerste keus technologie bij het gebruik van computers te blijven.</li>
+						<li>Wij zullen steeds de nieuwste ontwikkelingen in Open Source volgen, en proberen de juiste keuzes te maken tussen vernieuwing en stabiliteit in onze  uitgaven.</li>
+						<li>Wij zullen er alles aan doen om onze gemeenschap levend te houden en altijd streven naar gezamenlijke ontwikkeling. </li>
+						<li>Wij moedigen onze gebruikers aan het beste uit zichzelf en hun computers te halen.</li>
+						<li>Wij streven naar eenvoud in gebruik, maar willen tegelijkertijd brede vernieuwingen aan zowel beginners als gevorderde gebruikers aanbieden.</li>
+                    </ul>
+                    
+                    <h2>Mageia is kennis</h2>
+                    <ul>
+                    		<li>Software  is onlosmakelijk deel van ons dagelijks leven en wordt bij veel van ons handelen, thuis en overal op aarde, gebruikt. Het is geschreven in computercode. Wij willen begrijpen hoe dit in elkaar steekt en hoe het ons leven beïnvloedt.</li>
+						<li>Wij willen de kennis van onze gebruikers vergroten door inzicht te geven in de werking van geavanceerde technologieën.</li>
+						<li>Wij weten dat we mensen zijn. De wereld is constant in verandering, fouten maken is menselijk en tekortkomingen horen bij het leven. Wij zullen hiermee rekening houden in onze software en onze levens, evenals in ons streven naar geluk. Wij zullen ons blijven ontwikkelen.</li>
+						<li>Wij vinden dat niets mooier is dan diepgaande kennis van de software die we in ons dagelijkse leven gebruiken. Daarom zullen we mensen vrij en toegankelijk voorlichten over Vrije Software, onze gemeenschap en ons besturingssysteem.</li>
+                    	</ul>
+                    
+                    <h2>Mageia is kwaliteit</h2>
+                    <ul>
+                    		<li>Mageia wordt gebaseerd op een uitgebreide set eisen wat betreft kwaliteit en stabiliteit.</li>
+						<li>Mageia zal voldoen aan de Open Source standaarden.</li>
+						<li>Mageia zal vanwege veiligheid en privacy altijd strikte standaarden en geavanceerde technologie gebruiken om de gegevens van onze gebruikers te beveiligen.</li>
+                    </ul>
+                    
+                    <h2>Mageia is open relaties</h2>
+                    <ul>
+                        <li>Wij zullen de toekomst van de distributie borgen in onze gemeenschap.</li>
+						<li>Wij zullen bijdragen aan andere OSS distributies, kern- en kernelontwikkelaars door onze software codes beschikbaar te stellen.</li>
+						<li>Wij zullen met andere Open Source projecten samenwerken.</li>
+						<li>Wij verwelkomen ook commerciële bedrijven binnen onze gemeenschap.</li>
+						<li>Mageia zal altijd op Open Source Software gebaseerd zijn. Maar het staat ook open voor niet-OSS bijdragen, om zo alternatieven aan te kunnen bieden voor de uiteenlopende eisen en verwachtingen van onze gebruikers.</li>
+						<li>Mageia zal altijd een gemeenschappelijke, internationale en georganiseerde organisatie zijn, vertegenwoordiger van onze pluralistische gemeenschap.</li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+    </div>
+
+
+</body></html>
