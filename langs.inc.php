b9a997ad3becc12e212b8e2593669c6b006fea70
Romain d'Alverny <rda@mageia.org>
2010-10-22 08:15:12 +0000

    redirect root access after browser lang prefs
    git-svn-id: svn+ssh://svn.mageia.org/svn/web/www/trunk@27 ecb8f77e-9c09-4d67-9c5c-b96076beb801

---
+++ langs.inc.php
@@ -0,0 +1,23 @@
+<?php
+
+// languages for home
+$langs = array(
+    'de' => 'Deutsch',
+    'el' => 'Ελληνικά',
+    'en' => 'English',
+    'es' => 'Español',
+    'et' => 'Eesti',
+    'fi' => 'Suomeksi',
+    'fr' => 'Français',
+    'it' => 'Italiano',
+    'lv' => 'Latviešu',
+    'nb' => 'Bokmål',
+    'nl' => 'Nederlands',
+    'pl' => 'Polski',
+    'pt-br' => 'Português do Brasil',
+    'ru' => 'Русский',
+    'tr' => 'Türkçe',
+    'sl' => 'Slovenščina',
+    'zh-cn' => '简体中文',
+    'zh-tw' => '正體中文'
+);
