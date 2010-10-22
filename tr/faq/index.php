a20546ebf790ece1e97417bbf7b74f016b603daf
Romain d'Alverny <rda@mageia.org>
2010-10-22 08:27:16 +0000

    tr branch update
    git-svn-id: svn+ssh://svn.mageia.org/svn/web/www/trunk@28 ecb8f77e-9c09-4d67-9c5c-b96076beb801

--- tr/faq/index.php
+++ tr/faq/index.php
@@ -1,66 +1,52 @@
-<?xml version="1.0" encoding="utf-8"?>
 <!DOCTYPE html>
-<html dir="ltr" lang="en">
+<html lang="tr">
 <head>
-    <meta charset="utf-8" />
-    <title>Mageia Frequently Asked Questions</title>
-    <meta name="description" content="Mageia is new community-based Linux distribution." />
-    <meta name="keywords" content="mageia, linux, mandriva, free software" />
+    <meta charset="utf-8">
+    <title>Mageia Sık Sorulan Sorular</title>
+    <meta content="Mageia topluluk temelli yeni bir Linux dağıtımıdır." name="description">
+    <meta content="mageia, linux, mandriva, free software" name="keywords">
     <link rel="stylesheet" type="text/css" href="/g/style/all.css" />
     <?php include '../../analytics.php'; ?>
 </head>
 <body>
     <?php include '../../langs.php'; ?>
-    
-    <div id="doc" class="yui-t7">
-        <div id="hd" role="banner"><h1>Mageia <span>| FAQ</span></h1></div>
-        <div id="bd" role="main">
+    <div class="yui-t7" id="doc">
+        <div role="banner" id="hd"><h1>Mageia | S.S.S.</h1></div>
+        <div role="main" id="bd">
             <div class="yui-g">
                 <div class="para faq">
-                    <h3>What is Mageia?</h3>
-                    <p>Mageia is your new Linux distribution.</p>
+                    <h3>Mageia nedir?</h3>
+                    <p>Mageia yeni Linux dağıtımınızdır.</p>
                     
-                    <h3>How does one pronounce Mageia?</h3>
-                    <p>There is no official pronunciation, you can use whichever one you like.</p>
+                    <h3>Mageia ismini nasıl telaffuz edeceğim?</h3>
+                    <p>Resmi bir telaffuz şekli yoktur. Nasıl kolayınıza geliyorsa o şekilde telaffuz edin..</p>
                     
-                    <h3>What does the name "Mageia" mean?</h3>
-                    <p>The term "mageia" — μαγεία in Greek — means "magic" in English.</p>
+                    <h3>"Mageia" isminin anlamı nedir?</h3>
+                    <p>"Mageia" — Yunanca μαγεία — kelimesi "sihir" veya "b&uuml;y&uuml;" anlamına gelmektedir.</p>
                     
-                    <h3>I'm a Mandriva Linux user. What should I do?</h3>
-                    <p>For now you can continue using Mandriva Linux, there is no Mageia release available yet.
-                        You can
-                        <a href="https://www.mageia.org/mailman/listinfo/mageia-announce" hreflang="en">subscribe
-                            to the mageia-announce newsletter</a> to be informed
-                        when the first Mageia release is ready to use.</p>
-                    
-                    <h3>Will I be able to upgrade from Mandriva to Mageia?</h3>
-                    <p>Yes, it will be possible.</p>
-                    
-                    <h3>When the first release of Mageia will be available?</h3>
-                    <p>As soon as possible. We hope to have some alpha or beta release by the end of November.</p>
+                    <h3>Ben bir Mandriva Linux kullanıcısıyım. Ne yapmam gerekiyor?</h3>
+                    <p>Şimdilik Mandriva Linux kullanmaya devam edebilirsiniz. Şu anda bir Mageia s&uuml;r&uuml;m&uuml; yayınlanmamıştır. İlk Mageia s&uuml;r&uuml;m&uuml;n&uuml;n ne zaman yayınlanacağını &ouml;ğrenmek i&ccedil;in <a hreflang="en" href="https://www.mageia.org/mailman/listinfo/mageia-announce">mageia-announce
+e-mektubuna &uuml;ye olun</a>.</p>
 
-                    <h3>What will be the codename of the first Mageia release?</h3>
-                    <p>That hasn't been decided yet.</p>
-                    
-                    <h3>Will urpmi, MCC, drakxtools be a part of Mageia?</h3>
-                    <p>Yes, your favourite tools will be included in Mageia.</p>
+                    <h3>Mandriva' dan Mageia' ya y&uuml;kseltme yapabilecek miyim?</h3>
+                    <p>Evet. Bu m&uuml;mk&uuml;n olacaktır.</p>
 
-                    <h3>Where can I submit my logo ideas?</h3>
-                    <p>You can submit your logo ideas on the <a href="http://www.flickr.com/groups/mageia-art/" hreflang="en">Mageia art group</a>.</p>
+                    <h3>Mageia' nın ilk s&uuml;r&uuml;m&uuml; ne zaman yayınlanacak?</h3>
+                    <p>M&uuml;mk&uuml;n olan en kısa s&uuml;rede. Kasım ayı sonunda bir alfa veya beta s&uuml;r&uuml;m&uuml; yayınlamayı d&uuml;ş&uuml;n&uuml;yoruz.</p>
 
-                    <h3>How can I contribute to the mageia project?</h3>
-                    <p>All sorts of skill sets are welcome.
-                        You can <a href="http://mageia.org/wiki/doku.php">register yourself on the wiki</a>,
-                        in whatever teams that best fit your skills.</p>
+                    <h3>İlk Mageia s&uuml;r&uuml;m&uuml;n&uuml;n kod adı ne olacak?</h3>
+                    <p>Bu konu hen&uuml;z kararlaştırılmadı.</p> <h3>Urpmi, MDM, drakx ara&ccedil;ları Mageia' da bulunacak mı?</h3> <p>Evet. En beğendiğiniz bu ara&ccedil;lar Mageia' ya dahil olacak.</p>
 
-                    <h3>How can I contact the project?</h3>
-                    <p>You can send email to mageia-contact (at) mageia (dot) org.</p>
+                    <h3>Logo &ouml;nerilerimi nerede paylaşabilirim?</h3>
+                    <p>Logo ile ilgili &ouml;nerilerinizi <a hreflang="en" href="http://www.flickr.com/groups/mageia-art/">Mageia sanat
+grubunda</a> paylaşabilirsiniz.</p>
 
-                    <h3>How can I donate to the project?</h3>
-                    <p>You can go to the <a href="http://donate.mageia.org/">donation page</a>.</p>
-                </div>
-            </div>
-        </div>
-    </div>
-</body>
-</html>
+                    <h3>Mageia projesine nasıl katkıda bulunabilirim?</h3>
+                    <p>Her t&uuml;rl&uuml; yeteneğe kapımız a&ccedil;ıktır. Yeteğenize uygun bir alan i&ccedil;in <a href="http://mageia.org/wiki/doku.php">wiki sayfasında ilgilendiğiniz alana isminizi</a> kaydedin.</p>
+
+                    <h3>Proje ile nasıl iletişim kurabilirim?</h3>
+                    <p>mageia-contact (kuyruklu a) mageia (nokta) org adresine e-mektup g&ouml;nderebilirsiniz.</p> <h3>Projeye nasıl bağışta
+bulunabilirim?</h3> <p><a href="http://donate.mageia.org/">Bağış sayfamıza</a>
+uğrayabilirsiniz.<br> </p></div></div></div></div>
+
+</body></html>
