cfaf3a51aacaae78837ca2cc4795fbeceba4e999
Romain d'Alverny <rda@mageia.org>
2010-10-25 14:18:51 +0000

    git-svn-id: svn+ssh://svn.mageia.org/svn/web/www/trunk@34 ecb8f77e-9c09-4d67-9c5c-b96076beb801

---
+++ zh-tw/about/values/index.php
@@ -0,0 +1,73 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE html>
+<html dir="ltr" lang="zh-tw">
+<head>
+    <meta charset="utf-8" />
+    <title>Mageia Values</title>
+    <meta name="description" content="Mageia is new community-based Linux distribution." />
+    <meta name="keywords" content="mageia, values" />
+    <meta name="author" content="Mageia" />
+    <link rel="stylesheet" type="text/css" href="/g/style/all.css" />
+    <?php include '../../analytics.php'; ?>
+</head>
+<body>
+    <?php include '../../langs.php'; ?>
+
+    <div id="doc" class="yui-t7">
+        <div id="hd" role="banner"><h1>Mageia <span>| 價值觀</span></h1></div>
+        <div id="bd" role="main">
+            <div class="yui-g">
+                <div class="para values">
+                    <h2>Mageia 重視社群</h2>
+
+                    <ul>
+                        <li>我們做為一個社群，絕對珍惜我們的使用者。我們會始終與使用者保持聯繫，因為他們是形成與組成這個社群的最重要份子。</li>
+                        <li>我們瞭解，我們最有價值的資產，就是這個社群本身，與參與這個社群的人們。</li>
+                        <li>我們永遠會是社群開發的套件，因此我們珍惜社群的貢獻。</li>
+                        <li>我們會是個包羅萬象的套件，並確保社群中每個人的聲音都會被聽到。</li>
+                        <li>我們高度尊重與珍惜友誼，並希望促進社群朋友間的互動。</li>
+                        <li>我們珍惜整合、社群與信任，並且永遠遵循最高道德標準。</li>
+                        <li>最重要的是，我們享受這一切:-)。</li>
+                        <li>我們會努力爭取包括我們自己社群內，以及整個開放源碼社群的善意與認同。</li>
+                        <li>我們會彼此協助與支援。當出現問題時，我們會找出適當的步驟與更好的解決方法。</li>
+                    </ul>
+                    
+                    <h2>Mageia 重視強有力的科技</h2>
+                    <ul>
+                        <li>我們做為自由軟體以及作業系統，會不斷地創新，以持續成為優質的電腦科技選擇。</li>
+                        <li>我們會隨時掌握開放源碼的最新趨勢，並在創新與可靠性之間明智地找出平衡點，做為我們發行版的依據。</li>
+                        <li>我們會維持社群的活力，並永遠致力於帶領合作開發。</li>
+                        <li>我們會讓我們的使用者在他們的電腦上盡情發揮所有的可能。</li>
+                        <li>我們會力求易用性，但也同時帶給初學及進階使用者全面的創新。</li>
+                    </ul>
+                    
+                    <h2>Mageia 重視知識</h2>
+                    <ul>
+                        <li>不管是家庭中的互動或是世界性的交流，軟體已經整合到我們的生活當中。而它的核心即是電腦程式碼。我們會全力去瞭解它，以及它在我們日常生活裡是如何運作的。</li>
+                        <li>我們會深入淺出地解釋進階技術來讓我們的使用群更強大。</li>
+                        <li>我們知道我們是凡人。失敗和不足之處在所難免，且時空背景會變。我們對我們的軟體和生活，還有追求幸福快樂時會將其考慮在內。我們會逐漸成長。</li>
+                        <li>我們相信沒有什麼能比瞭解您日常生活中所用的軟體還要美好。因此，我們會公開教導人們理解自由軟體、我們的社群，以及我們的作業系統。</li>
+                    </ul>
+                    
+                    <h2>Mageia 重視品質</h2>
+                    <ul>
+                        <li>Mageia 會力求高品質與高穩定度。</li>
+                        <li>Mageia 會符合開放源碼的標準。</li>
+                        <li>Mageia 會永遠遵循標準，運用技術以保證使用者資料的安全性與私密性。</li>
+                    </ul>
+                    
+                    <h2>Mageia 重視開放性</h2>
+                    <ul>
+                        <li>我們將確保套件的未來由社群所決定。</li>
+                        <li>我們會與其他開源套件、關鍵以及核心開發者合作，並做出程式碼的貢獻。</li>
+                        <li>我們將會與其他開源計畫一起合作。</li>
+                        <li>我們歡迎任何營利企業成為我們社群的一份子。</li>
+                        <li>Mageia將永遠建立在開源軟體上面。但是，它也接受其他非開源軟體的貢獻，從而依照使用者不同的需求和期待提供替代方案。</li>
+                        <li>Mageia將永遠是一個整合性，全球化而且有組織的努力者，我們多元化社群的象徵。</li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+    </div>
+</body>
+</html>
