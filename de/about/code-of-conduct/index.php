33ea28cd09f4de87b3aa57d7a93fd9bc6ef64068
Romain d'Alverny <rda@mageia.org>
2010-10-25 12:42:47 +0000

    git-svn-id: svn+ssh://svn.mageia.org/svn/web/www/trunk@33 ecb8f77e-9c09-4d67-9c5c-b96076beb801

---
+++ de/about/code-of-conduct/index.php
@@ -0,0 +1,102 @@
+<!DOCTYPE html>
+<html dir="ltr" lang="de">
+<head>
+    <meta charset="utf-8">
+    <title>Verhaltenskodex - Mageia</title>
+    <meta name="description" content="Mageia is new community-based Linux distribution.">
+    <meta name="keywords" content="mageia, values, conduct, rules">
+    <meta name="author" content="Mageia">
+    <link rel="stylesheet" type="text/css" href="/g/style/all.css" />
+    <?php include '../../../analytics.php'; ?>
+</head>
+<body>
+    <?php include '../../../langs.php'; ?>
+    <div id="doc" class="yui-t7">
+        <div id="hd" role="banner"><h1>Mageia <span>| Verhaltenskodex</span></h1></div>
+        <div id="bd" role="main">
+            <div class="yui-g">
+                <div class="para codeofconduct">
+                    <div id="preamble">
+
+                    <p>Das Mageia Projekt ist eine lebendige Gemeinschaft, offen für jedermann. Die Community (engl. für Gemeinschaft) soll eine freundliche Plattform sowohl für Neulinge als auch bestehende Mitglieder sein, in der sich alle verstanden und akzeptiert fühlen, unabhängig von Art oder Größe ihres Beitrags zum Projekt.</p>
+
+                    <p>In der Mageia Community arbeiten Menschen aus aller Welt gemeinsam an der Entwicklung eines Betriebssystems aus Freier Software. Möglich wird dies durch die Unterstützung, die harte Arbeit und den Enthusiasmus tausender Menschen, die Mageia entwickeln und benutzen oder auch an anderen freien Softwareprojekten arbeiten.</p>
+
+                    <p>Dieses Dokument enthält einige Richtlinien, die eine wirkungsvolle Zusammenarbeit der Beteiligten in einer positiven und inspirierenden Atmosphäre sicherstellen sollen. Zugleich erklärt es, wie wir uns gegenseitig unterstützen und einander stärken können.</p>
+
+                    <p>Dieser Verhaltenskodex (engl. <i>Code of Conduct</i>) gilt für alle Mitwirkenden und Nutzer, die sich im Mageia Projekt und seiner Community engagieren.</p>
+                    </div>
+                    
+                    <div id="overview">
+                    <h2 class="be">Überblick</h2>
+
+                    <p>Dieser Verhaltenskodex zeigt eine Zusammenfassung der gemeinsamen Wertvorstellungen und unseres auf der Basis der Vernunft beruhenden Denkens innerhalb unserer Community. Folgende soziale Grundgedanken halten unsere Community dabei zusammen:</p>
+                    <ul>
+                        <li><a href="http://mageia.org/de/about/code-of-conduct/#be_considerate">Sei rücksichtsvoll</a>,</li>
+                        <li><a href="http://mageia.org/de/about/code-of-conduct/#be_respectful">Sei respektvoll</a>,</li>
+                        <li><a href="http://mageia.org/de/about/code-of-conduct/#be_collaborative">Sei kooperativ</a>,</li>
+                        <li><a href="http://mageia.org/de/about/code-of-conduct/#be_pragmatic">Sei pragmatisch</a>,</li>
+                        <li><a href="http://mageia.org/de/about/code-of-conduct/#support">Unterstütze andere Mitgliedern der Community</a>,</li>
+                        <li><a href="http://mageia.org/de/about/code-of-conduct/#get_support">Nimm Hilfe von anderen Mitgliedern der Community an</a>.</li>
+                    </ul>
+                    
+                    <p>Dieser Verhaltenskodex spiegelt das von allen akzeptierte Verhalten eines Mitgliedes der Mageia Community innerhalb jeglicher Foren, Mailing-Listen, Wiki- und Web-Seiten, IRC-Channels, öffentlicher Treffen oder privater Korrespondenzen wider, welche sich inhaltlich mit dem Mageia Projekt und dessen Angeboten befassen. Die Community richtet sich nach diesen im Verhaltenskodex festgehaltenen Standards und tritt zum Wohle der Gemeinschaft für sie ein. Die Leiter jeglicher Arbeitsgruppen des Mageia Projektes haben die Befugnis, Personen, welche wiederholt gegen den Verhaltenskodex handeln, den Zugang zum Projekt zu verweigern. Falls nötig kann die entsprechende Angelegenheit auch dem Mageia Council vorgelegt und zur endgültigen Entscheidung übergeben werden.</p>
+                    </div>
+
+                    <h2 id="be_considerate" class="be">Sei rücksichtsvoll.</h2>
+
+                    <p>Deine Arbeiten beeinflussen andere Personen und werden von diesen benutzt, ebenso wie Du auf die Arbeit anderer angewiesen bist. Jede Entscheidung, die Du fällst, wird also weitere Community Mitglieder beeinflussen und wir erwarten, dass Du Dir dieser Verantwortung bewusst bist und danach handelst.</p>
+
+                    <p>Als Mitwirkender solltest Du der Arbeit anderer stets den gebührenden Respekt entgegen bringen und Dir gut überlegen, welchen Einfluss deine Änderungen auf die Arbeit anderer haben. Weiterhin wird erwartet, dass Du stets versuchst, den Entwicklungsplan und die Richtlinien des Projekts einzuhalten.</p>
+
+                    <p>Als Benutzer solltest Du Dir darüber im Klaren sein, dass die Mitwirkenden viel Arbeit und Zeit in ihre jeweilige Aufgabe stecken und darauf gebührend stolz sind. Falls Du einmal wegen eines bestimmten Problems frustriert sein solltest, so wird Dir in der Regel am wahrscheinlichsten geholfen, wenn Du allen Beteiligten möglichst akkurate und höflich formulierte Informationen lieferst.</p>
+
+                    <h2 id="be_respectful" class="be">Sei respektvoll.</h2>
+
+                    <p>Für eine gesunde Mageia Community ist es wichtig, dass sich ihre Mitglieder wohl und akzeptiert fühlen. Daher ist gegenseitiger Respekt unabdingbar. Bei Unstimmigkeiten solltest Du in erster Linie davon ausgehen, dass Dein Diskussionspartner gute Absichten hat.</p>
+
+                    <p>Wir tolerieren keine persönlichen Angriffe, Rassismus, Sexismus oder jegliche Form von Diskriminierung! Unstimmigkeiten lassen sich nicht vermeiden und treten von Zeit zu Zeit auf, doch wer respektiert werden möchte, muss auch die Sichtweisen der anderen respektieren. Andere Personen zu respektieren, ihre Arbeit zu schätzen und ihnen guten Willen zu unterstellen führt zu einer guten Atmosphäre, welche sich in größerer Motivation und Produktivität äußert.</p>
+
+                    <p>Wir erwarten von den Mitgliedern unserer Community stets einen respektvollen Umgang mit anderen Mitwirkenden, Benutzern und Communities. Denke immer daran, dass Mageia ein internationales Projekt ist und Du eventuell wichtige Aspekte einer anderen Kultur nicht kennst.</p>
+
+                    <h2 id="be_collaborative" class="be">Sei kooperativ.</h2>
+
+                    <p>Die Bewegung der Freien Software basiert auf Zusammenarbeit: doppelte Arbeit wird vermieden, während die Qualität der Software gesteigert wird. Um Missverständnisse zu vermeiden, solltest Du versuchen, Dich eindeutig, kurz und bündig auszudrücken, wenn Du um Hilfe bittest oder welche gibst. Beachte, dass sich E-Mails leicht missdeuten lassen (besonders, wenn sie nicht in Deiner Muttersprache geschrieben wurden). Falls etwas unklar sein sollte, bitte den Betroffenen um Aufklärung, schließlich ist die erste Regel, stets guten Willen hinter den Worten und Taten eines anderen zu vermuten.</p>
+
+                    <p>Als Mitwirkender solltest Du stets die Zusammenarbeit mit anderen Community Mitgliedern sowie weiteren Communitys anstreben (übergeordnete Projekte zum Beispiel), sofern sie an Deiner Arbeit interessiert oder davon abhängig sind. Deine Arbeit sollte dabei stets transparent sein und so schnell wie möglich zur Community zurückfließen (selbst wenn sie noch nicht ganz fertig gestellt wurde). Falls Du innerhalb eines Projektes an etwas Neuem arbeiten möchtest, so informiere die Projektgruppe über deine Fortschritte und Ideen.</p>
+
+                    <p>Es wird nicht immer möglich sein, einen gemeinsamen Konsens zur Implementierung einer Idee zu finden, daher solltest Du Dich nicht verpflichtet fühlen, einen zu erreichen, bevor Du mit der Arbeit beginnst. Dennoch solltest Du die „Außenwelt“ stets über Deine Arbeit informiert halten und ihnen die Möglichkeit geben, sie zu testen, zu diskutieren und weiter dazu beizutragen.</p>
+
+                    <p>Mitwirkende eines Projektes kommen und gehen. Falls Du Deine Mitarbeit beenden oder reduzieren möchtest, solltest Du stolz auf das Erreichte sein und Dich verantwortlich gegenüber denen verhalten, die nach Dir kommen und an dem Projekt weiterarbeiten.</p>
+
+                    <p>Als Benutzer ist vor allem Deine Meinung aber auch deren Form gefragt. Unüberlegte Kommentare können dabei demotivierend oder gar verletzend auf andere Community Mitglieder wirken. Konstruktiv geführte Diskussionen dagegen fördern neue Ideen und positive Ergebnisse. Ein ermutigendes Wort kann Wunder wirken.</p>
+
+
+                    <h2 id="be_pragmatic" class="be">Sei pragmatisch.</h2>
+
+                    <p>Mageia ist eine pragmatische Community.</p>
+                    <p>Wir bewerten konkrete Ergebnisse höher als das letzte Wort in einer Diskussion zu haben. Wir treten für unsere Werte wie Freiheit und respektvolle Zusammenarbeit ein, lassen uns aber nicht durch Diskussionen über kleinere Probleme von Streben nach wichtigeren Resultaten ablenken. Wir sind stets für neue Vorschläge und Lösungen offen, ungeachtet welcher Herkunft sie sind. Im Zweifelsfall unterstütze lieber eine hilfreiche und praktikable Lösung als eine, die theoretisch zwar Vorteile besitzt aber nicht implementiert wird. Nutze die Werkzeuge und Verfahren, welche die Arbeit erledigen. Entscheidungen sollten von denjenigen getroffen werden, welche die entsprechende Arbeit ausführen. Falls scheinbar unlösbare Probleme auftreten sollten, so kontaktiere den entsprechenden Teamleiter. Falls nötig kann auch der Mageia Council konsultiert werden..</p>
+
+
+                    <h2 id="support" class="be">Unterstütze andere Mitglieder der Community.</h2>
+
+                    <p>Unsere Community basiert auf gegenseitigem Respekt, Zusammenarbeit und pragmatischem, verantwortungsvollem Handeln. Hin und wieder kann es vorkommen, dass für diese Werte eingetreten und anderen Community Mitgliedern geholfen werden muss.</p>
+
+                    <p>Falls Du mitbekommst, dass andere Personen angegriffen werden, solltest Du zuerst überlegen, wie Du ihnen persönlich helfen kannst. Falls Du der Meinung bist, dass die Situation Deine persönlichen Fähigkeiten übersteigt, so kannst Du das jeweilige „Opfer“ im Privaten (per privater Nachricht zum Beispiel) fragen, ob ein offizielles Eingreifen nötig ist / gewünscht wird. Genauso solltest Du jedem Hilfe und Unterstützung anbieten, der Anzeichen von Überarbeitung oder persönlichen Problemen zeigt.</p>
+
+                    <p>Beim Aufkommen von Problemen solltest Du die Betroffenen zuerst freundlich auf den gemeinsamen Verhaltenskodex hinweisen. Führungspersonen zeichnen sich durch ihr Handeln aus und können mit gutem Beispiel voran gehen, wenn sie Probleme im Sinne des Verhaltenskodexes lösen bevor die Situation eskaliert.</p>
+
+
+                    <h2 id="get_support" class="be">Nimm Hilfe von anderen Mitgliedern der Community an.</h2>
+
+                    <p>Politische und technische Unstimmigkeiten treten von Zeit zu Zeit auf. Unsere Community ist da keine Ausnahme. Wir streben auch nicht danach, solche Unstimmigkeiten oder andere Sichtweisen zu verhindern, sondern sie konstruktiv zu lösen. Du solltest Dich zur Lösung von Streitigkeiten ratsuchend an die Community wenden und, wenn möglich, das involvierte Team konsultieren.</p>
+
+                    <p>Überlege Dir gut, ob Du eine Unstimmigkeit in eine öffentliche Auseinandersetzung ausufern lassen willst. Falls nötig, kannst Du zur Vermeidung einer emotionalen Eskalation um eine Vermittlung oder Schlichtung bitten. Wenn Du Dich oder Deine Arbeit angegriffen fühlst, nimm Dir einen Moment Zeit zum Durchatmen, bevor Ddu antwortest. Bei sehr emotionalem Umgangston solltest Du besser eine Nacht darüber schlafen - manchmal ist eine „Abkühlphase“ alles, was benötigt wird. Wenn Du wirklich einen anderen Weg gehen möchtest, dann empfehlen wir Dir, Deine Ideen und Arbeiten zu veröffentlichen, so dass sie von anderen getestet und bewertet werden können.</p>
+
+                    <p class="license">Dieses Dokument unterliegt der <a href="http://creativecommons.org/licenses/by-sa/3.0/us/">Creative Commons Attribution - Share Alike 3.0 License</a> (<a href="http://wiki.mandriva.com/de/Code_of_Conduct" rel="nofollow">Ursprung des Dokuments</a>).</p>
+                </div>
+            </div>
+        </div>
+    </div>
+
+</body></html>
