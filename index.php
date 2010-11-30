d8077b4186a67ef4e29d1c6498a6589c9067f991
Romain d'Alverny <rda@mageia.org>
2010-11-30 09:17:19 +0000

    fix redirection for ll-ll locales
    git-svn-id: svn+ssh://svn.mageia.org/svn/web/www/trunk@68 ecb8f77e-9c09-4d67-9c5c-b96076beb801

--- index.php
+++ index.php
@@ -9,7 +9,11 @@ if (isset($_GET['flang']))
     $lang = array_key_exists($_GET['flang'], $langs) ? $_GET['flang'] : 'en';
     if (isset($_GET['furl']))
     {
-        $url = substr_replace($_GET['furl'], $lang, 1, 2);
+        $url = explode('/', $_GET['furl']);
+        array_shift($url);
+        array_shift($url);
+        $url = '/' . $lang . '/' . implode($url, '/');
+        //$url = substr_replace($_GET['furl'], $lang, 1, 2);
     }
     else
         $url = '/' . $lang . '/';
