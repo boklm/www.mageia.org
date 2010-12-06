a3e6177dfce286c0d493c3957be7099950004ff1
Romain d'Alverny <rda@mageia.org>
2010-12-06 09:00:37 +0000

    tr update
    git-svn-id: svn+ssh://svn.mageia.org/svn/web/www/trunk@77 ecb8f77e-9c09-4d67-9c5c-b96076beb801

--- tr/index.php
+++ tr/index.php
@@ -15,13 +15,13 @@
         <div id="hd" role="banner"><h1>Mageia <span>&ndash; Yeni bir Linux dağıtımı</span></h1></div>
         <div id="bd" role="main">
             <div class="yui-g bb1">
-                <div class="para"><p>Mageia is a fork of Mandriva Linux,
-                    supported by a not-for-profit organisation of
-                    recognized and elected contributors.</p>
+                <div class="para"><p>Mageia, tanınan ve seçilmiş katkıcıların kar amacı gütmeyen birliği
+                tarafından desteklenen bir Mandriva Linux çatallamasıdır.</p>
 
-                    <p>Further than just delivering a free, secure, stable and sustainable operating system,
-                        the goal is to set up a stable and trustable governance to direct collaborative projects
-                        (<a href="#announce">check original announcement</a>).</p>
+                    <p>Güvenli, sürdürülebilir, kararlı ve özgür bir işletim sistemi
+                    oluşturmanın ötesinde; hedefimiz, kararlı ve güvenilebilir bir
+                    yönetimle işbirliğine dayanan projeler oluşturmaktır
+                        (<a href="#announce">özgün duyuruya bakınız</a>).</p>
                 </div>
             </div>
             <div class="yui-g bb1">
@@ -33,7 +33,7 @@
                 </div>
                 <div class="yui-u">
                     <div class="para" lang="en">
-                        <h2>Happening right now</h2>
+                        <h2>Şu anda olanlar:</h2>
                         <?php include '../roadmap.php'; ?>
                     </div>
                 </div>
@@ -41,16 +41,16 @@
             <div class="yui-g bb1">
                 <div class="yui-u first">
                     <div class="para" id="contact">
-                        <h2>Join us!</h2>
+                        <h2>Bize katılın!</h2>
                         <ul>
-                            <li><a href="https://www.mageia.org/mailman/">Mailing-lists</a></li>
-                            <li><a href="http://mageia.org/wiki/doku.php?id=irc">IRC (Freenode network)</a></li>
-                            <li><a href="http://mageia.org/wiki/">Mageia Wiki</a></li>
-                            <li>Contact the founders team:
+                            <li><a href="https://www.mageia.org/mailman/" hreflang="en">E-posta listeleri</a></li>
+                            <li><a href="http://mageia.org/wiki/doku.php?id=irc">IRC (Freenode ağı)</a></li>
+                            <li><a href="http://mageia.org/wiki/">Mageia Wikisi</a></li>
+                            <li>Kurucular kurulu ile iletişim kurun:
                                 <ul>
                                     <li><a href="mailto:mageia-contact@mageia.org">mageia-contact@mageia.org</a></li>
                                     <li><a href="mailto:press@mageia.org">press@mageia.org</a></li>
-                                    <li>expect an answer within 2 days</li>
+                                    <li>2 gün içinde bir cevap bekleyiniz</li>
                                 </ul></li>
                             <li><a href="http://www.twitter.com/mageia_org">twitter.com/mageia_org</a></li>
                             <li><a href="http://identi.ca/mageia">identi.ca/mageia</a></li>
