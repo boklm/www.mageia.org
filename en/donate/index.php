f6be98d3067320dd23a4360e90ddcc4d0d9f3071
Damien Lallement <dams@mageia.org>
2010-12-14 13:19:35 +0000

    start the migration of the donate page on the svn
    git-svn-id: svn+ssh://svn.mageia.org/svn/web/www/trunk@88 ecb8f77e-9c09-4d67-9c5c-b96076beb801

---
+++ en/donate/index.php
@@ -0,0 +1,24 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE html>
+<html dir="ltr" lang="en">
+<head>
+    <meta charset="utf-8" />
+    <title>Mageia Donation Campaign</title>
+    <meta name="description" content="Mageia is new community-based Linux distribution." />
+    <meta name="keywords" content="mageia, donate, donation" />
+    <meta name="author" content="Mageia" />
+    <link rel="stylesheet" type="text/css" href="/g/style/all.css" />
+    <?php include '../../analytics.php'; ?>
+</head>
+<body>
+    <?php include '../../langs.php'; ?>
+    
+    <div id="doc" class="yui-t7">
+        <div id="hd" role="banner"><h1>Mageia <span>| Donation Campaign</span></h1></div>
+        <div id="bd" role="main">
+            <div class="yui-g">
+            </div>
+        </div>
+    </div>
+</body>
+</html>
