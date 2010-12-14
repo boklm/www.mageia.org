06a1d89ae26ee0a783bc6b3502be67830a686d65
Damien Lallement <dams@mageia.org>
2010-12-14 15:53:36 +0000

    start the migration of the donate page on the svn (en)
    git-svn-id: svn+ssh://svn.mageia.org/svn/web/www/trunk@89 ecb8f77e-9c09-4d67-9c5c-b96076beb801

--- en/donate/index.php
+++ en/donate/index.php
@@ -3,7 +3,7 @@
 <html dir="ltr" lang="en">
 <head>
     <meta charset="utf-8" />
-    <title>Mageia Donation Campaign</title>
+    <title>Mageia Donation Campaig</title>
     <meta name="description" content="Mageia is new community-based Linux distribution." />
     <meta name="keywords" content="mageia, donate, donation" />
     <meta name="author" content="Mageia" />
@@ -17,6 +17,51 @@
         <div id="hd" role="banner"><h1>Mageia <span>| Donation Campaign</span></h1></div>
         <div id="bd" role="main">
             <div class="yui-g">
+		<div class="para donate">
+			<h2>Why donate?</h2>
+Mageia.Org is a not-for-profit association to manage the Mageia distribution. As a not-for-profit association, it can receive donations from the community to help in a lot of differents fields:<ul><li>hardware and hosting for servers ;</li><li>domain names ;</li><li>registrations of Mageia tradmark ;</li><li>goodies for spreading Mageia ;</li><li>administrative expenses ;</li><li>etc...</li></ul>.
+
+			<h2>How to track donation?</h2>
+There will be a monthly report on the funds received and how they were used. We believe public accountability is crucial. As stated in the minutes of The First General Constitutive Assembly, the treasurer of Mageia.Org is Damien Lallement.<br /><br />
+			<h2>Donate via bank transfer</h2>
+You can donate through bank transfer by using the following Bank Identifier Code (IBAN-BIC):<br />
+<br />
+<div id="preamble">
+<u>In favor of:</u><br /><br />
+Assocation Mageia.Org<br />
+8B rue de la Terrasse<br />
+75017 Paris<br />
+France<br />
+<br />
+<u>Bank Account:</u><br /><br />
+CIC Paris Villiers<br />
+14 avenue de Villiers<br />
+75017 Paris<br />
+France<br />
+<br />
+IBAN: FR7630066108760002006450123<br />
+BIC: CMCIFRPP<br />
+RIB: 30066 10876 00020064501 23<br />
+<br />
+<u>Amount:</u><br /><br />
+
+Put here the amount of your donation in euro (€) or dollars ($).<br />
+/!\ The conversion rate bank (if currency other than euro) will be charged for Mageia /!\
+</div><br />
+			<h2>Donate via check</h2>
+You can then send your donation by check to (if the currency is not euro, thank you to prefer bank transfer or Paypal):<br />
+<br />
+<div id="preamble">
+Association Mageia.Org<br />
+8B rue de la Terrasse<br />
+75017 Paris<br />
+France
+</div>
+<br />
+			<h2>Donate via PayPal</h2>
+/!\ PayPal withdraws a commission from each donation (so we get less than you sent) /!\<br />
+			<h2>Donators</h2>
+                </div>
             </div>
         </div>
     </div>
