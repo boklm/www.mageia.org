72c2350580951cf446dfe4f3dd0e7341c70ffc53
Damien Lallement <dams@mageia.org>
2010-12-14 17:00:37 +0000

    add the PayPal button to test
    git-svn-id: svn+ssh://svn.mageia.org/svn/web/www/trunk@92 ecb8f77e-9c09-4d67-9c5c-b96076beb801

--- en/donate/index.php
+++ en/donate/index.php
@@ -83,8 +83,14 @@ RIB:  30066 10876 00020064501 23
                 <div class="yui-u">
                     <div class="para" style="padding-left: 0;">
                         <h3>via PayPal</h3>
-                        <p><strong>/!\ PayPal withdraws a commission from each donation
-                            (so we get less than you sent) /!\</strong></p>
+			<p><form action="https://www.paypal.com/cgi-bin/webscr" method="post">
+<input type="hidden" name="cmd" value="_s-xclick">
+<input type="hidden" name="hosted_button_id" value="62X2MFQRAYABL">
+<input type="image" src="https://www.paypal.com/en_US/i/btn/btn_donateCC_LG.gif" border="0" name="submit" alt="PayPal - The safer, easier way to pay online!">
+<img alt="" border="0" src="https://www.paypal.com/fr_FR/i/scr/pixel.gif" width="1" height="1">
+</form>
+
+/!\ PayPal withdraws a commission from each donation (so we get less than you send) /!\</p>
 
                         <br />
                         <br />
