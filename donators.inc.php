5c799e3e916f315fb6131f9adcb88a0e700e31be
Romain d'Alverny <rda@mageia.org>
2010-12-17 15:27:17 +0000

    thank you page
    git-svn-id: svn+ssh://svn.mageia.org/svn/web/www/trunk@110 ecb8f77e-9c09-4d67-9c5c-b96076beb801

---
+++ donators.inc.php
@@ -0,0 +1,214 @@
+<?php
+/**
+*/
+
+/**
+*/
+$donators = array(
+    "Pascal Vilarem (Maât)",
+    "Vicente Salvador Cubedo",
+    "Samuel Verschelde (Stormi)",
+    "Frédéric Hugot",
+    "Jonathan Bayle",
+    "Markus Schimpf (arno911)", 
+    "HTJ Simons", 
+    "François Jaouen", 
+    "Josep Guallar-Esteve", 
+    "Daniel Le Berre", 
+    "Richard Neill", 
+    "Peter Trussel", 
+    "Marc Pare", 
+    "Dave Postles", 
+    "Basilio Rosa", 
+    "George Mitchell", 
+    "Louis Ramel", 
+    "Tomislav Jankovic", 
+    "Alexandre Lucazeau", 
+    "Michael Golden", 
+    "Jean Peyratout", 
+    "Philippe Seraphin", 
+    "Bernard Siaud", 
+    "Luis Maria Garcia Ruiperez", 
+    "Céline Harrand", 
+    "Ángel Jiménez Álvaro", 
+    "Jean-Michel Varvou (vouf)", 
+    "Margot Lawrence", 
+    "Teofilo Casares De La Iglesia", 
+    "Steven Hess", 
+    "Erwien Yustitiawan", 
+    "Thibaut François", 
+    "Bernd Deinzer", 
+    "Roland Gillard", 
+    "Dušan Pavlík", 
+    "Enrico Gsell", 
+    "Ernst Pini", 
+    "Dick Gevers", 
+    "Gonzalo Igartua", 
+    "Alexander Andreyev", 
+    "Frank Eisele", 
+    "Björn Olsson", 
+    "Lanfranco Gandolfo", 
+    "Gerardo Bueno Jiménez", 
+    "Murat Demir", 
+    "Николай Путроенок (nick7rus)", 
+    "Alfred Lang", 
+    "Antonio Pimentel", 
+    "Olivier Mergnac", 
+    "Anonymous", 
+    "Eduardo Llana Ugalde", 
+    "Derek Jennings", 
+    "Brian Saunderson", 
+    "Abel Rodríguez Reádigos", 
+    "Barry Jackson", 
+    "Mike Burgener", 
+    "Jin-tong Hu", 
+    "Jani J Välimaa", 
+    "Jose Fernández Rosa", 
+    "Pierre Bonneau", 
+    "Richard Monnier", 
+    "Frederic Forjan", 
+    "Vincent Calame", 
+    "Renaud Michel", 
+    "Sahaghian", 
+    "Wolfgang Bornath (wobo)", 
+    "Werner Brinkmann", 
+    "Serge Moreau", 
+    "Jean Goujoun", 
+    "Anonymous", 
+    "Freddy Janssens", 
+    "Nico Bergmann", 
+    "Adrien Guichard", 
+    "Markus Vollmer", 
+    "Philippe Cherpentier", 
+    "Jean-François Bellanger", 
+    "Laurent Clerel", 
+    "Anonymous", 
+    "Glen Ogilvie", 
+    "Jérôme Benoit", 
+    "Anonymous", 
+    "Doug Lytle", 
+    "M. &amp; Mme Vilarem", 
+    "Anonymous", 
+    "Anonymous", 
+    "Hoyt Duff", 
+    "Henk Elbers", 
+    "Didier Leunen (sisopetron)", 
+    "Emmanuel Andry", 
+    "Sébastien Pinet", 
+    "Vicente Salvador Cubedo", 
+    "Sébastien Guerin", 
+    "Tomeu Ferrer Redondo", 
+    "Philippe Jusseaume", 
+    "Anonymous", 
+    "Dick Gevers", 
+    "Thomas Lottmann", 
+    "姜 奕轲", 
+    "Raphael Vinet", 
+    "Pierre-Yves Dirand", 
+    "Yannick Le Ny", 
+    "Jean-jacques Barret", 
+    "Heiko Geier", 
+    "Livre Livre", 
+    "Patrick Charpentier", 
+    "Brian Hewitt", 
+    "Christian Prior", 
+    "Livre Livre", 
+    "Arnaud Gibert", 
+    "David Pernot (deap)", 
+    "Sylvain Haye", 
+    "Dave Null", 
+    "Jacques Spring", 
+    "Anonymous", 
+    "Anonymous", 
+    "Felipe Valladolid Gonzalo", 
+    "Rémi Verschelde (Akien)", 
+    "Fabrice Gaillard", 
+    "Jerome Martin (jmartin)", 
+    "Jean-Luc Versini", 
+    "Joao P S Carvalho", 
+    "Pierre Gabet", 
+    "Åke Mellin", 
+    "Laurent Espitallier", 
+    "Laurent Breton", 
+    "Fabrice Jade", 
+    "Julio Geraldo", 
+    "Patrice Riant", 
+    "Corrado Bove", 
+    "Marek Walter", 
+    "Anonymous", 
+    "Stefan John", 
+    "Paul Dupouy", 
+    "Cedric Drolet", 
+    "Jacques Paret-gris", 
+    "Sreejiraj Eluvangal", 
+    "Rémy Clouard (shikamaru)", 
+    "Sandro Veronese", 
+    "Alain Gudefin", 
+    "Vasileios Karampelas", 
+    "Morgan Leijström (Tribun AB)", 
+    "Alain Lamarche", 
+    "Marita Launonen", 
+    "Distrowatch.com", 
+    "Denis Prost", 
+    "François Lamboley"
+);
+
+//$html .= '<img src="/g/images/mageia_0.svg" style="position: absolute; top: 180px; width: 300px; opacity: 0.2;" />';
+
+$orgs = array(
+    array('http://www.mandrivafr.org/', 'AUFML'),
+    array('http://dedibox.fr/', 'Dédibox'),
+    array('http://www.gandi.net/', 'Gandi.net'),
+    array('http://www.kazar.net/', 'Kazar.net'),
+    array('http://www.lost-oasis.net/', 'Lost-Oasis'),
+    array('http://www.mandrivalinux-online.org/', 'MLO'),
+    array('http://nfrance.fr/', 'NFrance'),
+    array('http://www.spreadshirt.com/', 'Spreadshirt'),
+    array('http://zarb.org/', 'zarb.org'),
+);
+
+
+/**
+ * @param array  $donators
+ *
+ * @return string
+*/
+function html_donators_list($donators)
+{
+    $count_donators = count($donators);
+    $donators = array_unique($donators);
+    sort($donators);
+
+    $html = sprintf('<p>%d people <a href="/en/donate/">donated money</a> to Mageia.Org:</p>', $count_donators);
+    $html .= '<ul class="ty-ppl-list">';
+    foreach ($donators as $p)
+        $html .= sprintf('<li>%s</li>', $p);
+    $html .= '</ul>';
+    
+    return $html;
+}
+
+/**
+*/
+function html_people_list($people)
+{
+    $html = '<ul class="ty-ppl-list">';
+    foreach ($people as $p)
+        $html .= sprintf('<li>%s</li>', $p);
+    $html .= '</ul>';
+
+    return $html;
+}
+
+/**
+*/
+function html_orgs_list($orgs)
+{
+    $html = '<ul class="ty-ppl-list">';
+    foreach ($orgs as $o)
+        $html .= sprintf('<li><a href="%s">%s</a> %s</li>', $o[0], $o[1], @$o[2]);
+    $html .= '</ul>';
+
+    return $html;
+}
+
---
+++ en/thank-you/index.php
@@ -0,0 +1,54 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE html>
+<html dir="ltr" lang="en">
+<head>
+    <meta charset="utf-8" />
+    <title>Thank you for making Mageia possible.</title>
+    <meta name="description" content="People and orgs without which Mageia would not exist." />
+    <meta name="keywords" content="mageia, donate, donation, thanks, partners, help" />
+    <meta name="author" content="Mageia" />
+    <link rel="stylesheet" type="text/css" href="/g/style/all.css" />
+    <?php include '../../analytics.php'; ?>
+</head>
+<body>
+    <?php include '../../langs.php'; ?>
+    
+    <div id="doc" class="yui-t7">
+        <div id="hd" role="banner"><h1>Mageia <span>| Thank <em>you</em>!</span></h1></div>
+        <div id="bd" role="main">
+            <div class="yui-g">
+                <div class="para donate">
+                    <?php include '../../donators.inc.php'; ?>
+
+                    <p>Since September 2010, Mageia would not have come to a reality without
+                        the enthusiasm, advice and involvement of hundreds of people.</p>
+
+                    <p>It would not have either, without help and donation from many people and organizations.
+                        This page is here to remind of their contribution to this project.</p>
+
+                    <?php echo html_donators_list($donators); ?>
+
+                    <p>Places and teams that welcomed us and offerred us some tea, coffee, seats and advice:</p>
+                    <ul class="ty-ppl-list">
+                        <li>the famous <a href="http://lacantine.org/">La Cantine</a> coworking space</li>
+                        <li>Starbucks Coffee (Paris)</li>
+                    </ul>
+                    <p>Thanks a lot to all the people we met, discussed with about this project
+                        and its potential implications: you know who you are!</p>
+                    
+                    <p>Those provided Mageia.Org with hosting, hosting advice, server hardware,
+                        network bandwidth or some facilities:</p>
+                    <?php echo html_orgs_list($orgs); ?>
+                </div>
+            </div>
+            <div class="yui-g">
+                <div class="yui-u first"><div class="para" style="padding-right: 0;">
+
+                </div></div>
+                <div class="yui-u"><div class="para">
+
+            </div></div>
+        </div>
+    </div>
+</body>
+</html>
