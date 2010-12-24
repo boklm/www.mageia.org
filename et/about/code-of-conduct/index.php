58c2f2daed46ea47eaa5bb706d8228d4e898503f
Romain d'Alverny <rda@mageia.org>
2010-12-24 11:39:53 +0000

    estonian code-of-conduct
    git-svn-id: svn+ssh://svn.mageia.org/svn/web/www/trunk@132 ecb8f77e-9c09-4d67-9c5c-b96076beb801

---
+++ et/about/code-of-conduct/index.php
@@ -0,0 +1,103 @@
+<!DOCTYPE html>
+<html dir="ltr" lang="et">
+<head>
+    <meta charset="utf-8">
+    <title>Käitumisjuhised - Mageia</title>
+    <meta name="description" content="Mageia on uus kogukonna loodav Linuxi distributsioon.">
+    <meta name="keywords" content="mageia, väärtused, käitumine, juhised">
+    <meta name="author" content="Mageia">
+    <link rel="stylesheet" type="text/css" href="/g/style/all.css" />
+    <?php include '../../../analytics.php'; ?>
+</head>
+<body>
+    <?php include '../../../langs.php'; ?>
+
+    <div id="doc" class="yui-t7">
+        <div id="hd" role="banner"><h1>Mageia <span>| Käitumisjuhised</span></h1></div>
+        <div id="bd" role="main">
+            <div class="yui-g">
+                <div class="para codeofconduct">
+                    <div id="preamble">
+
+                    <p>Mageia projekt kujutab endast kõigile avatud ärksat kogukonda. Me soovime, et see kogukond valmistaks heameelt nii uustulnukatele kui ka senistele liikmetele, kus kõik võiksid tunda ennast mugavalt, millist laadi ja kui suur ka poleks nende panus.</p>
+
+                    <p>Mageia kogukonna liikmed on üle kogu maailma koondunud looma vaba tarkvara operatsioonisüsteemi. Selle on muutnud võimalikuks tuhandete inimeste, sealhulgas nii Mageia loojate ja kasutajate kui ka teiste vaba tarkvara projektide liikmete, toetus, raske töö ja innukus.</p>
+
+                    <p>Käesolev dokument pakub mõningaid juhiseid, et Mageia osalised võiksid teha tõhusat koostööd positiivse meelsusega innustavas õhkkonnas, ning üritab näidata, kuidas me saame üksteist toetada.</p>
+
+                    <p>Neid käitumisjuhiseod toetavad kõik kaastöötajad ja kasutajad, kes on seotud Mageia projekti ning selle kogukonna teenustega.</p>
+                    </div>
+                    
+                    <div id="overview">
+                    <h2 class="be">Ülevaade</h2>
+
+                    <p>Käesolevad käitumisjuhised kujutavad endast kokkuvõtet meie kogukonna ühistest väärtustest ja mõttelaadist. Kõige tähtsamad sotsiaalsed tähendussõnad, mis hoiavad meie projekti koos, on järgmised:</p>
+                    <ul>
+                        <li><a href="#be_considerate">olge hooliv</a></li>
+                        <li><a href="#be_respectful">olge aupaklik</a></li>
+                        <li><a href="#be_collaborative">olge koostööaldis</a></li>
+                        <li><a href="#be_pragmatic">olge pragmaatiline</a></li>
+                        <li><a href="#support">toetage teisi kogukonna liikmeid</a></li>
+                        <li><a href="#get_support">toetuge teistele kogukonna liikmetele</a></li>
+                    </ul>
+                    
+                    <p>Käesolevad käitumisjuhised kajastavad Mageia kogukonnas kokku lepitud käitumisstandardeid kõigis foorumites, postiloendites, wikis, veebilehel, IRC kanalil, avalikel kohtumistel ning erasuhtluses Mageia projekti ja selle teenuste kontekstis. Kogukond tegutseb vastavalt käesolevates käitumisjuhistes kirja pandud standarditele ning kaitseb neid kogu kogukonna huvides. Kõigi Mageia meeskondade juhtidel on õigus peatada isiku liikmesus, kes järjekindlalt rikub meie ühiseid käitumisjuhiseid, ning vajaduse korral, kui lahkhelid peaksid liiga suureks kasvama, tuuakse probleem lõplikuks lahendamiseks Mageia nõukogu ette.</p>
+                    </div>
+
+                    <h2 id="be_considerate" class="be">Olge hooliv</h2>
+
+                    <p>Teie tegevus ja töö mõjutavad teisi inimesi, kes kasutavad teie loomingut, ning te ise omakorda sõltute teiste inimeste tööst ja tegevusest. Kõik teie otsused mõjutavad teisi kogukonna liikmeid, mistõttu me eeldame, et te võtate otsust langetades arvesse ka selle võimalikke tagajärgi.</p>
+
+                    <p>Kaastöötajana kontrollige alati, et tunnustaksite ausalt teiste inimeste tööd, ning arvestage sellega, et teie tehtud muudatused mõjutavad teisi. Samuti eeldame, et te üritate järgida projekti ajakava ja üldisi suuniseid.</p>
+
+                    <p>Kasutajana pidage meeles, et kaastöötajad näevad oma panus Mageiasse andes rohkelt vaeva ning tunnevad selle üle suurt uhkust. Kui mõni probleem paneb teid nördima, arvestage, et selle lahendamine on tõenäolisem ja kiirem, kui jagate kõigile, keda see puudutab, asjakohast ja täpset teavet võimalikult emotsioonivabalt.</p>
+
+                    <h2 id="be_respectful" class="be">Olge aupaklik</h2>
+
+                    <p>Et Mageia kogukond võiks püsida tugeva ja tervena, peavad kõik selle liikmed tundma ennast mugavalt ja kindlalt. Selle nimel on teiste kogukonna liikmete aupaklik kohtlemine absoluutselt hädavajalik. Erimeelsuste korral tuleks alati eeldada, et teised ei ole pahatahtlikud.</p>
+
+                    <p>Me ei salli isiklikke rünnakuid, rassismi, seksismi ega muud laadi diskrimineerimist. Paratamatult tuleb ette erimeelsusi, kuid arvestage alati, et teiste austuse väljateenimiseks tuleb ka ise teistega arvestada. Teiste inimeste, nende töö ja tegevuse austamine ning eeldamine, et nad ei käitu pahatahtlikult, lubab kõigil kogukonna liikmetel tunda ennast mugavalt ja turvaliselt, mis omakorda suurendab nende motivatsiooni ja produktiivsust.</p>
+
+                    <p>Me eeldame, et meie kogukonna liikmed ilmutavad aupaklikkust suhtlemisel teiste kaastöötajate, kasutajate ja kogukondadega. Pidage meeles, et Mageia on rahvusvaheline projekt ja te ei pruugi olla teadlik asjadest, mis mõnes teises kultuuris seisavad oluliselt tähtsamal kohal kui teie omas.</p>
+
+                    <h2 id="be_collaborative" class="be">Olge koostööaldis</h2>
+
+                    <p>Vaba tarkvara liikumine sõltub alati koostööst: see aitab vältida topelttööd ning parandab loodava tarkvara kvaliteeti. Arusaamatuste vältimiseks püüdke alati väljenduda võimalikult täpselt ja lühidalt, kui soovite abi saada või ise seda annate. Arvestage, et e-kirja sisu võib väga kergesti valesti mõista, eriti kui autor ei kirjuta oma emakeeles. Kui midagi tundub arusaamatuna, paluge alati selgitust ning pidage alati meeles põhireeglit: igal juhul eeldage, et teised inimesed käituvad heas usus, mitte pahatahtlikult.</p>
+
+                    <p>Kaastöötajana peaks teie eesmärk olema koostöö nii teiste kogukonna liikmete kui ka teiste kogukondadega (eriti näiteks mujal arendatavate projektide korral), kes tunnevad huvi teie töö vastu või sõltuvad sellest. Teie töö peaks olema läbipaistev ning jõudma kogukonna ette võimalikult kiiresti, isegi kui see ei ole saanud veel päris lõplikku viimistlust. Kui soovite töötada mõne olemasoleva projekti sootuks uue omaduse või võimaluse kallal, andke kindlasti ka vastava projekti meeskonnale oma mõtetest ja edenemisest teada.</p>
+
+                    <p>Alati ei ole mõne idee teostamise puhul võimalik saavutada konsensust, mistõttu ei tohiks seda seada vältimatuks nõudeks. Küll aga tuleks alati anda teistele teada oma tegemistest ning avaldada oma töö, et teised saaksid seda testida, selle üle arutada ning soovi korral ka oma panuse anda.</p>
+
+                    <p>Kaastöötajad tulevad ja lähevad kõigi projektide korral. Kui lahkute või loobute projektist osaliselt või täielikult, tundke uhkust selle üle, mida te olete korda saatnud, ning suhtuge aupaklikult neisse, kes teie järel asuvad projektiga tegelema.</p>
+
+                    <p>Kasutajana teadke, et oluline on nii teie tagasiside kui ka selle vorm. Läbimõtlemata kommentaarid võivad tekitada paksu verd ning vähendada teiste kogukonna liikmete motivatsiooni, samal ajal kui läbimõeldud diskuteerimine probleemide üle võib kiiresti viia vajalike tulemusteni. Julgustav sõna võib teha imesid.</p>
+
+
+                    <h2 id="be_pragmatic" class="be">Olge pragmaatiline</h2>
+
+                    <p>Mageia on pragmaatiline kogukond.</p>
+                    <p>Me hindame rohkem käega katsutavaid tulemusi kui vaidluses pealejäämist. Me kaitseme oma põhiväärtusi, eriti vabadust ja teistega arvestavat koostööd, kuid me ei kavatse lasta vaidlustel pisiasjade üle takistada meid saavutamast suuremaid eesmärke. Me oleme ettepanekutele avatud ning võtame lahendusi vastu nende päritolust hoolimata. Kahtluste korral eelistame me sellist lahendust, mis likvideerib probleemi, mitte sellist, mis võib olla teoreetiliselt täiuslikum, aga mille teostamine ei ole tõenäoline. Me kasutame tööriistu ja meetodeid, mis aitavad meil jõuda eesmärgile. Kui mõned probleemid tunduvad olevat lahendamatud, tuleb kindlasti konsulteerida meeskonna juhiga ja vajaduse korral ka Mageia nõukoguga.</p>
+
+
+                    <h2 id="support" class="be">Toetage teisi kogukonna liikmeid</h2>
+
+                    <p>Meie kogukonna muudab tugevaks vastastikune austus, koostöö ning vastutustundlik ja pragmaatiline käitumine. Mõnikord tuleb neid väärtusi kaitsta, mõnikord vajavad teised kogukonna liikmed abi.</p>
+
+                    <p>Kui näete pealt, kuidas kedagi rünnatakse, mõelge kõigepealt, kuidas teie ise saate rünnatavat abistada. Kui tunnete, et te isiklikult ei suuda abi pakkuda, pöörduge eraviisiliselt ohvri poole ja küsige, kas on vaja kuidagi ametlikult sekkuda. Samamoodi tuleks toetada neid, kes tunduvad olevat läbipõlemise äärel, olgu töö või eraelu probleemide tõttu.</p>
+
+                    <p>Probleemide kerkimisel püüdke kõigepealt osapooltele taktitundeliselt meelde tuletada meie ühiseid käitumisjuhiseid. Juhtide üle otsustatakse nende tegude järgi ning nad võivad anda head eeskuju, lahendades tüliküsimusi meie käitumisjuhiste vaimus juba enne seda, kui need muutuvad tõsisemaks ja raskesti lahendatavaks.</p>
+
+
+                    <h2 id="get_support" class="be">Toetuge teistele kogukonna liikmetele.</h2>
+
+                    <p>Nii poliitilisi kui ka tehnilisi erimeelsusi tuleb tahes-tahtmata ikka ja jälle ette. Erand ei ole ka meie kogukond. Meie siht ei ole erimeelsusi või erinevaid seisukohti vältida, vaid neid konstruktiivselt lahendada. Nõuannete järele ja erimeelsuste lahendamiseks tasub kindlasti pöörduda kogukonna poole ning võimaluse korral kaasata arutellu meeskonnad, keda antud küsimus kõige enam puudutab.</p>
+
+                    <p>Mõelge sügavalt järele, enne kui muudate oma vastumeelsuse millegi suhtes avalikuks debatiks. Vajaduse korral kutsuge keegi vahemeheks ja igal juhul püüdke eriarvamused lahendada võimalikult emotsioonivabalt. Kui tunnete, et teie töö või te ise olete rünnaku alla sattunud, hingake sügavalt sisse ja mõelge, mitte ärge kiirustage raevutsevat vastust kirjutama. Kui tunded kipuvad üle keema, võiks pidada näiteks ööpäeva debatis vahet - mõnikord piisab ka lihtsalt ajast, et inimesed maha rahuneksid. Kui te aga olete täiesti kindel, et peate valima muu tee kui teised, siis tasuks kindlasti oma mõtted ja töö avalikuks teha, et neid oleks võimalik hinnaga ja järele proovida.</p>
+
+                    <p class="license">Käesolev dokument on litsenseeritud vastavalt <a href="http://creativecommons.org/licenses/by-sa/3.0/us/">Creative Commons Attribution - Share Alike 3.0</a> tingimustele (<a href="/en/about/code-of-conduct/" rel="nofollow">algversioon</a>).</p>
+                </div>
+            </div>
+        </div>
+    </div>
+
+<div></body></html>
