0bd21e17a9a7e61863e3289c2aee8145a7123c1e
Romain d'Alverny <rda@mageia.org>
2011-01-18 14:59:53 +0000

    pl code of conduct (Jerzy)
    git-svn-id: svn+ssh://svn.mageia.org/svn/web/www/trunk@159 ecb8f77e-9c09-4d67-9c5c-b96076beb801

---
+++ pl/about/code-of-conduct/index.php
@@ -0,0 +1,104 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE html>
+<html dir="ltr" lang="pl">
+<head>
+    <meta charset="utf-8" />
+    <title>Mageia - Kodeks postępowania</title>
+    <meta name="description" content="Mageia is new community-based Linux distribution." />
+    <meta name="keywords" content="mageia, values, conduct, rules, kodeks, zasady" />
+    <meta name="author" content="Mageia" />
+    <link rel="stylesheet" type="text/css" href="/g/style/all.css" />
+    <?php include '../../../analytics.php'; ?>
+</head>
+<body>
+    <?php include '../../../langs.php'; ?>
+
+    <div id="doc" class="yui-t7">
+        <div id="hd" role="banner"><h1>Mageia <span>| Kodeks postępowania</span></h1></div>
+        <div id="bd" role="main">
+            <div class="yui-g">
+                <div class="para codeofconduct">
+                    <div id="preamble">
+
+                    <p>Projekt Mageia jest żywą społecznością, otwartą dla wszystkich. Chcemy, aby ta społeczność była doskonałym miejscem zarówno dla nowych jak i obecnych uczestników, gdzie każdy czuje się komfortowo i jest akceptowany bez względu na ilość i rodzaj włożonego wkładu.</p>
+
+                    <p>W społeczności Mageia uczestnicy z całego świata jednoczą się, aby stworzyć system operacyjny oparty na wolnym oprogramowaniu. Jest to możliwe dzięki wsparciu, ciężkiej pracy i entuzjazmowi tysięcy ludzi, w tym tych, którzy tworzą i używają Mageia, jak również uczestników innych projektów wolnego oprogramowania.</p>
+
+                    <p>Ten dokument zawiera kilka wskazówek mających sprawić, że uczestnicy Mageia mogą skutecznie współpracować w pozytywnej i twórczej atmosferze, i wyjaśnić, jak możemy się wzajemnie wspierać.</p>
+
+                    <p>Niniejszy kodeks postępowania jest wspólny dla wszystkich uczestników i użytkowników, którzy angażują się w projekt Mageia i jego usługi społecznościowe.</p>
+                    </div>
+                    
+                    <div id="overview">
+                    <h2 class="be">Przegląd</h2>
+
+                    <p>Niniejszy kodeks postępowania stanowi podsumowanie wspólnych wartości i racjonalnego myślenia w naszej społeczności. Podstawowe imperatywy scalające nasz projekt w jedną całość obejmują:</p>
+                    <ul>
+                        <li><a href="#be_considerate">Bądź taktowny</a>,</li>
+                        <li><a href="#be_respectful">Szanuj innych</a>,</li>
+                        <li><a href="#be_collaborative">Współpracuj z innymi</a>,</li>
+                        <li><a href="#be_pragmatic">Bądź pragmatyczny</a>,</li>
+                        <li><a href="#support">Udzielaj wsparcia</a>,</li>
+                        <li><a href="#get_support">Korzystaj ze wsparcia innych uczestników</a>.</li>
+                    </ul>
+                    
+                    <p>Niniejszy kodeks przedstawia uzgodnione standardy zachowania dla uczestników społeczności Mageia, na każdym forum, listach dyskusyjnych, wiki, stronach internetowych, kanałach IRC, na spotkaniach publicznych lub prywatnej korespondencji, wszędzie tam, gdzie jest to związane z projektem Mageia i jego usługami. Społeczność działa zgodnie z normami zawartymi w niniejszym kodeksie postępowania i będzie tych standardów bronić dla dobra społeczności. Liderzy wszystkich zespółów Mageia mają prawo do zawieszenia dostępu każdej osobie, która uporczywie narusza zasady kodeksu postępowania, lub w razie potrzeby, sprawa może być przedstawiona Radzie Mageia celem podjęcia ostatecznej decyzji.</p>
+                    </div>
+
+                    <h2 id="be_considerate" class="be">Bądź taktowny.</h2>
+
+                    <p>Twoje działania i praca będą miały wpływ na innych ludzi i będą przez nich używane. Ty z kolei będziesz uzależniony od pracy i działania innych. Jakąkolwiek podejmiesz decyzję, wpłynie ona na innych członków społeczności, i oczekujemy, że weźmiesz te konsekwencje pod uwagę przy podejmowaniu decyzji.</p>
+
+                    <p>Wnosząc swój udział upewnij się, że zawiera on uznanie wkładu pracy innych współautorów i weź pod uwagę, jak wprowadzone zmiany mogą wpłynąć na innych. Oczekuje się również, że przestrzegany będzie harmonogram realizacji projektu i wytyczne do niego.</p>
+
+                    <p>Jako użytkownik pamiętaj, że autorzy ciężko pracują na swoim wkładem do projektu Mageia i są z tego bardzo dumni. Jeśli znalazłeś coś co sprawia Ci problemy, jest bardzo prawdopodobne, że znajdzie się rozwiązanie, o ile w kulturalny sposób podasz wszystkim zainteresowanym dokładne informacje o zaistniałym problemie.</p>
+
+                    <h2 id="be_respectful" class="be">Szanuj innych.</h2>
+
+                    <p>Dla zachowania dobrej atmosfery w społeczności Mageia, jej uczestnicy muszą czuć się komfortowo i mieć poczucie akceptacji. Można to osiągnąć traktując się wzajemnie z szacunkiem. W przypadku nieporozumień należy przede wszytkim przyjąć, że ludźmi kierują dobre intencje.</p>
+
+                    <p>Nie tolerujemy ataków osobistych, rasizmu, seksizmu, ani żadnych innych form dyskryminacji. Nieporozumienia są od czasu do czasu nieuniknione, ale okazanie szacunku dla poglądów innych osób sprawi, że nasz punkt widzenia również spotka się z uznaniem. Poszanowanie innych ludzi, ich pracy i wkładu, przy założeniu pozytywnego nastawienia, spowoduje, że członkowie społeczności poczują się wygodnie i bezpiecznie, co wpłynie na ich motywację i efektywność.</p>
+
+                    <p>Oczekujemy, że członkowie naszej społeczności będą odnosili się z szacunkiem  wobec innych autorów, użytkowników i społeczności. Pamiętaj, że Mageia jest projektem międzynarodowym i możesz być nieświadomy istotnych aspektów innych kultur.</p>
+
+                    <h2 id="be_collaborative" class="be">Współpracuj z innymi.</h2>
+
+                    <p>Ruch wolnego oprogramowania opiera się na współpracy: pomaga ograniczać powielanie wysiłków przy jednoczesnej poprawie jakości tworzonego oprogramowania. W celu uniknięcia nieporozumień, staraj się wyrażać w sposób jasny i zwięzły przy żądaniu lub udzielanie pomocy. Pamiętaj, że łatwo jest źle zrozumieć e-maile (szczególnie gdy nie są napisane w języku ojczystym). Poproś o wyjaśnienia, jeśli coś wydaje się niejasne; zapamiętaj podstawową zasadę - po pierwsze zakładamy, że ludźmi kieruje dobra wola.</p>
+
+                    <p>Jako twórca wkładu powinieneś współpracować z innymi członkami społeczności, jak również z innymi społecznościami (na przykład upstream projektów), które są nimi zainteresowane, lub są zależne od wykonywanej przez ciebie pracy. Twoja praca powinna być przejrzysta i prekazywana do wspólnoty tak szybko jak to jest tylko możliwe (nawet jeśli nie jest w pełni zakończona). Jeśli chcesz pracować nad czymś nowym w istniejących już projektach, powiadom zespoły tych projektów o swoich pomysłach i dotychczasowych osiągnięciach.</p>
+
+                    <p>Osiągnięcie konsensusu w sprawie realizacji pomysłu nie zawsze jest to możliwe, więc nie czuj się zobowiązany do osiągnięcia przed rozpoczęciem jego realizacji. Jednak zawsze udzielaj informacji o swojej pracy i opublikuj ją w sposób, który pozwala osobom z zewnątrz testować, dyskutować i wnosić do niej swój wkład.</p>
+
+                    <p>Współtwórcy każdego projektu przychodzą i odchodzą. Wycofując się z projektu, w całości lub w części, należy być dumnym z tego, co osiągnęliśmy i działając w sposób odpowiedzialny wobec tych, którzy przyjdą po was do kontynuowania pracy nad projektem.</p>
+
+                    <p>Twoja opinia jako użytkownika jest bardzo ważna. Ważna jest również forma tej opinii. Nieprzemyślane uwagi są frustrujące i mogą spowodować demotywację innych członków społeczności, ale rzeczowa dyskusja o problemach z pewnością przyniesie pozytywne rezultaty. Słowo zachęty może czasem zdziałać cuda.</p>
+
+
+                    <h2 id="be_pragmatic" class="be">Bądź pragmatyczny.</h2>
+
+                    <p>Mageia jest społecznością pragmatyczną.</p>
+                    <p>Wymierne rezultaty są dla nas ważniejsze niż mieć ostatnie słowo w dyskusji. Będziemy bronić naszych podstawowych wartości jak wolność i szacunek dla współpracy, ale nie pozwolimy, aby spory o drobne sprawy stawały na drodze do osiągnięcia bardziej znaczących wyników. Jesteśmy otwarci na sugestie i przyjmujemy rozwiązania, niezależnie od ich pochodzenia. W razie wątpliwości, wspieramy rozwiązanie, które sprawia, że praca jest wykonana, zamiast tego, które teoretycznie jest poprawne, ale nie zostało zrealizowane. Korzystamy z narzędzi i metod, które pomagają zrealizować pracę. Decyzje mają być podejmowane przez tych, którzy pracę wykonują. Jeśli pojawią się pewne problemy, które wydają się nie do pogodzenia, zasięgamy opinii właściwego kierownika zespołu. Jeśli to konieczne, skonsultujemy się z Radą Mageia.</p>
+
+
+                    <h2 id="support" class="be">Udzielaj wsparcia.</h2>
+
+                    <p>Nasza społeczność jest silna dzięki wzajemnemu szacunkowi, współpracy i odpowiedzialnym, pragmatycznym zachowaniu. Czasem wartości tych trzeba bronić i udzielać pomocy innym członkom społeczności.</p>
+
+                    <p>Jeśli ktoś został zaatakowany, pomyśl, jak możesz udzielić mu osobistej pomocy. Jeśli pomoc przekracza twoje możliwości, zapytaj poszkodowaną osobę, czy potrzebuje interwencji w oficjalnej formie. Podobnie trzeba wesprzeć każdego, kto "wypala się" z przepracowania lub z powodów osobistych.</p>
+
+                    <p>Gdy pojawiają się problemy, rozważ w pierwszym kroku taktowne przypomnienie zwaśnionym stronom o naszym kodeksie postępowania. Liderzy są zdefiniowani przez swoje działania i mogą pomóc dając dobry przykład rozwiązując problemy w duchu niniejszego kodeksu postępowania i nie dopuszczjąc do ich eskalacji.</p>
+
+
+                    <h2 id="get_support" class="be">Korzystaj ze wsparcia innych uczestników.</h2>
+
+                    <p>Nieporozumienia, zarówno polityczne, jak i techniczne, zawsze się zdarzają. Nasza wspólnota nie jest tutaj wyjątkiem.  Celem nie jest unikanie nieporozumień czy różnic zdań, ale ich rozwiązywanie w sposób konstruktywny. Należy zwrócić się do wspólnoty, aby zasięgnąć porady i rozwiązać spór, a jeśli to możliwe, zasięgnąć opinii zespołu (-ów) bezpośrednio zainteresowanych.</p>
+
+                    <p>Zastanów się zanim z nieporozumienia uczynisz sprawę publiczną. W razie potrzeby poproś o pośrednictwo i spróbuj rozwiązać problem w mniej emocjonalny sposób. Jeśli czujesz zgrożenie osobiste, lub zagrożona jest twoja praca, nie spiesz się. Weź głeboki oddech zanim napiszesz coś, czego możesz później żałować. Czas łagodzi emocje. Poczekaj 24 godziny - to pozwoli ostudzić temperaturę dyskusji. Jeśli jednak deydujesz się na inny sposób rozwiązania sporu, to zachęcamy do opublikowania swoich pomysłów i pracy, tak aby mogła zostać ona sprawdzona i przetestowana.</p>
+
+                    <p class="license">This document is licensed under the <a href="http://creativecommons.org/licenses/by-sa/3.0/us/">Creative Commons Attribution - Share Alike 3.0 License</a> (<a href="/en/about/code-of-conduct/" rel="nofollow">parent version</a>).</p>
+                </div>
+            </div>
+        </div>
+    </div>
+</body>
+</html>
