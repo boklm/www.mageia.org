2f688d509bbaf76a41ee4239f715409b4362f62d
Romain d'Alverny <rda@mageia.org>
2011-01-18 14:55:06 +0000

    et faq page (Marek)
    git-svn-id: svn+ssh://svn.mageia.org/svn/web/www/trunk@158 ecb8f77e-9c09-4d67-9c5c-b96076beb801

--- et/faq/index.php
+++ et/faq/index.php
@@ -1,66 +1,63 @@
 <?xml version="1.0" encoding="utf-8"?>
 <!DOCTYPE html>
-<html dir="ltr" lang="en">
+<html dir="ltr" lang="et">
 <head>
     <meta charset="utf-8" />
-    <title>Mageia Frequently Asked Questions</title>
-    <meta name="description" content="Mageia is new community-based Linux distribution." />
-    <meta name="keywords" content="mageia, linux, mandriva, free software" />
+    <title>Mageia korduma kippuvad küsimused</title>
+    <meta name="description" content="Mageia on uus kogukonna loodav Linuxi distributsioon.">
+    <meta name="keywords" content="mageia, linux, mandriva, vaba tarkvara">
     <link rel="stylesheet" type="text/css" href="/g/style/all.css" />
     <?php include '../../analytics.php'; ?>
 </head>
 <body>
     <?php include '../../langs.php'; ?>
-    
     <div id="doc" class="yui-t7">
-        <div id="hd" role="banner"><h1>Mageia <span>| FAQ</span></h1></div>
+        <div id="hd" role="banner"><h1>Mageia <span>| KKK</span></h1></div>
         <div id="bd" role="main">
             <div class="yui-g">
                 <div class="para faq">
-                    <h3>What is Mageia?</h3>
-                    <p>Mageia is your new Linux distribution.</p>
+                    <h3>Mis on Mageia?</h3>
+                    <p>Mageia on teie uus Linuxi distributsioon.</p>
                     
-                    <h3>How does one pronounce Mageia?</h3>
-                    <p>There is no official pronunciation, you can use whichever one you like.</p>
+                    <h3>Kuidas hääldada Mageiat?</h3>
+                    <p>Mingit ametlikku hääldust pole, kõige lihtsam on seda hääldada just nii, nagu kirjutatakse.</p>
                     
-                    <h3>What does the name "Mageia" mean?</h3>
-                    <p>The term "mageia" — μαγεία in Greek — means "magic" in English.</p>
+                    <h3>Mida Mageia tähendab?</h3>
+                    <p>Sõna "mageia" — kreeka keeles μαγεία — tähendab maagiat, võlukunsti.</p>
                     
-                    <h3>I'm a Mandriva Linux user. What should I do?</h3>
-                    <p>For now you can continue using Mandriva Linux, there is no Mageia release available yet.
-                        You can
-                        <a href="https://www.mageia.org/mailman/listinfo/mageia-announce" hreflang="en">subscribe
-                            to the mageia-announce newsletter</a> to be informed
-                        when the first Mageia release is ready to use.</p>
+                    <h3>Ma olen Mandriva Linuxi kasutaja. Mida ma peaksin ette võtma?</h3>
+                    <p>Praegu võib rahulikult Mandriva Linuxit edasi kasutada, sest Mageia väljalase seisab alles ees.
+                        Küll võite
+                        <a href="https://www.mageia.org/mailman/listinfo/mageia-announce" hreflang="en">tellida endale
+                            uudiskirja mageia-announce</a>, kust saate teada, millal Mageia esimene väljalase ilmavalgust näeb.</p>
                     
-                    <h3>Will I be able to upgrade from Mandriva to Mageia?</h3>
-                    <p>Yes, it will be possible.</p>
+                    <h3>Kas Mandrivast saab Mageia peale uuendada?</h3>
+                    <p>Jah, me pakume sellist võimalust.</p>
                     
-                    <h3>When the first release of Mageia will be available?</h3>
-                    <p>As soon as possible. We hope to have some alpha or beta release by the end of November.</p>
+                    <h3>Millal ilmub Mageia esimene väljalase?</h3>
+                    <p>Nii pea kui võimalik. Me loodame esimese katsetamiseks mõeldud alfaväljalaskega valmis saada jaanuaris.</p>
 
-                    <h3>What will be the codename of the first Mageia release?</h3>
-                    <p>That hasn't been decided yet.</p>
+                    <h3>Milline on Mageia esimese väljalaske nimetus?</h3>
+                    <p>Seda pole veel otsustatud.</p>
                     
-                    <h3>Will urpmi, MCC, drakxtools be a part of Mageia?</h3>
-                    <p>Yes, your favourite tools will be included in Mageia.</p>
+                    <h3>Kas urpmi, MCC, drakxtools ja muud töötavad Mageias?</h3>
+                    <p>Jah, teie lemmiktööriistu pakub ka Mageia.</p>
 
-                    <h3>Where can I submit my logo ideas?</h3>
-                    <p>You can submit your logo ideas on the <a href="http://www.flickr.com/groups/mageia-art/" hreflang="en">Mageia art group</a>.</p>
+                    <h3>Kuhu ma saan esitada oma ideed logo kohta?</h3>
+                    <p>Oma ideed logo kohta tuleks esitada <a href="http://www.flickr.com/groups/mageia-art/" hreflang="en">Mageia kunstigrupis</a>.</p>
 
-                    <h3>How can I contribute to the mageia project?</h3>
-                    <p>All sorts of skill sets are welcome.
-                        You can <a href="http://mageia.org/wiki/doku.php">register yourself on the wiki</a>,
-                        in whatever teams that best fit your skills.</p>
+                    <h3>Kuidas ma saan kaasa aidata Mageia projektile?</h3>
+                    <p>Me ootame igasuguste oskustega inimesi.
+                        Te võite <a href="http://mageia.org/wiki/">registreerida ennast wikis</a>
+                        just sellises meeskonnas, mis kõige paremini vastab teie oskustele ja võimetele.</p>
 
-                    <h3>How can I contact the project?</h3>
-                    <p>You can send email to mageia-contact (at) mageia (dot) org.</p>
+                    <h3>Kuidas projektiga ühendust võtta?</h3>
+                    <p>Te võite saata e-kirja aadressile mageia-contact (ätt) mageia (punkt) org.</p>
 
-                    <h3>How can I donate to the project?</h3>
-                    <p>You can go to the <a href="http://www.mageia.org/en/donate//">donation page</a>.</p>
+                    <h3>Kuidas projekti heaks annetada?</h3>
+                    <p>Selleks leiab juhised <a href="http://www.mageia.org/et/donate/">annetuste leheküljel</a>.</p>
                 </div>
             </div>
         </div>
     </div>
-</body>
-</html>
+</body></html>
