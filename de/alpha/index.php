9baa2e25a16d4e45423376f56852081185984c8c
Oliver Burger <obgr_seneca@mageia.org>
2011-02-15 13:21:42 +0000

    added German translation of alpha1 page
    git-svn-id: svn+ssh://svn.mageia.org/svn/web/www/trunk@188 ecb8f77e-9c09-4d67-9c5c-b96076beb801

---
+++ de/alpha/index.php
@@ -0,0 +1,134 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE html>
+<html dir="ltr" lang="de">
+<head>
+    <meta charset="utf-8" />
+    <title>Mageias Alpha-Releases (Testversionen) herunterladen</title>
+    <meta name="description" content="Download-Seite f&uuml;r Mageias erste Alpha-Version" />
+    <meta name="keywords" content="mageia, alpha, unstable, iso, download, herunterladen, test, nicht produktiv" />
+    <meta name="author" content="Mageia" />
+    <link rel="stylesheet" type="text/css" href="/g/style/all.css" >
+    <?php include '../../analytics.php'; ?>
+</head>
+<body>
+    <?php include '../../langs.php'; ?>
+    
+    <div id="doc" class="yui-t7">
+        <div id="hd" role="banner"><h1>Mageia <span>| Alpha Download-Seite</span></h1>
+        <div id="bd" role="main">
+            <div class="yui-g">
+                <div class="para" style="color: #f22; text-align: center;">
+                    <h2 style="font-weight: bold;">Vorsicht! Alpha == keine stabile Software!</h2>
+                    <p>Diese Version ist NICHT F&Uuml;R folgende Zwecke: Benutzung, Testberichte, pers&ouml;nliche oder produktive Auswertung.</p>                                      
+                    <p>Diese Version is F&Uuml;R: folgende Zwecke: Entwickler- und Paketbauer-Tests.</p>
+                </div>
+            </div>
+            <div class="yui-g"><div class="para">
+                <p>Egal was Sie mit dem angebotenen ISO-Abbild tun, geschieht auf Ihr
+                    eigenes Risiko und Ihre eigene Verantwortung.
+                    Wenn Sie auf der Suche nach einem stabilen Linuxsystem sind, sollten
+                    Sie auf eine sp&auml;tere Ver&ouml;ffentlichung von Mageia warten.</p>
+
+                <p style="font-weight: bold; color: #f22">Nochmals: Bitte LADEN SIE diese
+                    Ver&ouml;ffentlichung NICHT herunter und/oder INSTALLIEREN SIE sie NICHT,
+                    wenn Sie sich nicht sicher sind, dass Sie genau verstehen, um was es sich
+                    bei dieser ISO handelt und um was nicht!</p>
+                
+                <h2>Sie wollen Mageia-Factory-Alpha1 also immer noch herunterladen?</h2>
+                <p>Na gut. Zuerst die Vorstellung: Wir bieten drei ISO-Abbilder an: eine
+                    32bit-DVD, eine 64bit-DVD und eine DVD f&uuml;r beide Architekturen.
+                    Jede ist etwa 3&nbsp;GiB gro&szlig;.</p>
+                <p>Unten finden Sie eine Liste von Servern, die zur Zeit Mageia-Software
+                    spiegeln:</p>
+                <ol>
+                    <li>W&auml;hlen Sie den Server, der am besten zu Ihnen passt (vorzugsweise in
+                        Ihrem Land.);</li>
+                    <li>W&auml;hlen Sie das ISO-Abbild, das f&uuml;r Sie am besten geeignet ist (die
+                        Namen sollten selbsterkl&auml;rend sein, das hoffen wir jedenfalls).</li>
+                </ol>
+                <br />
+                <table id="dl-table"><thead>
+                    <tr><th>Ort</th><th>Protokoll</th><th>Server</th></tr>
+                </thead><tbody>
+                <?php
+                /* @fixme note to self: brains! */
+                    $mirrors = array(
+                        array('China', 'Beijing', 'http://fundawang.lcuc.org.cn/mageia/'),
+                        array('Czech Republic', 'Praha', 'http://mageia.supp.name/'),
+                        array('Deutschland', 'Erfurt', 'http://ftp.mandrivauser.de/mirrors/Mageia/'),
+                        array('Deutschland', 'Erfurt', 'ftp://ftp.mandrivauser.de/mirrors/Mageia/'),
+                        array('France', 'Paris', 'http://distrib-coffee.ipsl.jussieu.fr/pub/linux/Mageia/'),
+                        array('France', 'Paris', 'ftp://distrib-coffee.ipsl.jussieu.fr/pub/linux/Mageia/'),
+                        array('Nouvelle Calédonie', 'Nouméa', 'http://mageia.nautile.nc/mageia/distrib/'),
+                        array('USA', 'Durham', 'ftp://distro.ibiblio.org/pub/linux/distributions/mageia/'),
+                        array('USA', 'Durham', 'http://distro.ibiblio.org/pub/linux/distributions/mageia/'),
+                    );
+                    $tmpl = '<tr><td>%s, %s</td><td>%s</td><td><a href="%s%s">%s</a></td></tr>';
+                    foreach ($mirrors as $m):
+                        $pu = parse_url($m[2]);
+                        echo sprintf($tmpl,
+                            $m[1], $m[0],
+                            strtoupper($pu['scheme']),
+                            $m[2],
+                            'iso/',
+                            $pu['host']);
+                    endforeach;
+                ?>
+                </body>
+                </table>
+                
+                
+            </div></div>
+            <div class="yui-g">
+                <div class="yui-u first"><div class="para" style="padding-right: 0;">
+                    <h2>&Uuml;ber diese Ver&ouml;ffentlichung</h2>
+                    <p>Dies ist unsere allererste ISO, die wir mit unserem Buildsystem erstellt
+                        haben. Sie nun zu ver&ouml;ffentlichen ist eine gute Methode, um dies zu testen
+                        und zu sehen, was daran in den kommenden Wochen noch verbessert
+                        werden muss.</p>
+                    
+                    <ul>
+                        <li><a href="/wiki/doku.php?id=iso1:alpha1_release_notes" hreflang="en">Release-Notes</a></li>
+                        <li><a href="/wiki/doku.php?id=iso1:alpha1_errata" hreflang="en">Erratas</a></li>
+                        <li><a href="/en/">Was ist Mageia?</a></li>
+                    </ul>
+                    <p>Alpha2 wird am 15. M&auml;rz ver&ouml;ffentlicht<sup>th</sup>.</p>
+                    <p>Sie k&ouml;nnen unsere weitere <a href="http://mageia.org/wiki/doku.php?id=iso_1_specifications#expected_milestones">Roadmap f&uuml;r Mageia 1</a> ansehen.</p>
+                    
+                    
+                </div></div>
+                <div class="yui-u"><div class="para">
+                    <h2>Sie k&ouml;nnen helfen!</h2>
+                    <p>Ja! Diese Ver&ouml;ffentlichung ist noch ein bisschen holperig, und - auch
+                        wenn die n&auml;chste besser sein wird - wird diese 
+                        Alpha2-Version vermutlich ebenfalls noch unausgereift sein.</p>
+                    <p>Es gibt viele Dinge, die man zu Mageia beitragen kann: entwickeln,
+                        Pakete bauen, testen, &uuml;bersetzen, Bugs melden und beheben und 
+                        <a href="http://mageia.org/wiki/">so vieles mehr</a>!</p>
+                    <p><a href="http://mageia.org/wiki/doku.php?id=contribute">Erfahren Sie, wie
+                    Sie sich an diesem Spa&szlig; beteiligen und helfen k&ouml;nnen.</a></p>
+
+                </div>
+                <div class="para">
+                    <h3>Spenden Sie!</h3>
+                    <p>Ja! <a href="/en/donate/">Sie k&ouml;nnen auch spenden!</a></p>
+                    <p>Mageia kann sich nur auf die Hilfe seiner eigenen Community verlassen,
+                        sei dies in Bezug auf Zeit, F&auml;higkeiten und auch Geld!</p>
+                </div></div>
+            </div>
+            <div class="para" style="color: #999;">
+                <p>Ja, dies ist bis jetzt nur die Rohfassung einer Downloadseite. Aber sie hat
+                    ihre
+                    Vorteile darin, dass sie das Herunterladen dieser Alpha weniger attraktiv f&uuml;r
+                    Nichtentwickler macht. Haben wir schon erw&auml;hnt, dass wir Helfer in unseren
+                    <a href="http://www.mageia.org/wiki/doku.php?id=web">Web-</a> und 
+                    <a href="http://www.mageia.org/wiki/doku.php?id=sysadmin">
+                    Sysadmin-Teams</a> begr&uuml;&szlig;en?
+                    Es gibt auch noch <a href="http://www.mageia.org/wiki/doku.php?#teams">viele
+                    andere sehr freundliche Teams</a> in unserem Projekt.</p>
+
+            </div>
+        </div>
+    </div>
+</body>
+</html>
