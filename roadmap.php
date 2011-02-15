fe90cbfd1694b032a436ed5474856aa1859d7222
Damien Lallement <dams@mageia.org>
2011-02-15 15:01:41 +0000

    update 'happening now' and add a link to 'alpha'
    git-svn-id: svn+ssh://svn.mageia.org/svn/web/www/trunk@195 ecb8f77e-9c09-4d67-9c5c-b96076beb801

--- roadmap.php
+++ roadmap.php
@@ -7,9 +7,9 @@ $roadmap = array(
 
 ?>
 <ul id="rightnow">
-    <li>Alpha 1 release pending: February&nbsp;15<sup>th</sup></li>
+    <li>Alpha 1 available! <a href="/alpha/">Enjoy it</a>!</li>
     <li>Logo/identity refinements</li>
-    <li>Teams setup (Marcom)</li>
+    <li>Teams setup (Marcom/QA)</li>
     <li>Web tools setup</li>
 </ul>
 <p><a href="http://mageia.org/wiki/doku.php?id=roadmap">Roadmap</a></p>
