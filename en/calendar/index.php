a0d2bec69fe48dc15c7557bb439d46909bbfee22
Romain d'Alverny <rda@mageia.org>
2011-03-03 10:28:09 +0000

    add ICS URLs
    git-svn-id: svn+ssh://svn.mageia.org/svn/web/www/trunk@250 ecb8f77e-9c09-4d67-9c5c-b96076beb801

--- en/calendar/index.php
+++ en/calendar/index.php
@@ -19,23 +19,47 @@
             <div class="yui-g">
                 <br />
                 <?php
-                $params = array(
-                    'height' => 600,
-                    'wkst' => 2,
-                    'hl' => 'en_GB',
-                    'bgcolor' => '#FFFFFF',
-                    'src' => 'ojiv9mbtj8nt248dcjsfn6n664@group.calendar.google.com',
-                    'color' => '#125A12',
-                    'src' => 'gpm9kdohufidedmlpnuuq4pvmo@group.calendar.google.com',
-                    'color' => '#8C500B',
-                    'src' => 'hm0j50l2vmv0dlstaigbm7nt30@group.calendar.google.com',
-                    'color' => '#691426',
-                    'src' => 'uo3onvtl8q6qk5m3emq83rekag@group.calendar.google.com',
-                    'color' => '#42104A',
-                    'ctz' => 'Europe/Paris'
+                /**
+                 *
+                */
+                $calendars = array(
+                    array(
+                        'id' => 'ojiv9mbtj8nt248dcjsfn6n664@group.calendar.google.com',
+                        'title' => 'events',
+                        'color' => '#125A12'
+                    ),
+                    array(
+                        'id' => 'gpm9kdohufidedmlpnuuq4pvmo@group.calendar.google.com',
+                        'title' => 'meetings &amp; organization',
+                        'color' => '#8C500B'
+                    ),
+                    array(
+                        'id' => 'hm0j50l2vmv0dlstaigbm7nt30@group.calendar.google.com',
+                        'title' => 'development &amp; release plan',
+                        'color' => '#691426'
+                    ),
+                    array(
+                        'id' => 'uo3onvtl8q6qk5m3emq83rekag@group.calendar.google.com',
+                        'title' => 'mentoring',
+                        'color' => '#42104A'
+                    )
                 );
+                
+                $params = array(
+                    'height'  => 600,
+                    'wkst'    => 2,
+                    'hl'      => 'en_GB',
+                    'bgcolor' => '#FFFFFF',
+                    'ctz'     => 'UTC'
+                );
+                $params = http_build_query($params);
+                foreach ($calendars as $c)
+                    $params .= sprintf('&src=%s&color=%s',
+                        urlencode($c['id']), urlencode($c['color']));
+
+                $url = 'https://www.google.com/calendar/b/0/embed?' . $params;
                 ?>
-                <iframe src="https://www.google.com/calendar/b/0/embed?height=600&amp;wkst=2&amp;hl=en&amp;bgcolor=%23FFFFFF&amp;src=ojiv9mbtj8nt248dcjsfn6n664%40group.calendar.google.com&amp;color=%23125A12&amp;src=gpm9kdohufidedmlpnuuq4pvmo%40group.calendar.google.com&amp;color=%238C500B&amp;src=uo3onvtl8q6qk5m3emq83rekag%40group.calendar.google.com&amp;color=%2342104A&amp;src=hm0j50l2vmv0dlstaigbm7nt30%40group.calendar.google.com&amp;color=%23691426&amp;ctz=UTC"
+                <iframe src="<?php echo $url; ?>"
                     style="border-width:0"
                     width="749"
                     height="600"
@@ -54,6 +78,21 @@
                         <li>your <a href="http://mageia.org/wiki/">team</a> leader or representative,</li>
                         <li><a href="irc://irc.freenode.net/#mageia-dev">#mageia-dev</a> on Freenode,</li>
                         <li><a href="mailto:mageia-contact@mageia.org">mageia-contact@mageia.org</a> in last resort.</li>
+                    </ul>
+                </div>
+                <div class="para">
+                    <h3>ICS files</h3>
+                    <p>You may get read-only access directly to these .ics files:</p>
+                    <ul>
+                    <?php
+                    $ical_tmpl = 'http://www.google.com/calendar/ical/%s/public/basic.ics';
+                    foreach ($calendars as $c)
+                    {
+                        echo sprintf('<li><a href="%s">Mageia %s</a></li>',
+                            sprintf($ical_tmpl, urlencode($c['id'])),
+                            $c['title']);
+                    }
+                    ?>
                     </ul>
                 </div>
             </div>
