dab90e205ccc6f4b0d134838382127c801fdc2d8
Damien Lallement <dams@mageia.org>
2011-03-16 11:11:02 +0000

    add financial reports
    git-svn-id: svn+ssh://svn.mageia.org/svn/web/www/trunk@270 ecb8f77e-9c09-4d67-9c5c-b96076beb801

---
+++ en/about/reports/2010/index.php
@@ -0,0 +1,27 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE html>
+<html dir="ltr" lang="en">
+<head>
+    <meta charset="utf-8" />
+    <title>Mageia Values</title>
+    <meta name="description" content="Mageia is new community-based Linux distribution." />
+    <meta name="keywords" content="mageia, financial report, 2010" />
+    <meta name="author" content="Mageia" />
+    <link rel="stylesheet" type="text/css" href="/g/style/all.css" />
+    <?php include '../../../../analytics.php'; ?>
+</head>
+<body>
+    <?php include '../../../../langs.php'; ?>
+    
+    <div id="doc" class="yui-t7">
+        <div id="hd" role="banner"><h1>Mageia <span>| Financial Report (2010)</span></h1></div>
+        <div id="bd" role="main">
+            <div class="yui-g">
+                <div class="para values">
+
+                </div>
+            </div>
+        </div>
+    </div>
+</body>
+</html>
