9bd38cff7e52b0d01d82ef2d23984a6dbf8b5fb7
Damien Lallement <dams@mageia.org>
2011-03-16 16:09:15 +0000

    update financial reports (2010)
    git-svn-id: svn+ssh://svn.mageia.org/svn/web/www/trunk@279 ecb8f77e-9c09-4d67-9c5c-b96076beb801

--- en/about/reports/2010/index.php
+++ en/about/reports/2010/index.php
@@ -20,28 +20,34 @@
                 <div class="para values">
 			<h2>Summary</h2>
 			<ul>
-				<li>Amount collected: 8 804.72&nbsp;&euro;</li>
+				<li>Amount collected: EUR&nbsp;8 801.66</li>
 				<li>Donators number: 149</li>
-				<li>Average donation: 59.07&nbsp;&euro;</li>
-				<li>Amount spent: 1 274.40&nbsp;&euro;</li>
-				<li>Amount remaining: 7 527.02&nbsp;&euro;</li>
+				<li>Average donation: EUR&nbsp;59.07</li>
+				<li>Amount spent: EUR&nbsp;1 274.40</li>
+				<li>Amount remaining: EUR&nbsp;7 527.02</li>
 			</ul>
 			<h2>Financial Report</h2>
 			<table><thead><tr><th>Wording of the financial transaction</th><th>Income</th><th>Outgoing</th></tr></thead><tbody><tr>
-				<tr><td>Donations by Paypal/transfer/check/cash<sup>[1]</sup></td><td>8 801.42&nbsp;&euro;</td><td>&nbsp;</td></tr>
-				<tr><td>Paypal test to validate bank account</td><td>0.24&nbsp;&euro;</td><td>&nbsp;</td></tr>
-				<tr><td>Interest bank account (2%)</td><td>3.06&nbsp;&euro;</td><td>&nbsp;</td></tr>
+				<tr><td>Donations by Paypal/transfer/check/cash</td><td>EUR&nbsp;8 801.42</td><td>&nbsp;</td></tr>
+				<tr><td>Paypal test to validate bank account</td><td>EUR&nbsp;0.24</td><td>&nbsp;</td></tr>
 				<tr><td></td><td>&nbsp;</td><td>&euro;</td></tr>
 				<tr><td></td><td>&nbsp;</td><td>&euro;</td></tr></tbody></table>
-			<sup>[1]</sup>: all donations are mixed for 2010 as at the beginning donation where managed by AUFML. So we had a total bank transfert with all the donations.<br/>
+			<h2>Bank accounts situation</h2>
+			<ul>
+                                <li>Paypal (non-for-profit organisation account): EUR&nbsp;63.51</li>
+                                <li>Main account (with checkbook and no credit card): EUR&nbsp;1 310.24</li>
+				<li>Savings account (between 2% and 4% of interet): EUR&nbsp;4 000.00</li>
+				<li>AUFML account (part to be transfered to Mageia.Org): EUR&nbsp;2 153.28</li>
+                        </ul>
 			<h2>Miscellaneous</h2>
 			<ul>
+				<li>Donations are mixed for 2010 as at the beginning they were managed by AUFML. So we had a total bank transfert with all donations.</li>
 				<li>You can <a href="#" hreflang="en">download this report</a> as a PDF file.</li>
 				<li>You can help Mageia by <a href="/en/donate/" hreflang="en">doing a donation</a>.</li>
 				<li>You can see the <a href="/en/thank-you" hreflang="en">"thank you" page</a>.</li>
 			</ul>
 			<h2>Contact</h2>
-			Financial report published by Damien Lallement (Mageia Treasurer on the 2010 Fiscal Year).<br/>You can contact him by sending an email to <em>treasurer[at]mageia[dot]org</em>.<br/>Last updated: Wed Mar 16 15:02:51 CET 2011.
+			Financial report published by Damien Lallement (Mageia Treasurer on the 2010 Fiscal Year).<br/>You can contact him by sending an email to <em>treasurer[at]mageia[dot]org</em>.<br/>Last updated: Wed Mar 16 16:43:17 CET 2011.
                 </div>
             </div>
         </div>
