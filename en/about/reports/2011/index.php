976d4e60b9cefad1d9bd61fa02e775e9b05048e1
Damien Lallement <dams@mageia.org>
2011-03-18 14:19:00 +0000

    update financial reports (2011)
    git-svn-id: svn+ssh://svn.mageia.org/svn/web/www/trunk@292 ecb8f77e-9c09-4d67-9c5c-b96076beb801

--- en/about/reports/2011/index.php
+++ en/about/reports/2011/index.php
@@ -5,7 +5,7 @@
     <meta charset="utf-8" />
     <title>Mageia Values</title>
     <meta name="description" content="Mageia is new community-based Linux distribution." />
-    <meta name="keywords" content="mageia, financial report, 2010" />
+    <meta name="keywords" content="mageia, financial report, 2011" />
     <meta name="author" content="Mageia" />
     <link rel="stylesheet" type="text/css" href="/g/style/all.css" />
     <?php include '../../../../analytics.php'; ?>
@@ -14,11 +14,49 @@
     <?php include '../../../../langs.php'; ?>
     
     <div id="doc" class="yui-t7">
-        <div id="hd" role="banner"><h1>Mageia <span>| Financial Report (2010)</span></h1></div>
+        <div id="hd" role="banner"><h1>Mageia <span>| Financial Report (2011)</span></h1></div>
         <div id="bd" role="main">
             <div class="yui-g">
                 <div class="para values">
-
+			<h2>Summary</h2>
+			<ul>
+				<li>Outstanding amount of 2010: EUR&nbsp;</li>
+				<li>Amount collected: EUR&nbsp;</li>
+				<li>Donators number: </li>
+				<li>Average donation: EUR&nbsp;</li>
+				<li>Amount spent: EUR&nbsp;</li>
+				<li>Amount remaining: EUR&nbsp;</li>
+			</ul>
+			<h2>Financial Report</h2>
+			<table id="fr-table"><thead><tr><th>Wording of the financial transaction</th><th>Income</th><th>Outgoing</th></tr></thead><tbody><tr>
+				<tr><td></td><td class="money">EUR&nbsp;</td><td>&nbsp;</td></tr>
+				<tr><td></td><td class="money">EUR&nbsp;</td><td>&nbsp;</td></tr>
+				<tr><td colspan=3></td></tr>
+				<tr><td></td><td>&nbsp;</td><td class="money">EUR&nbsp;</td></tr>
+				<tr><td></td><td>&nbsp;</td><td class="money">EUR&nbsp;</td></tr>
+				<tr><td colspan=3></td></tr>
+				<tr><td></td><td>&nbsp;</td><td class="money">EUR&nbsp;</td></tr>
+				<tr><td></td><td>&nbsp;</td><td class="money">EUR&nbsp;</td></tr>
+				<tr><td></td><td>&nbsp;</td><td class="money">EUR&nbsp;</td></tr>
+				<tr><td></td><td>&nbsp;</td><td class="money">EUR&nbsp;</td></tr>
+				<tr><td colspan=3>&nbsp;</td></tr>
+				<tr><td>Total</td><td class="money">EUR&nbsp;</td><td class="money">EUR&nbsp;</td></tr>
+				<tr><td>Balance</td><td colspan=2 class="money">EUR&nbsp;</td></tr>
+				</tbody></table>
+			<h2>Bank accounts situation</h2>
+			<ul>
+                                <li>Paypal (non-for-profit organisation account): EUR&nbsp;</li>
+				<li>Main account (with checkbook and no credit card): EUR&nbsp;</li>
+				<li>Savings account (between 2% and 4% of interest): EUR&nbsp;</li>
+			</ul>
+			<h2>Miscellaneous</h2>
+			<ul>
+				<li>You can <a href="#" hreflang="en">download this report</a> as a PDF file.</li>
+				<li>You can help Mageia by <a href="/en/donate/" hreflang="en">doing a donation</a>.</li>
+				<li>You can see the <a href="/en/thank-you" hreflang="en">"thank you" page</a>.</li>
+			</ul>
+			<h2>Contact</h2>
+			Financial report published by Damien Lallement (Mageia Treasurer on the 2011 Fiscal Year).<br/>You can contact him by sending an email to <em>treasurer[at]mageia[dot]org</em>.<br/>Last updated: Mar 18 15:15:32 CET 2011.
                 </div>
             </div>
         </div>
