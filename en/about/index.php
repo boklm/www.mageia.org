fae5255191f84c3d4a491c490299ce9b5a16b725
Romain d'Alverny <rda@mageia.org>
2011-03-18 14:46:49 +0000

    temp index for about section
    git-svn-id: svn+ssh://svn.mageia.org/svn/web/www/trunk@296 ecb8f77e-9c09-4d67-9c5c-b96076beb801

---
+++ en/about/index.php
@@ -0,0 +1,35 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE html>
+<html dir="ltr" lang="en">
+<head>
+    <meta charset="utf-8" />
+    <title>About Mageia</title>
+    <meta name="description" content="Mageia" />
+    <meta name="keywords" content="mageia" />
+    <meta name="author" content="Mageia" />
+    <link rel="stylesheet" type="text/css" href="/g/style/all.css" >
+    <?php include '../../analytics.php'; ?>
+</head>
+<body>
+    <?php include '../../langs.php'; ?>
+    
+    <div id="doc" class="yui-t7">
+        <div id="hd" role="banner"><h1>Mageia <span>| About</span></h1></div>
+        <div id="bd" role="main">
+            <div class="yui-g"><div class="para">
+
+                <ul>
+                    <li><a href="/wiki/doku.php?id=org">Governance</a></li>
+                    <li><a href="./code-of-conduct/">Code of conduct</a></li>
+                    <li><a href="./policies/">Policies</a></li>
+                    <li><a href="./values/">Values</a></li>
+                    <li>Financial reports: <a href="./reports/2010/">2010</a>,
+                        <a href="./reports/2011/">2011</a></li>
+                    <li><a href="../donate/">Donations</a></li>
+                    <li><a href="../thank-you/">Thanks!</a></li>
+                </ul>
+            </div></div>
+        </div>
+    </div>
+</body>
+</html>
