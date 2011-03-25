80d136cd335e0cbab0ca7fd1905fcabac617800f
Romain d'Alverny <rda@mageia.org>
2011-03-25 21:54:13 +0000

    tr alpha page (murat)
    git-svn-id: svn+ssh://svn.mageia.org/svn/web/www/trunk@300 ecb8f77e-9c09-4d67-9c5c-b96076beb801

--- tr/alpha/index.php
+++ tr/alpha/index.php
@@ -3,9 +3,9 @@
 <html dir="ltr" lang="tr">
 <head>
     <meta charset="utf-8" />
-    <title>Download Mageia alpha (unstable) releases</title> 
-    <meta name="description" content="Download page for Mageia first technical alpha" /> 
-    <meta name="keywords" content="mageia, alpha, unstable, iso, download, test, not for production" /> 
+    <title>Mageia Alfa (Kararsız) Sürümlerini İndir</title>
+    <meta name="description" content="Mageia Alfa (Kararsız) Sürümlerini İndir" />
+    <meta name="keywords" content="mageia, alpha, unstable, iso, download, kararsız, sürüm, indir, test" />
     <meta name="author" content="Mageia" />
     <link rel="stylesheet" type="text/css" href="/g/style/all.css" >
     <?php include '../../analytics.php'; ?>
@@ -17,44 +17,48 @@
         <div id="hd" role="banner"><h1>Mageia <span>| Alfa Sürümünü İndir</span></h1></div>
         <div id="bd" role="main">
             <div class="yui-g">
-                <div class="para" style="color: #f22; text-align: center;">
-                    <h2 style="font-weight: bold;">Dikkatli olun! Alfa == Kararsız Yazılım!</h2> 
-                    <p>Bunun için değildir: normal kullanım, kamu eleştirisi, kişisel ya da üretim değerlendirmesi.</p> 
-                    <p>Bunun İçindir: geliştiriciler ve paketleyicilerin test etmesi.</p> 
+                <div class="para" style="color: #f22; text-align: left;">
+                    <p>Lütfen Dikkat! Alfa == Kararsız  Yazılım <br />
+                    Normal kullanım, kamu eleştirisi, kişisel ye da üretim değerlendirmesi için UYGUN DEĞİLDİR.<br />
+                    Geliştiriciler ve paketleyicilerin testi için UYGUNDUR.</p>
                 </div>
             </div>
             <div class="yui-g"><div class="para">
-                <p>Her ne olursa olsun ISO disk imajı ile yapabildiğin herşey senin kendi riskin ile olacak, senin kendi sorumluluğun ile.
-                    Eğer kararlı bir Linux sistemi arıyorsanız, Mageia'nın daha sonraki sürümleri için beklemeyi tercih edebilirsiniz.</p> 
- 
-                <p style="font-weight: bold; color: #f22">Ek olarak , bu ISO'nun ne olup olmadığını kesin olarak anlamadıysanız lütfen bunu indirmeyin ve/veya kurmayın.</p> 
+                <p>Her ne olursa olsun, bu ISO disk kalıbı ile yapacağınız her şeyin riski ve sorumluluğu size ait olacaktır. Eğer kararlı bir Linux sistemi arıyorsanız, Mageia'nın sonraki sürümlerini bekleyebilirsiniz.</p> 
                 
-                <h2>Hala Mageia Atölye Alfa'sını indirmek istiyor musunuz?</h2> 
-                <p>Tamam. İlk olarak, sunumlar: İki ISO imajımız var: 32 bit DVD ve 64 bit DVD. Her ikisinin büyüklüğü 2 GİB.</p> 
-                <p>Aşağıda güncel Mageia yazılım yansısı için sunucu listesini bulacaksınız:</p> 
+                <h2>Hala Mageia Alpha 2'yi indirmek istiyor musunuz?</h2> 
+                <p>Tamam. İlk olarak açıklamalar: Üç (3) ISO kalıbımız var: Biri 32bit DVD, biri 64bit DVD (yaklaşık 4 GB) ve biri de ikili (dual) yapıda CD (yaklaşık 700 MB).</p> 
+                <p>Mageia Alpha 2 kalıplarını  bulabileceğiniz sunucuların listesi aşağıdadır:</p> 
                 <ol> 
-                    <li>size en uygun sunucuyu seçin (tercihen sizin ülkenizde ki);</li> 
+                    <li>Önce size en uygun sunucuyu seçin (tercihen kendi ülkenizdeki).</li> 
  
-                    <li>size en uygun ISO imajını seçin (isimler kendini belli etmeli, öyle umarız).</li> 
+                    <li>Sonra size en uygun ISO kalıbını seçin (Kalıp isimlerinden  aradığınızı bulacağınızı umuyoruz).</li> 
                 </ol>
                 <?php include '../../mirrors.php'; ?>
             </div></div>
             <div class="yui-g">
                 <div class="yui-u first"><div class="para" style="padding-right: 0;">
-                    <h2>Bu sürüm hakkında</h2> 
-                    <p>Bu, inşa sistemimizden yayınlanmış ilk ISO'muzdur.
-                        Onu şimdi yayınlamak sonraki haftalar için nelerin iyileştirileceğini görmenin ve test etmenin en iyi yolu.</p> 
+                    <h2>Alfa 2 sürümü hakkında</h2> 
+                    <p>Alfa  1'e göre en önemli değişiklikler:</p> 
+                    <ul>
+                        <li>Daha fazla paket. Yaklaşık 1800 tane!</li>
+                        <li>LibreOffice</li>
+                        <li>KDE 4.6.1</li>
+                        <li>Mozilla Firefox 4 RC1</li>
+                        <li>Bir dizi temizlik</li>
+                        <li>Kullanıcı arayüzü (UI) temizlikleri</li>
+                    </ul>
                     
+                    <p>Bakınız:</p>
                     <ul> 
-                        <li><a href="http://blog.mageia.org/?p=492" hreflang="en">Blog post</a></li> 
-                        <li><a href="/wiki/doku.php?id=iso1:alpha1_release_notes" hreflang="en">Sürüm Notları</a></li> 
-                        <li><a href="/wiki/doku.php?id=iso1:alpha1_errata" hreflang="en">Errata</a></li> 
+                        <li><a href="http://blog.mageia.org/en/2011/03/15/mageia-1-alpha-2-primavera-is-coming" hreflang="en">Blog post</a></li> 
+                        <li><a href="/wiki/doku.php?id=iso1:alpha2_release_notes" hreflang="en">Sürüm Notları</a></li> 
+                        <li><a href="/wiki/doku.php?id=mageia1:errata" hreflang="en">Errata</a></li> 
                         <li><a href="/tr/">Mageia nedir?</a></li> 
                     </ul> 
-                    <p>Alfa 2, 15 Mart'ta yayınlanmış olacak.</p> 
-                    <p><a href="http://mageia.org/wiki/doku.php?id=iso_1_specifications#expected_milestones">Mageia 1 için yol haritasını</a> kontrol edebilirsiniz.</p>
-                    
-                    
+                    <p>Beta 1, 5 Nisan'da  yayınlanacak.</p> 
+                    <p>Yol haritasını <a href="http://mageia.org/wiki/doku.php?id=iso_1_specifications#expected_milestones">buradan kontrol edebilirsiniz.</a></p>
+
                 </div></div>
                 <div class="yui-u"><div class="para">
                     <h2>Yardım edebilirsiniz!</h2> 
@@ -65,14 +69,12 @@
                 </div>
                 <div class="para">
                     <h3>Bağış!</h3> 
-                    <p>Evet! <a href="/en/donate/">Bağışta yapabilirsin!</a></p> 
+                    <p>Evet! <a href="/tr/donate/">Bağışta yapabilirsin!</a></p> 
                     <p>Mageia zaman, beceri ve para konularında sadece kendi topluluğundan yararlanabiliyor!</p>
                 </div></div>
             </div>
             <div class="para" style="color: #999;">
-                <p>Evet, oldukça kaba bir indirme safyası, şimdilik. Fakat geliştirci olmayanlar için bu alfayı indirmenin daha az cazip olması iyidir.
-                    Bizim <a href="http://www.mageia.org/wiki/doku.php?id=web">Web</a> ve <a href="http://www.mageia.org/wiki/doku.php?id=sysadmin">Sysadmin</a> ekiplerimize katkıda bulunmaları için insanları hoşça karşıladığımızı söyledik dimi?
-                    Yanı sıra proje içerisinde <a href="http://www.mageia.org/wiki/doku.php?#teams">oldukça dostça diğer ekiplerde</a> var.</p>
+                <p>Evet, şimdilik oldukça kaba bir indirme safyası. Fakat geliştirici olmayanlar için alfa sürümünü indirmenin daha az cazip olması iyidir. Biz, <a href="http://www.mageia.org/wiki/doku.php?id=web">Web</a> ve <a href="http://www.mageia.org/wiki/doku.php?id=sysadmin">Sysadmin</a> ekiplerimize katkıda bulunmaları için insanları hoş  karşıladığımızı söyledik değil mi? Ayrıca, proje içerisinde <a href="http://www.mageia.org/wiki/doku.php?#teams">oldukça dostça diğer ekiplerde</a> var.</p>
 
             </div>
         </div>
