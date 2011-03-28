01e5b1b0418c37b6352874f80f0ef1c6e24163d6
Damien Lallement <dams@mageia.org>
2011-03-28 16:07:25 +0000

    update financial report (typo)
    git-svn-id: svn+ssh://svn.mageia.org/svn/web/www/trunk@302 ecb8f77e-9c09-4d67-9c5c-b96076beb801

--- en/about/reports/2011/index.php
+++ en/about/reports/2011/index.php
@@ -57,13 +57,13 @@
                                 <tr><td>Gandi (hosting)</td><td class="money">EUR&nbsp;8.00</td><td>5</td><td>12</td><td class="money">EUR&nbsp;480.00</td><td>pending</td></tr>
                                 <tr><td>Dedibox (build iso)</td><td class="money">EUR&nbsp;14.99</td><td>1</td><td>12</td><td class="money">EUR&nbsp;179.88</td><td>pending</td></tr>
                                 <tr><td>Internet domains</td><td class="money">EUR&nbsp;15.00</td><td>7</td><td>1</td><td class="money">EUR&nbsp;105.00</td><td>pending</td></tr>
-                                <tr><td>Fiona (backup server)</td><td class="money">EUR&nbsp;1 500.00</td><td>1</td><td>1</td><td class="money">EUR&nbsp;1 500.OO</td><td>pending</td></tr>
+                                <tr><td>Fiona (backup server)</td><td class="money">EUR&nbsp;1 500.00</td><td>1</td><td>1</td><td class="money">EUR&nbsp;1 500.00</td><td>pending</td></tr>
                                 <tr><td>2 new nodes for BS</td><td class="money">EUR&nbsp;2 500.00</td><td>2</td><td>1</td><td class="money">EUR&nbsp;5 000.00</td><td>pending</td></tr>
-                                <tr><td>Event box</td><td class="money">EUR&nbsp;1500</td><td>1</td><td>1</td><td class="money">EUR&nbsp;1 500.00</td><td>pending</td></tr>
+                                <tr><td>Event box</td><td class="money">EUR&nbsp;1500.00</td><td>1</td><td>1</td><td class="money">EUR&nbsp;1 500.00</td><td>pending</td></tr>
                                 <tr><td>Event travel</td><td class="money">EUR&nbsp;1 000.00</td><td>1</td><td>2</td><td class="money">EUR&nbsp;2 000.00</td><td>pending</td></tr>
                                 <tr><td>HW for spare</td><td class="money">EUR&nbsp;3 000.00</td><td>1</td><td>1</td><td class="money">EUR&nbsp;3 000.00</td><td>pending</td></tr>
-                                <tr><td>Brand registration</td class="money">EUR&nbsp;250.00<td></td>1<td></td><td>1</td><td class="money">EUR&nbsp;250.00</td><td>pending</td></tr>
-				<tr><td>Reserve fund</td class="money">EUR&nbsp;4 000.00<td></td>1<td></td><td>1</td><td class="money">EUR&nbsp;4 000.00</td><td>pending</td></tr>
+                                <tr><td>Brand registration</td><td class="money">EUR&nbsp;250.00</td><td>1</td><td>1</td><td class="money">EUR&nbsp;250.00</td><td>pending</td></tr>
+				<tr><td>Reserve fund</td><td class="money">EUR&nbsp;4 000.00</td><td>1</td><td>1</td><td class="money">EUR&nbsp;4 000.00</td><td>pending</td></tr>
 				<tr><td colspan=6>&nbsp;<td></tr>
                                 <tr><td colspan=4>Total</td><td class="money">EUR&nbsp;22 614.88</td><td>pending</td></tr>
 			</table>			
