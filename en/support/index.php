e926ed8ad421e4371d7610d351ae7f4e1f6b412e
Romain d'Alverny <rda@mageia.org>
2011-03-31 10:04:51 +0000

    support page draft
    git-svn-id: svn+ssh://svn.mageia.org/svn/web/www/trunk@320 ecb8f77e-9c09-4d67-9c5c-b96076beb801

---
+++ en/support/index.php
@@ -0,0 +1,84 @@
+<!DOCTYPE html>
+<html dir="ltr" lang="en">
+<head>
+    <meta charset="utf-8">
+    <title>Support for Mageia distribution</title>
+    <meta name="description" content="List of support resources for Mageia distribution.">
+    <meta name="keywords" content="mageia, linux, support, help, hardware, qa, test">
+    <meta name="author" content="Mageia">
+    <link rel="stylesheet" type="text/css" href="/g/style/all.css" >
+    <?php include '../../analytics.php'; ?>
+</head>
+<body>
+    <?php include '../../langs.php'; ?>
+    
+    <div id="doc" class="yui-t7">
+        <div id="hd" role="banner"><h1><a id="logo" href="/"><span>Mageia</span></a> <span class="lsep">|</span> <span class="subh">Support</span></h1></div>
+        <div id="bd" role="main">
+            <div class="yui-g">
+                <div class="para donate">
+                    <div style="float: right; border: 1px solid #aaa; padding: 0 0 1em 0; margin: 0 0 2em 1em;">
+                        <ul>
+                            <li><a href="#comm">Community Support</a></li>
+                            <li><a href="#hw">Hardware Certification</a></li>
+                            <li><a href="#pro">Professional Support</a></li>
+                        </ul>
+                    </div>
+                    <h2 id="comm">Community Support</h2>
+                    <p>If you need help, information or directions about the Mageia distribution
+                        you installed or about the project, you can try to reach us through:</p>
+                    <ul class="hl">
+                        <li><a href="http://forums.mageia.org/en/">Community support forums</a> in English
+                            &ndash; see <a href="https://forums.mageia.org/en/viewforum.php?f=20">other
+                                Mageia community forums</a> for support in your language;</li>
+                        <li>IRC channels where you can discuss live with other Mageia users and contributors:
+                            <ul>
+                                <li><a href="irc://irc.freenode.net/#mageia" hreflang="en">#mageia</a>,</li>
+                                <li>or other localized <a href="http://mageia.org/wiki/doku.php?id=irc#channels_in_other_languages">channels</a>;</li>
+                            </ul>
+                        </li>
+                        <li>our <a href="http://mageia.org/wiki/">Wiki</a> (still rough at the time, we're working on it!).</li>
+                        <li>(to be done: mailing-lists, documentation, local events info)</li>
+                    </ul>
+
+                    <hr />
+
+                    <h2 id="hw">Hardware Certification</h2>
+                    <p>Mageia software runs on most of today's available computer systems.</p>
+
+                    <p>TODO</p>
+
+                    <hr />
+
+                    <h2 id="pro">Professional Support</h2>
+                    <p>Mageia.Org does not provide or endorse commercial/professional
+                        support or other services around the distribution.
+                        But there are organizations providing such services and more.</p>
+
+                    <ul class="hl">
+                        <li><p>name/link (based in city/country)
+                            &ndash; manages ?-speaking projects.<br />
+                            Provides ...</p>
+                            </li>
+                    </ul>
+
+                    <p>Please note that Mageia.Org is independant from these companies
+                        and does not endorse any responsability for the service
+                        provided by these.</p>
+
+                    <p>You might be interest in checking out our
+                        <em>Professional Guide to Mageia</em> as well (to be done).</p>
+
+                    <p>If you do provide professional services based on Mageia.Org software and would like
+                        to be listed above, please contact us at ....</p>
+                    <p>If you plan to build/provide such professional services as well, contact us too,
+                        we can help!</p>
+
+                    <hr />
+
+                </div>
+            </div>
+        </div>
+    </div>
+</body>
+</html>
