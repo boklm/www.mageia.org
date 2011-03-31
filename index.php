ed8a89fc3a357233e37d269824d3cb0ee05ed6bf
Romain d'Alverny <rda@mageia.org>
2011-03-31 10:04:15 +0000

    lang redirection changes
    git-svn-id: svn+ssh://svn.mageia.org/svn/web/www/trunk@319 ecb8f77e-9c09-4d67-9c5c-b96076beb801

--- index.php
+++ index.php
@@ -1,5 +1,6 @@
 <?php
 /**
+ *
 */
 
 require_once 'langs.inc.php';
@@ -22,11 +23,4 @@ if (isset($_GET['flang']))
     die;
 }
 
-require_once 'localeDetection.class.php';
-
-$locale = new ChooseLocale(array_keys($langs));
-
-$locale->setDefaultLocale('en');
-
-header(sprintf('Location: /%s/', $locale->getCompatibleLocale()));
-die;
+relocate($langs);
--- langs.inc.php
+++ langs.inc.php
@@ -22,3 +22,27 @@ $langs = array(
     'zh-cn' => '简体中文',
     'zh-tw' => '正體中文'
 );
+
+/**
+ * Redirect to a localized path.
+ *
+ * @param array $langs list of languages
+ * @param string $page optional path to which we want to redirect
+ * @param string $default_locale
+ *
+ * @return void
+*/
+function relocate($langs, $page = '', $default_locale = 'en')
+{
+    require_once 'localeDetection.class.php';
+
+    $locale = new ChooseLocale(array_keys($langs));
+
+    $locale->setDefaultLocale($default_locale);
+
+    header(sprintf('Location: /%s/%s',
+        $locale->getCompatibleLocale(),
+        $page
+    ));
+    die;    
+}
