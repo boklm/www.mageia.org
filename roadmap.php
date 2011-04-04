dd4562feac6b687f337f76eee46ca81532087789
Romain d'Alverny <rda@mageia.org>
2011-04-04 14:37:41 +0000

    solutions linux event
    git-svn-id: svn+ssh://svn.mageia.org/svn/web/www/trunk@324 ecb8f77e-9c09-4d67-9c5c-b96076beb801

--- roadmap.php
+++ roadmap.php
@@ -15,5 +15,10 @@ H;
 ?>
 </li>
 <li>Beta 1 in the works</li>
+<li>We will attend <a href="http://www.solutionslinux.fr/preinscription.html?code=partenariat">Solutions Linux</a>
+    in Paris<br />
+    <a href="http://www.solutionslinux.fr/preinscription.html?code=partenariat">
+<img src="http://www.solutionslinux.fr/upload/image/
+Solutions%20Linux%202011/Kit%20M%C3%A9dia/Logo%20Linux%202011.gif" title="Solutions Linux, 10-11-12 mai 2011" /></a></li>
 </ul>
 <p><a href="http://mageia.org/wiki/doku.php?id=roadmap">Roadmap</a></p>
