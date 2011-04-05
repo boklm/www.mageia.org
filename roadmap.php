bb85a60d93fbdb75ffaa0d5b8f52cbf91ae72655
Romain d'Alverny <rda@mageia.org>
2011-04-05 15:59:36 +0000

    roadmap update
    git-svn-id: svn+ssh://svn.mageia.org/svn/web/www/trunk@355 ecb8f77e-9c09-4d67-9c5c-b96076beb801

--- roadmap.php
+++ roadmap.php
@@ -6,19 +6,20 @@ $roadmap = array(
 );
 
 ?>
-<ul id="rightnow">
-<li>Alpha 2 is now available! 
-<?php
-echo <<<H
-<a href="/{$locale}/alpha/">Get it</a>, test it and send us your feedback!
-H;
-?>
-</li>
-<li>Beta 1 in the works</li>
-<li>We will attend <a href="http://www.solutionslinux.fr/preinscription.html?code=partenariat">Solutions Linux</a>
-    in Paris<br />
-    <a href="http://www.solutionslinux.fr/preinscription.html?code=partenariat">
-<img src="http://www.solutionslinux.fr/upload/image/
+<ul id="rightnow" class="hl">
+<li><strong><a href="/downloads/">Beta 1 is now available!</a></strong></li>
+<?php /*
+<li>Next events:
+    <ul>
+        <li><a href="http://lanyrd.com/2011/webworkerscamp3/">WebWorkersCamp Paris</a> (April 15-16)</a>
+        <li><a href="http://www.solutionslinux.fr/preinscription.html?code=partenariat">Solutions Linux</a>
+            in Paris<br />
+            <a href="http://www.solutionslinux.fr/preinscription.html?code=partenariat">
+                <img src="http://www.solutionslinux.fr/upload/image/
 Solutions%20Linux%202011/Kit%20M%C3%A9dia/Logo%20Linux%202011.gif" title="Solutions Linux, 10-11-12 mai 2011" /></a></li>
+        <li><a href="http://www.linuxtag.org/2011/">LinuxTag</a> in Berlin (May 12-14)</li>
+    </ul>
+</li>
+*/ ?>
 </ul>
 <p><a href="http://mageia.org/wiki/doku.php?id=roadmap">Roadmap</a></p>
