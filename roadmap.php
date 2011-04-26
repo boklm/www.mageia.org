513e84f0e0dbf03eeb944e4ff21febb13fcf3b2e
Romain d'Alverny <rda@mageia.org>
2011-04-26 14:12:54 +0000

    update
    git-svn-id: svn+ssh://svn.mageia.org/svn/web/www/trunk@388 ecb8f77e-9c09-4d67-9c5c-b96076beb801

--- roadmap.php
+++ roadmap.php
@@ -7,11 +7,9 @@ $roadmap = array(
 
 ?>
 <ul id="rightnow" class="hl">
-<li><strong><a href="/downloads/">Beta 1 is now available!</a></strong></li>
-<?php /*
+<li><strong><a href="/downloads/">Beta 2 is now available!</a></strong></li>
 <li>Next events:
     <ul>
-        <li><a href="http://lanyrd.com/2011/webworkerscamp3/">WebWorkersCamp Paris</a> (April 15-16)</a>
         <li><a href="http://www.solutionslinux.fr/preinscription.html?code=partenariat">Solutions Linux</a>
             in Paris<br />
             <a href="http://www.solutionslinux.fr/preinscription.html?code=partenariat">
@@ -20,6 +18,5 @@ Solutions%20Linux%202011/Kit%20M%C3%A9dia/Logo%20Linux%202011.gif" title="Soluti
         <li><a href="http://www.linuxtag.org/2011/">LinuxTag</a> in Berlin (May 12-14)</li>
     </ul>
 </li>
-*/ ?>
 </ul>
 <p><a href="http://mageia.org/wiki/doku.php?id=roadmap">Roadmap</a></p>
