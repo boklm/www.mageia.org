5b1571091dd25399ef70cbf74971bb356f33601c
Romain d'Alverny <rda@mageia.org>
2011-05-17 22:39:06 +0000

    roadmap
    git-svn-id: svn+ssh://svn.mageia.org/svn/web/www/trunk@430 ecb8f77e-9c09-4d67-9c5c-b96076beb801

--- roadmap.php
+++ roadmap.php
@@ -7,16 +7,7 @@ $roadmap = array(
 
 ?>
 <ul id="rightnow" class="hl">
-<li><strong><a href="/downloads/">Beta 2 is now available!</a></strong></li>
-<li>Next events:
-    <ul>
-        <li><a href="http://www.solutionslinux.fr/preinscription.html?code=partenariat">Solutions Linux</a>
-            in Paris<br />
-            <a href="http://www.solutionslinux.fr/preinscription.html?code=partenariat">
-                <img src="http://www.solutionslinux.fr/upload/image/
-Solutions%20Linux%202011/Kit%20M%C3%A9dia/Logo%20Linux%202011.gif" title="Solutions Linux, 10-11-12 mai 2011" /></a></li>
-        <li><a href="http://www.linuxtag.org/2011/">LinuxTag</a> in Berlin (May 12-14)</li>
-    </ul>
-</li>
+<li><strong><a href="/downloads/">RC is now available!</a></strong></li>
+<li>Finale coming on June, 1<sup>st</sup>!</li>
 </ul>
 <p><a href="http://mageia.org/wiki/doku.php?id=roadmap">Roadmap</a></p>
