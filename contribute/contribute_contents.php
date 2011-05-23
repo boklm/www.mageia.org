f4d832c9972a9ead5a299cba7836a83cdcd841bf
Romain d'Alverny <rda@mageia.org>
2011-05-23 12:49:27 +0000

    rewrite (English draft) contents
    git-svn-id: svn+ssh://svn.mageia.org/svn/web/www/trunk@441 ecb8f77e-9c09-4d67-9c5c-b96076beb801

--- contribute/contribute_contents.php
+++ contribute/contribute_contents.php
@@ -1,140 +1,125 @@
     <div id="doc" class="yui-t7">
-		<a name="top" />
+        <a name="top" />
         <div id="hd" role="banner"><h1><a id="logo" href="/"><span>Mageia</span></a> <span class="lsep">|</span> <span class="subh"><?php echo $_t['page_h1']; ?></span></h1></div>
         <div id="bd" role="main">
             <div class="yui-g">
-				<div class="para">
-					<h2><?php echo $_t['h2_general']; ?></h2>
-					<p>
-						<?php echo $_t['contribute_general_1']; ?><br />
-						<?php echo $_t['contribute_general_2']; ?><br />
-						<?php echo $_t['contribute_general_3']; ?><br />
-						<?php echo $_t['contribute_general_4']; ?><br />&nbsp;<br />
-						<span style="font-weight:bold;"><?php echo $_t['contribute_general_remark']; ?></span>
-					</p>
-					<h2><?php echo $_t['choose_profile']; ?></h2>
-					<p><ul>
-						<li><a href="#documenter"><?php echo $_t['profile_documenter']; ?></a></li>
-						<li><a href="#graphical"><?php echo $_t['profile_graphical']; ?></a></li>
-						<li><a href="#ambassador"><?php echo $_t['profile_ambassador']; ?></a></li>
-						<li><a href="#technical"><?php echo $_t['profile_technical']; ?></a></li>
-						<li><a href="#translator"><?php echo $_t['profile_translator']; ?></a></li>
-						<li><a href="#web"><?php echo $_t['profile_web']; ?></a></li>
-						<li><a href="#donator"><?php echo $_t['profile_donator']; ?></a></li>
-					</ul></p>
-					<a name="documenter">
-						<h3><?php echo $_t['profile_documenter']; ?></h3>
-					</a>
-					<p>
-						<span style="font-weight: bold;"><?php echo $_t['contribute_role']; ?> : </span>
-						<?php echo $_t['document_role']; ?><br />&nbsp;<br />
-						<span style="font-weight: bold;"><?php echo $_t['contribute_skills']; ?> :</span><br />
-						<ul><?php foreach ( $_t['document_skills'] as $item ) {
-								echo '<li>'.$item.'</li>';
-							} ?></ul><br />
-						<span style="font-weight: bold;"><?php echo $_t['contribute_examples']; ?> :</span><br />
-						<ul><?php foreach ( $_t['document_example'] as $item ) {
-								echo '<li>'.$item.'</li>';
-							}?></ul><br />
-						<a href="#top"><?php echo $_t['ancor_top']; ?></a>
-					</p>
-					<a name="graphical">
-						<h3><?php echo $_t['profile_graphical']; ?></h3>
-					</a>
-					<p>
-						<span style="font-weight: bold;"><?php echo $_t['contribute_role']; ?> : </span>
-						<?php echo $_t['graphical_role']; ?><br />&nbsp;<br />
-						<span style="font-weight: bold;"><?php echo $_t['contribute_skills']; ?> :</span><br />
-						<ul><?php foreach ( $_t['graphical_skills'] as $item ) {
-								echo '<li>'.$item.'</li>';
-							} ?></ul><br />
-						<span style="font-weight: bold;"><?php echo $_t['contribute_examples']; ?> :</span><br />
-						<ul><?php foreach ( $_t['graphical_example'] as $item ) {
-								echo '<li>'.$item.'</li>';
-							} ?></ul><br />
-						<a href="#top"><?php echo $_t['ancor_top']; ?></a>
-					</p>
-					<a name="ambassador">
-						<h3><?php echo $_t['profile_ambassador']; ?></h3>
-					</a>
-					<p>
-						<span style="font-weight: bold;"><?php echo $_t['contribute_role']; ?> : </span>
-						<?php echo $_t['ambassador_role']; ?><br />&nbsp;<br />
-						<span style="font-weight: bold;"><?php echo $_t['contribute_skills']; ?> :</span><br />
-						<ul><?php foreach ( $_t['ambassador_skills'] as $item ) {
-								echo '<li>'.$item.'</li>';
-							} ?></ul><br />
-						<span style="font-weight: bold;"><?php echo $_t['contribute_examples']; ?> :</span><br />
-						<ul><?php foreach ( $_t['ambassador_example'] as $item ) {
-								echo '<li>'.$item.'</li>';
-							} ?></ul><br />
-						<a href="#top"><?php echo $_t['ancor_top']; ?></a>
-					</p>
-					<a name="technical">
-						<h3><?php echo $_t['profile_technical']; ?></h3>
-					</a>
-					<p>
-						<span style="font-weight: bold;"><?php echo $_t['contribute_role']; ?> : </span>
-						<?php echo $_t['technical_role']; ?><br />&nbsp;<br />
-						<span style="font-weight: bold;"><?php echo $_t['contribute_skills']; ?> :</span><br />
-						<ul><?php foreach ( $_t['technical_skills'] as $item ) {
-								echo '<li>'.$item.'</li>';
-							} ?></ul><br />
-						<span style="font-weight: bold;"><?php echo $_t['contribute_examples']; ?> :</span><br />
-						<ul><?php foreach ( $_t['technical_example'] as $item ) {
-								echo '<li>'.$item.'</li>';
-							} ?></ul><br />
-						<a href="#top"><?php echo $_t['ancor_top']; ?></a>
-					</p>
-					<a name="translator">
-						<h3><?php echo $_t['profile_translator']; ?></h3>
-					</a>
-					<p>
-						<span style="font-weight: bold;"><?php echo $_t['contribute_role']; ?> : </span>
-						<?php echo $_t['translator_role']; ?><br />&nbsp;<br />
-						<span style="font-weight: bold;"><?php echo $_t['contribute_skills']; ?> :</span><br />
-						<ul><?php foreach ( $_t['translator_skills'] as $item ) {
-								echo '<li>'.$item.'</li>';
-							} ?></ul><br />
-						<span style="font-weight: bold;"><?php echo $_t['contribute_examples']; ?> :</span><br />
-						<ul><?php foreach ( $_t['translator_example'] as $item ) {
-								echo '<li>'.$item.'</li>';
-							} ?></ul><br />
-						<a href="#top"><?php echo $_t['ancor_top']; ?></a>
-					</p>
-					<a name="web">
-						<h3><?php echo $_t['profile_web']; ?></h3>
-					</a>
-					<p>
-						<span style="font-weight: bold;"><?php echo $_t['contribute_role']; ?> : </span>
-						<?php echo $_t['web_role']; ?><br />&nbsp;<br />
-						<span style="font-weight: bold;"><?php echo $_t['contribute_skills']; ?> :</span><br />
-						<ul><?php foreach ( $_t['web_skills'] as $item ) {
-								echo '<li>'.$item.'</li>';
-							} ?></ul><br />
-						<span style="font-weight: bold;"><?php echo $_t['contribute_examples']; ?> :</span><br />
-						<ul><?php foreach ( $_t['web_example'] as $item ) {
-								echo '<li>'.$item.'</li>';
-							} ?></ul><br />
-						<a href="#top"><?php echo $_t['ancor_top']; ?></a>
-					</p>
-					<a name="donator">
-						<h3><?php echo $_t['profile_donator']; ?></h3>
-					</a>
-					<p>
-						<span style="font-weight: bold;"><?php echo $_t['contribute_role']; ?> : </span>
-						<?php echo $_t['donator_role']; ?><br />&nbsp;<br />
-						<span style="font-weight: bold;"><?php echo $_t['contribute_skills']; ?> :</span><br />
-						<ul><?php foreach ( $_t['donator_skills'] as $item ) {
-								echo '<li>'.$item.'</li>';
-							} ?></ul><br />
-						<span style="font-weight: bold;"><?php echo $_t['contribute_examples']; ?> :</span><br />
-						<ul><?php foreach ( $_t['donator_example'] as $item ) {
-								echo '<li>'.$item.'</li>';
-							} ?></ul><br />
-						<a href="#top"><?php echo $_t['ancor_top']; ?></a>
-					</p>
-				</div>
+                <div class="para">
+                    <p>Diverse people from around the world gather to build Mageia
+                        &ndash; a Linux-based operating system <em>and</em>
+                        a <a href="/en/about/code-of-conduct/">lively, fun community</a>
+                        for <a href="/en/about/values/">building Free Software projects</a>.</p>
+
+                    <p>Contributing is open to everyone, this is Free Software!
+                        If you are curious and willing to join,
+                        there are things you can do,
+                        depending on your time and your skills,
+                        You will always find someone to welcome you and help/mentor
+                        you if needed so you bring your best to the project!</p>
+
+                    <p>Check what you could do below!</p>
+
+                    <ul id="contrib-areas">
+                        <li id="support">
+                            <h2>Helping users &amp; advocating the project</h2>
+                            <p>Want to welcome and help new users or share tips with experienced ones?
+                                In <a href="/wiki/doku.php?id=irc">IRC channels</a>,
+                                <a href="http://forums.mageia.org/">forums</a>,
+                                <a href="http://mageia.org/mailman/">mailing-lists</a>,
+                                local events and places? Just get in touch with us there and share the fun!</p>
+                        </li>
+                        <li id="docu">
+                            <h2><a href="/wiki/doku.php?id=documentation">Writing, copywriting and documenting</a></h2>
+                            <p>You have a taste for practical, clear, concise, proofread, nice writing?
+                                You like to take the challenge of explaining clearly
+                                complex ideas or systems and teach others?
+                                You know how to mix form &amp; content to push the right message?
+                                Get in touch with our <a href="/wiki/doku.php?id=documentation">Documentation team</a>!</p>
+                        </li>
+                        <li id="i18n">
+                            <h2><a href="/wiki/doku.php?id=translators">Translating</a></h2>
+                            <p>Mageia is localized in more than 180 languages!
+                                Clarifying, completing, improving
+                                translations of software, guides, tutorials, Web sites, marketing material,
+                                etc. happens thanks to the effort of so many persons.
+                                Join <a href="/wiki/doku.php?id=translators">them</a>!</p>
+                        </li>
+                        <li id="triage">
+                            <h2><a href="/wiki/doku.php?id=triage">Triaging</a></h2>
+                            <p>Bugs happen! Some get reported. And need to be triaged before reaching
+                                the person that can fix them: validated, informed, properly assigned.
+                                <a href="/wiki/doku.php?id=triage">Grow the Triage team</a> and be the link between users who report bugs
+                                in the forums or mailing-lists, and <a href="http://bugs.mageia.org/">Mageia Bugzilla</a>
+                                used by developers.</p>
+                        </li>
+                        <li id="qa">
+                            <h2><a href="/wiki/doku.php?id=qateam">Testing &amp; <abbr title="Quality Assurance">QA</abbr></a></h2>
+                            <p>We can't ship software if we are not confident enough in it!
+                                <a href="/wiki/doku.php?id=qateam">Testers and QA</a>
+                                people make sure what we do (software, packages, ISO, Web sites)
+                                match our expectations for quality before they reach most users.</p>
+                        </li>
+                        <li id="marketing">
+                            <h2>Marketing, Communication &amp; Evangelism</h2>
+                            <p>Better understanding who use and contribute to the project
+                                to help them even more, making sure the Mageia voice is consistent and heard,
+                                that's a job for the <a href="http://mageia.org/wiki/doku.php?id=marketing">marcomm' team</a>
+                                (Marketing and Communication), on global and local scales.</p>
+                        </li>
+                        <li id="graphic">
+                            <h2>Graphic &amp; UI design</h2>
+                            <p>Software isn't only about code and Mageia about technology.
+                                So make it human, practical and beautiful!
+                                If you have a talent and experience in graphic design, ergonomics
+                                <a href="/wiki/doku.php?id=designers">join the artwork team</a>!</p>
+                        </li>
+                        <li id="dev">
+                            <h2><a href="/wiki/doku.php?id=packagers">Coding &amp; packaging</a></h2>
+                            <p>Contribute to the core of the distribution with your technical skills!
+                                Adding, fixing, patching and maintaining software to be included
+                                in the distribution, from upstream projects or from Mageia-specific
+                                sources. Join the <a href="/wiki/doku.php?id=packagers">Packagers team</a>!</p>
+                        </li>
+                        <li id="web">
+                            <h2>Web, tools, systems design &amp; administration</h2>
+                            <p>Mageia exists through an infrastructure and tools that enable everyone
+                                to collaborate. These need experts to build, maintain, develop, provide,
+                                manage servers, connections, security, applications, user flow, etc.
+                                It takes from <a href="/wiki/doku.php?id=sysadmin">system administrators</a>
+                                to <a href="/wiki/doku.php?id=web">Web designers/developers/integrators</a>.</p>
+                        </li>
+                        <li id="mirror">
+                            <h2>Mirroring</h2>
+                            <p>Making all Mageia software available requires <a href="http://mirrors.mageia.org/">several mirrors
+                                all around the world</a>, to distribute ISO and installation or update packages.
+                                If you have some space and bandwidth to share, please <a href="/wiki/doku.php?id=mirrors_policy#how_to_become_a_mirror">see how you can
+                                become an official Mageia mirror</a>.</p>
+                        </li>
+                        <li id="donation">
+                            <h2><a href="/en/donate/">Donating</a></h2>
+                            <p>Financial donations help us allocate specific tasks, secure our infrastructure,
+                                fund events, goodies &amp; transportation.
+                                <a href="/en/thank-you/">200+ persons already expressed us their trust</a>
+                                with their money, hardware or other resources.
+                                We keep a <a href="/en/about/reports/">public record of what we get and
+                                    how we use it</a>.</p>
+                        </li>
+                        <hr>
+                        <li id="data">
+                            <h2>Data mining</h2>
+                            <p>There are tons of data out there, most of which we don't use,
+                                or even know about. If you like to grab and analyze data
+                                to reveal &amp; visualize them and what that means we can do better,
+                                get in touch!</p>
+                        </li>
+                        <li id="design">
+                            <h2>Designing, experimenting, revealing the unknown</h2>
+                            <p>Ideas are great, actionable prototypes are even better.
+                                The Mageia project is not about only making a different Linux
+                                distribution but as well about building new products and experiences
+                                with it and the data around it.</p>
+                        </li>
+                    </ul>
+                </div>
             </div>
         </div>
     </div>
