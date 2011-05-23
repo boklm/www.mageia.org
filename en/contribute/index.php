655596322fe5fb1c60b0083c90f347d549f7b631
Oliver Burger <obgr_seneca@mageia.org>
2011-05-23 20:22:47 +0000

    merged php files in contribute, removed LOCALE/contribute dirs for replacing by symlinks to en
    git-svn-id: svn+ssh://svn.mageia.org/svn/web/www/trunk@445 ecb8f77e-9c09-4d67-9c5c-b96076beb801

--- en/contribute/index.php
+++ en/contribute/index.php
@@ -1,19 +1,3 @@
+<?xml version="1.0" encoding="utf-8"?>
 <?php require '../../contribute/contribute.php'; ?>
-<!DOCTYPE html>
-<html lang="<?php echo $locale; ?>">
-<head>
-    <meta charset="utf-8" />
-    <title><?php echo $_t['page_title']; ?></title>
-    <meta name="description" content="<?php echo $_t['page_desc']; ?>" />
-    <meta name="keywords" content="<?php echo $_t['page_kw']; ?>" />
-    <meta name="author" content="Mageia" />
-    <link rel="stylesheet" type="text/css" href="/g/style/all.css" >
-    <?php # include '../../analytics.php'; ?>
-</head>
-<body>
-    <?php include '../../langs.php'; ?>
-    
-	<?php include '../../contribute/contribute_contents.php'; ?>
 
-</body>
-</html>
