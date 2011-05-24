34a7abfd3b4699a1c7bc6f117639279023a43415
Romain d'Alverny <rda@mageia.org>
2011-05-24 14:13:13 +0000

    new migrate page (still blank) for mgaupdate info
    git-svn-id: svn+ssh://svn.mageia.org/svn/web/www/trunk@451 ecb8f77e-9c09-4d67-9c5c-b96076beb801

---
+++ migrate/index.php
@@ -0,0 +1,27 @@
+<?php
+
+require_once '../langs.inc.php';
+
+$langs = array(
+//    'de' => 'Deutsch',
+//    'el' => 'Ελληνικά',
+    'en' => 'English',
+//    'es' => 'Español',
+//    'et' => 'Eesti',
+//    'fi' => 'Suomeksi',
+//    'fr' => 'Français',
+//    'it' => 'Italiano',
+//    'lv' => 'Latviešu',
+//    'nb' => 'Bokmål',
+//    'nl' => 'Nederlands',
+//    'pl' => 'Polski',
+//    'pt' => 'Português',
+//    'ro' => 'Română',
+//    'ru' => 'Русский',
+//    'tr' => 'Türkçe',
+//    'sl' => 'Slovenščina',
+//    'zh-cn' => '简体中文',
+//    'zh-tw' => '正體中文'
+);
+
+relocate($langs, 'migrate/');
