9dc033e61eb9f7352b6d4a648f490e45becfe28b
Romain d'Alverny <rda@mageia.org>
2011-05-29 22:27:23 +0000

    more elements/links - will need some reorg
    git-svn-id: svn+ssh://svn.mageia.org/svn/web/www/trunk@487 ecb8f77e-9c09-4d67-9c5c-b96076beb801

--- en/about/index.php
+++ en/about/index.php
@@ -18,14 +18,25 @@
         <div id="bd" role="main">
             <div class="yui-g"><div class="para">
 
+                <h2>Current activity</h2>
                 <ul>
+                    <li><a href="../donate/">Donations</a></li>
+                    <li><a href="/en/calendar/">Calendar</a></li>
+                    <li><a href="http://forums.mageia.org/">Forums</a>,
+                        <a href="/mailman/">mailing-lists</a>,
+                        <a href="/wiki/">Wiki</a></li>
+                    <li><a href="http://svnweb.mageia.org/">Code repository</a>,
+                        dashboard</li>
+                </ul>
+                <h2>Learn more about Mageia</h2>
+                <ul>
+                    <li><a href="2010-sept-announcement.html">Original announcement in Sept. 2010</a></li>
                     <li><a href="/wiki/doku.php?id=org">Governance</a></li>
+                    <li><a href="./values/">Values</a></li>
                     <li><a href="./code-of-conduct/">Code of conduct</a></li>
                     <li><a href="./policies/">Policies</a></li>
-                    <li><a href="./values/">Values</a></li>
                     <li>Financial reports: <a href="./reports/2010/">2010</a>,
                         <a href="./reports/2011/">2011</a></li>
-                    <li><a href="../donate/">Donations</a></li>
                     <li><a href="../thank-you/">Thanks!</a></li>
                     <li><a href="./policies/privacy/">Privacy policy</a></li>
                 </ul>
