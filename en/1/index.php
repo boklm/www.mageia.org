8b4a94848690b1e72456f870432bb82fe8a1a98f
Romain d'Alverny <rda@mageia.org>
2011-05-30 20:40:27 +0000

    update (trish)
    git-svn-id: svn+ssh://svn.mageia.org/svn/web/www/trunk@509 ecb8f77e-9c09-4d67-9c5c-b96076beb801

--- en/1/index.php
+++ en/1/index.php
@@ -22,39 +22,18 @@
 
                     <h2 class="download-btn"><a href="/downloads/">Download it</a></h2>
 
-                    <p>Mageia 1 is a friendly Linux-based distribution for your computer.</p>
+                    <p>Mageia 1 is a GNU/Linux distribution for your computer,
+                        released by the Mageia community.</p>
 
                     <h3>What is in it?</h3>
-                    <p>Lots. See the <a href="notes/">release notes</a> for an extensive exposé.</p>
+                    <p>Plenty! See the <a href="notes/">release notes</a> for an extensive exposé.</p>
 
                     <div class="clear"></div>
-                    
-                    <p><a href="/en/about/2010-sept-announcement.html">9&nbsp;months</a>.</p>
-                    <p><a href="">100+&nbsp;contributors</a> in <a href="/wiki/doku.php?id#teams">10+ teams</a>.
-                        <a href="/en/thank-you/">160+&nbsp;generous donators</a> for
-                        <a href="/en/donate/">8.800+&nbsp;EUR</a>
-                        with only <a href="/en/about/reports/">1.277 spent</a> so far
-                        and 20.000 forecasted for 2011.</p>
-                    <p>5&nbsp;intermediary technical releases.
-                        7.389&nbsp;source packages,
-                        2 &times; 14.226&nbsp;binary packages for 2&nbsp;architectures (i586 and x86_64
-                        &ndash; ARM is on its way).</p>
-                        
-                    <p>7&nbsp;servers, 2&nbsp;datacenters (Paris and Marseille, France),
-                        <a href="http://mirrors.mageia.org/">19&nbsp;mirrors worldwide</a>.</p>
-                    <p><a href="http://mageia.org/?langs">21&nbsp;languages</a> localized Website,
-                        <a href="">161&nbsp;languages</a> localized distribution.</p>
-                    <p>All this for <a href="http://mageia.org/en/1/">one distribution</a>,
-                        ready for you, now.</p>
-                    <p>How many users? Time will tell.</p>
-                    
-                    <p>But more important than vanity figures: <a href="/en/1/next/">what will it do for you?</a></p>
-                    
-                    <hr>
-                
+
                     <h3>Mageia in context</h3>
 
-                    <p>Mageia started as a fork of Mandriva Linux in September 2010.
+                    <p>Mageia began in September 2010 as
+                        <a href="/en/about/2010-sept-announcement.html">a fork of Mandriva Linux</a>.
                         It is supported by a <a href="/en/about/">not-for-profit organisation</a>,
                         <a href="http://mageia.org/wiki/doku.php?id=org">governed by</a>
                         a body of recognized and elected contributors</a>,
@@ -67,8 +46,31 @@
                         a <a href="">developer</a>,
                         or a <a href="">business</a>.</p>
 
-                    <p>And, on your own time or through your professional activity,
-                        <a href="/en/contribute/">you can be part of it</a>.</p>
+                    <p>And we encourage you to <a href="/en/contribute/">be part of it</a>,
+                        on your own time or through your professional activity.</p>
+
+                    <hr>
+
+                    <p>9&nbsp;months from <a href="/en/about/2010-sept-announcement.html">inception</a> we've reached:</p>
+                    <p><a href="">100+&nbsp;contributors</a> in <a href="/wiki/doku.php?id#teams">10+ teams</a>.
+                        <a href="/en/thank-you/">160+&nbsp;generous donors</a> of
+                        <a href="/en/donate/">8.800+&nbsp;EUR</a>
+                        with only <a href="/en/about/reports/">1.277 spent</a> so far
+                        and 20.000 forecast for 2011.</p>
+                    <p>5&nbsp;intermediary technical releases.
+                        7.389&nbsp;source packages,
+                        2 &times; 14.226&nbsp;binary packages for 2&nbsp;architectures (i586 and x86_64
+                        &ndash; ARM is on its way).</p>
+                    <p>7&nbsp;servers, 3&nbsp;datacenters (Paris and Marseille, France),
+                        <a href="http://mirrors.mageia.org/">19&nbsp;mirrors worldwide</a>.</p>
+                    <p><a href="http://mageia.org/?langs">21-language</a> localized Website,
+                        <a href="">161-languages</a> localized distribution.</p>
+                    <p>All this for <a href="http://mageia.org/en/1/">one distribution</a>,
+                        ready for you, now.</p>
+                    <p>How many users? Time will tell.</p>
+                    
+                    <p>But more important than vanity figures: <a href="/en/1/next/">what will it do for you?</a></p>
+                    
                 </div>
             </div>
             <div class="yui-g bb1">
