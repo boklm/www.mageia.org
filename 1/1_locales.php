8617e9af681e9c5d0fbc89c1adb85a38263e8a81
Oliver Burger <obgr_seneca@mageia.org>
2011-05-31 17:40:37 +0000

    localised 1/
    git-svn-id: svn+ssh://svn.mageia.org/svn/web/www/trunk@528 ecb8f77e-9c09-4d67-9c5c-b96076beb801

---
+++ 1/1_locales.php
@@ -0,0 +1,47 @@
+<?php
+
+$_t = array(
+    'en' => array(
+        '1_title' => 'Mageia 1',
+        '1_desc' => 'Mageia 1 is a new community-based Linux distribution.',
+        '1_kw' => 'mageia, 1, first release, linux, mandriva, free software',
+        'img_desktop' => 'Mageia 1 KDE Desktop',
+        'download_it' => 'Download it',
+        'mageia_is' => 'Mageia 1 is a GNU/Linux distribution for your computer,
+                        released by the Mageia community.',
+        'h3_what_is' => 'What is in it?',
+        'plenty_see_notes' => 'Plenty! See the <a href="notes/">release notes</a> for an extensive exposé.',
+        'h3_context' => 'Mageia in context',
+        'mageia_began' => 'Mageia began in September 2010 as
+                        <a href="/en/about/2010-sept-announcement.html">a fork of Mandriva Linux</a>.
+                        It is supported by a <a href="/en/about/">not-for-profit organisation</a>,
+                        <a href="http://mageia.org/wiki/doku.php?id=org">governed by</a>
+                        a body of recognized and elected contributors</a>,
+                        and made by 100+ people around the world.',
+        'our_work' => 'Our work adds to the excellent work of the wider Linux 
+                        and Free Software community. We aim to bring one of the best,
+                        most stable, reliable and enjoyable experience and platform we can make;
+                        for a regular user, a developer, or a business.',
+        'encourage_you' => 'And we encourage you to <a href="/en/contribute/">be part of it</a>,
+                        on your own time or through your professional activity.',
+        '9_months' => '9&nbsp;months from <a href="/en/about/2010-sept-announcement.html">inception</a> we\'ve reached:',
+        'people' => '<a href="">100+&nbsp;contributors</a> in <a href="/wiki/doku.php?id#teams">10+ teams</a>.
+                        <a href="/en/thank-you/">160+&nbsp;generous donors</a> of
+                        <a href="/en/donate/">8.800+&nbsp;EUR</a>
+                        with only <a href="/en/about/reports/">1.277 spent</a> so far
+                        and 20.000 forecast for 2011.',
+        'prereleases' => '5&nbsp;intermediary technical releases.
+                        7.389&nbsp;source packages,
+                        2 &times; 14.226&nbsp;binary packages for 2&nbsp;architectures (i586 and x86_64
+                        &ndash; ARM is on its way).',
+        'servers' => '7&nbsp;servers, 3&nbsp;datacenters (Paris and Marseille, France),
+                        <a href="http://mirrors.mageia.org/">19&nbsp;mirrors worldwide</a>.',
+        'languages' => '<a href="http://mageia.org/?langs">21-language</a> localized Website,
+                        <a href="">161-languages</a> localized distribution.',
+        'distribution' => 'All this for <a href="http://mageia.org/en/1/">one distribution</a>,
+                        ready for you, now.',
+        'users' => 'How many users? Time will tell.',
+        'in_for_you' => 'But more important than vanity figures: <a href="/en/1/next/">what will it do for you?</a>'
+    )
+);
+?>
