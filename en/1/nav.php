c6702dab94ecd31f0f010d81df4853494e06f2a3
Romain d'Alverny <rda@mageia.org>
2011-06-01 16:37:53 +0000

    l10ned nav
    git-svn-id: svn+ssh://svn.mageia.org/svn/web/www/trunk@588 ecb8f77e-9c09-4d67-9c5c-b96076beb801

--- en/1/nav.php
+++ en/1/nav.php
@@ -1,11 +1,21 @@
 <?php
 
+$_nt = array(
+    'en' => array('for-you' => 'For You', 'dl' => 'Download', 'next' => 'And Next?', 'notes' => 'Notes'),
+    'fr' => array('for-you' => 'Pour Vous', 'dl' => 'Télécharger', 'next' => 'Et après&nbsp;?')
+);
+
+if (array_key_exists($locale, $_nt))
+    $_nt = array_merge($_nt['en'], $_nt[$locale]);
+else
+    $_nt = $_nt['en'];
+
 $nav = array(
     "/{$locale}/1/"         => '1',
-    "/{$locale}/1/for-you/" => 'For You',
-    "/{$locale}/downloads/" => 'Download',
-    "/{$locale}/1/next/"    => 'And next?',
-    "/{$locale}/1/notes/"   => 'Notes'
+    "/{$locale}/1/for-you/" => $_nt['for-you'],
+    "/{$locale}/downloads/" => $_nt['dl'],
+    "/{$locale}/1/next/"    => $_nt['next'],
+    "/{$locale}/1/notes/"   => $_nt['notes']
 );
 
 $sru = trim($_SERVER['REQUEST_URI']);
