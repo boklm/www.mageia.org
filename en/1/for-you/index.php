f812d2e8be3e658e96b1a30215bc1e82e0f407a7
Romain d'Alverny <rda@mageia.org>
2011-06-01 13:11:46 +0000

    small fixes + add sysadmin category
    git-svn-id: svn+ssh://svn.mageia.org/svn/web/www/trunk@564 ecb8f77e-9c09-4d67-9c5c-b96076beb801

--- en/1/for-you/index.php
+++ en/1/for-you/index.php
@@ -15,50 +15,6 @@ $_t = array(
     <meta name="keywords" content="<?php echo $_t['page_kw']; ?>">
     <meta name="author" content="Mageia">
     <link rel="stylesheet" type="text/css" href="/g/style/all.css">
-    <style>
-        .product-list {
-            padding: 0;
-            margin: 0;
-        }
-        .product-list > li {
-            list-style: none;
-            padding-left: 70px;
-            min-height: 60px;
-            background-image: url(/g/images/smi/vcard-48x48.png);
-            background-repeat: no-repeat;
-            background-position: 0 4px;
-            margin-bottom: 2em;
-        }
-        .product-list li h3 {
-            font-size: 140%;
-            margin: 0;
-            padding: 5px;
-            color: #000;
-            font-weight: bold;
-        }
-        .product-list li h3 a {
-            color: #000;
-        }
-
-        .product-list li#marketing { 
-            background-image: url(/g/images/smi/feed-48x48.png); 
-        }
-        .product-list li#web, .contrib-list li#mirror { 
-            background-image: url(/g/images/smi/website-48x48.png); 
-        }
-        .product-list li#triage { 
-            background-image: url(/g/images/smi/feed-48x48.png); 
-        }
-        .product-list li#triage { 
-            background-image: url(/g/images/smi/feed-48x48.png); 
-        }
-        .product-act li { 
-            margin-bottom: 0.6em; 
-        }
-        .product-act { 
-            margin-bottom: 2.5em; 
-        }
-    </style>
     <?php include '../../../analytics.php'; ?>
 </head>
 <body class="release">
@@ -88,26 +44,20 @@ $_t = array(
                 <hr />
                 <h2>Mageia is focusing on...</h2>
                     <ul class="product-list">
-                        <li><h3><a href="./user/">...users</a></h3>
+                        <li><h3><s>Users</s> People!</h3>
                             <p>Run Mageia inside or beside your existing Operation system, or use a LiveCD to try Mageia without changing your system.</p>
-                            <hr />
                             <p>Easy installation - choose from a standard list of things to do, or choose individual packages. Have your fully-working system up and running really fast! Choose from a range of desktops - make your computing environment just the way you like it.</p>
-                            <hr />
                             <p>Mageia is safe, stable and secure by default. Automatic updates and an on-by-default firewall keep it that way.</p>
-                            <hr />
                             <p>Lots of Productive Desktop applications - Web browsing, email, Office suites, Graphics management and editing, audio and video players, CD and DVD burning, games and more - we've got your software needs covered.</p>
-                            <hr />
                             <p>All the system administration tools at your fingertips - control your system yourself, or use our simple tools. New users can feel safe with the default settings.</p>
-                            <hr />
                             <p>Support from the community - join our mailing lists and forums, or talk to people online using IRC.</p>
+                            <hr>
                             </li>
 
-                        <li><h3><a href="./developer/">...developers</a></h3>
+                        <li><h3>Developers!</h3>
                             <p>We're following the UNIX tradition of letting people have fun with their computer by programming - Mageia 1 is fully loaded with various popular compilers and interpreters</p>
                             <p>While our schedule didn't permit us to offer everything we planned, we tried very hard to cover most needs.</p>
-                            <hr />
-                            <p>
-                            Mageia provides the most used compilers, libraries and interpreters like
+                            <p>Mageia provides the most used compilers, libraries and interpreters like:</p>
                             <ul>
                                 <li>gcc 4.5.2</li>
                                 <li>glibc 2.12.1</li>
@@ -120,9 +70,7 @@ $_t = array(
                                 <li>java openjdk 1.6.0.0</li>
                                 <li>java sun 1.6.0.25</li>
                             </ul>
-                            </p>
-                            <hr />
-                            <p>Along with those basic development tools Mageia is providing several IDEs for making teh developer's life easier.
+                            <p>Along with those basic development tools Mageia is providing several IDEs for making teh developer's life easier.</p>
                             <ul>
                                 <li>eclipse 3.6.2</li>
                                 <li>netbeans 6.9</li>
@@ -132,9 +80,12 @@ $_t = array(
                             </ul>
                             </p>
                             <p>We're ready to welcome your feedback and contributions to improve this for next release - check the contact links, and contact us with questions and suggestions.</p>
-                            <hr />
+                            <hr>
                             </li>
-                        <li><h3><a href="./contributor/">...contributors</a></h3>
+                        <li><h3>Sysadmins!</h3>
+                            <hr>
+                            </li>
+                        <li><h3><a href="./contributor/">Contributors!</a></h3>
                             <p>Get more information <a href="./contributor/">here</a>.</p></li>
                     </ul>    
                 </div>
