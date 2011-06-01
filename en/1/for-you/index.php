0f3191bae9dc957ad352c67b7f39167894599a3c
Oliver Burger <obgr_seneca@mageia.org>
2011-06-01 19:18:23 +0000

    internationalized for-you
    git-svn-id: svn+ssh://svn.mageia.org/svn/web/www/trunk@611 ecb8f77e-9c09-4d67-9c5c-b96076beb801

--- en/1/for-you/index.php
+++ en/1/for-you/index.php
@@ -1,12 +1,17 @@
 <?php
-$_t = array(
-    'page_title' => 'Mageia 1 is for you!',
-    'page_kw' => 'mageia, everybody, user, developer, contributor, hacker, student, teacher',
-    'page_h1' => 'For You!'
-);
 
+$locale = explode('/', $_SERVER['REQUEST_URI']);
+$locale = $locale[1];
 
-?><!DOCTYPE html>
+require 'locales.php';
+if (!array_key_exists($locale, $_t)) {
+    $locale = 'en';
+}
+
+$_t = $_t[$locale];
+
+?>
+<!DOCTYPE html>
 <html lang="<?php echo $locale; ?>">
 <head>
     <meta charset="utf-8">
@@ -26,67 +31,58 @@ $_t = array(
             <div class="yui-g">
                 <?php include '../nav.php'; ?>
                 <div class="para">
-                <h2>Mageia is for...</h2>
+                <h2><?php echo $_t['h2_for']; ?></h2>
                     <ul class="product-list">
-                        <li><h3>...the daily work!</h3>
-                            <p>Providing office suites, development environments, database tools,...</p></li>
-                        <li><h3>...fun and entertainment!</h3>
-                            <p>Providing games, audio and video players, PVRs,...</p></li>
-                        <li><h3>...actuality and stability!</h3>
-                            <p>Not sacrificing stability for the single purpose of being cutting edge, but staying actual enough, providing recent stable releases of applications...</p></li>
-                        <li><h3>...universal usage!</h3>
-                            <p>Providing a large scale of applications, mirroring the different needs of the community building the distribution</p></li>
-                        <li><h3>...the community!</h3>
-                            <p>Mageia is a distribution built by the community for the community, maing it easy and comfortable for everyone to contribute to the great work!</p></li>
-                        <li><h3>...people!</h3>
-                            <p>Mageia is not only a technical project. Mageia is a living community consisting of people from all over the world. So the distribution is made for the people using it.</p></li>
+                        <li><h3><?php echo $_t['h3_daily']; ?></h3>
+                            <p><?php echo $_t['p_daily']; ?></p></li>
+                        <li><h3><?php echo $_t['h3_fun']; ?></h3>
+                            <p><?php echo $_t['p_fun']; ?></p></li>
+                        <li><h3><?php echo $_t['h3_actuality']; ?></h3>
+                            <p><?php echo $_t['p_actuality']; ?></p></li>
+                        <li><h3><?php echo $_t['h3_universal']; ?></h3>
+                            <p><?php echo $_t['p_universal']; ?></p></li>
+                        <li><h3><?php echo $_t['h3_community']; ?></h3>
+                            <p><?php echo $_t['p_community']; ?></p></li>
+                        <li><h3><?php echo $_t['h3_people']; ?></h3>
+                            <p><?php echo $_t['p_people']; ?></p></li>
                     </ul>
                 <hr />
-                <h2>Mageia is focusing on...</h2>
+                <h2><?php echo $_t['h2_focusing']; ?></h2>
                     <ul class="product-list">
-                        <li><h3><s>Users</s> People!</h3>
-                            <p>Run Mageia inside or beside your existing Operation system, or use a LiveCD to try Mageia without changing your system.</p>
-                            <p>Easy installation - choose from a standard list of things to do, or choose individual packages. Have your fully-working system up and running really fast! Choose from a range of desktops - make your computing environment just the way you like it.</p>
-                            <p>Mageia is safe, stable and secure by default. Automatic updates and an on-by-default firewall keep it that way.</p>
-                            <p>Lots of Productive Desktop applications - Web browsing, email, Office suites, Graphics management and editing, audio and video players, CD and DVD burning, games and more - we've got your software needs covered.</p>
-                            <p>All the system administration tools at your fingertips - control your system yourself, or use our simple tools. New users can feel safe with the default settings.</p>
-                            <p>Support from the community - join our mailing lists and forums, or talk to people online using IRC.</p>
+                        <li><h3><?php echo $_t['h3_users']; ?></h3>
+                            <p><?php echo $_t['p_users_1']; ?></p>
+                            <p><?php echo $_t['p_users_2']; ?></p>
+                            <p><?php echo $_t['p_users_3']; ?></p>
+                            <p><?php echo $_t['p_users_4']; ?></p>
+                            <p><?php echo $_t['p_users_5']; ?></p>
+                            <p><?php echo $_t['p_users_6']; ?></p>
                             <hr>
                             </li>
 
-                        <li><h3>Developers!</h3>
-                            <p>We're following the UNIX tradition of letting people have fun with their computer by programming - Mageia 1 is fully loaded with various popular compilers and interpreters</p>
-                            <p>While our schedule didn't permit us to offer everything we planned, we tried very hard to cover most needs.</p>
-                            <p>Mageia provides the most used compilers, libraries and interpreters like:</p>
+                        <li><h3><?php echo $_t['h3_developers']; ?></h3>
+                            <p><?php echo $_t['p_developers_1']; ?></p>
+                            <p><?php echo $_t['p_developers_2']; ?></p>
+                            <p><?php echo $_t['p_developers_3']; ?></p>
                             <ul>
-                                <li>gcc 4.5.2</li>
-                                <li>glibc 2.12.1</li>
-                                <li>gtk+ 2.24.4</li>
-                                <li>qt 4.7.3</li>
-                                <li>perl 5.12.3</li>
-                                <li>python 2.7.1</li>
-                                <li>ruby 1.8.7</li>
-                                <li>php 5.3.6</li>
-                                <li>java openjdk 1.6.0.0</li>
-                                <li>java sun 1.6.0.25</li>
+                                <?php foreach ( $_t['array_developers_1'] as $anItem ) {
+                                    echo '<li>'.$anItem.'</li>';
+                                } ?>
                             </ul>
-                            <p>Along with those basic development tools Mageia is providing several IDEs for making teh developer's life easier.</p>
+                            <p><?php echo $_t['p_developers_4']; ?></p>
                             <ul>
-                                <li>eclipse 3.6.2</li>
-                                <li>netbeans 6.9</li>
-                                <li>anjuta 2.32.1.1</li>
-                                <li>kdevelop 4.2.2</li>
-                                <li>padre 0.840.0</li>
+                                <?php foreach ( $_t['array_developers_2'] as $anItem ) {
+                                    echo '<li>'.$anItem.'</li>';
+                                } ?>
                             </ul>
                             </p>
-                            <p>We're ready to welcome your feedback and contributions to improve this for next release - check the contact links, and contact us with questions and suggestions.</p>
+                            <p><?php echo $_t['p_developers_5']; ?></p>
                             <hr>
                             </li>
-                        <li><h3>Sysadmins!</h3>
+                        <li><h3><?php echo $_t['h3_sysadmins']; ?></h3>
                             <hr>
                             </li>
-                        <li><h3><a href="./contributor/">Contributors!</a></h3>
-                            <p>Get more information <a href="./contributor/">here</a>.</p></li>
+                        <li><h3><a href="./contributor/"><?php echo $_t['h3_contributors']; ?></a></h3>
+                            <p><?php echo $_t['p_contributors']; ?></p></li>
                     </ul>    
                 </div>
             </div>
