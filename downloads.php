be7a319214238a8257df51c3d44e4e64ac6eb787
Romain d'Alverny <rda@mageia.org>
2011-06-01 17:53:24 +0000

    new mirrors countries
    git-svn-id: svn+ssh://svn.mageia.org/svn/web/www/trunk@606 ecb8f77e-9c09-4d67-9c5c-b96076beb801

--- downloads.php
+++ downloads.php
@@ -15,6 +15,7 @@ $_t = array_merge($_t['en'], $_t[$locale]);
 $prods = parse_ini_file('downloads.ini', true);
 $countries = array(
     'BR' => 'Brasil',
+    'CA' => 'Canada',
     'CH' => 'Switzerland',
     'CN' => 'China',
     'CZ' => 'Czechia',
