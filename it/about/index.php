3fdb287e0efbed34830ad695875bb8431b790667
Romain d'Alverny <rda@mageia.org>
2011-06-21 21:42:10 +0000

    about it (marcello)
    git-svn-id: svn+ssh://svn.mageia.org/svn/web/www/trunk@732 ecb8f77e-9c09-4d67-9c5c-b96076beb801

---
+++ it/about/index.php
@@ -0,0 +1,46 @@
+<!DOCTYPE html>
+<html dir="ltr" lang="it">
+<head>
+    <meta charset="utf-8">
+    <title>About Mageia</title>
+    <meta name="description" content="Mageia">
+    <meta name="keywords" content="mageia">
+    <meta name="author" content="Mageia">
+    <link rel="stylesheet" type="text/css" href="/g/style/all.css">
+    <link rel="icon" type="image/png" href="/g/favicon.png">
+    <?php include '../../analytics.php'; ?>
+</head>
+<body>
+    <?php include '../../langs.php'; ?>
+    <div id="doc" class="yui-t7">
+        <div id="hd" role="banner"><h1><a id="logo" href="/it/"><span>Mageia</span></a> <span class="lsep">|</span> <span class="subh">About</span></h1></div>
+        <div id="bd" role="main">
+            <div class="yui-g"><div class="para">
+
+                <h2>Attività</h2>
+                <ul>
+                    <li><a href="/it/donate/">Donazioni</a></li>
+                    <li><a href="/it/calendar/">Calendario</a></li>
+                    <li><a href="http://forums.mageia.org/">Forum</a>,
+                        <a href="/mailman/">Mailing-list</a>,
+                        <a href="/wiki/">Wiki</a></li>
+                    <li><a href="http://svnweb.mageia.org/">Deposito del codice</a>,
+                        dashboard</li>
+                </ul>
+                <h2>Scopri di più riguardo a Mageia</h2>
+                <ul>
+                    <li><a href="/it/about/2010-sept-announcement.html">Annuncio Originale fatto a Settembre 2010</a></li>
+                    <li><a href="/wiki/doku.php?id=org">Modello di governo</a></li>
+                    <li><a href="/it/about/values/">Valori</a></li>
+                    <li><a href="/en/about/code-of-conduct/">Codice di condotta</a></li>
+                    <li><a href="/en/about/policies/">Politiche</a></li>
+                    <li>Report finanziari: <a href="/en/about/reports/2010/">2010</a>,
+                        <a href="/en/about/reports/2011/">2011</a></li>
+                    <li><a href="/en/thank-you/">Grazie!</a></li>
+                    <li><a href="/en/about/policies/privacy/">Politiche relative alla privacy</a></li>
+                </ul>
+            </div></div>
+        </div>
+    </div>
+
+</body></html>
