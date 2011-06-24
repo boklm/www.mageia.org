2f9c8bfe7ec608f5c4e3a117f3f205f6e7f09945
Romain d'Alverny <rda@mageia.org>
2011-06-24 08:41:41 +0000

    quick media page
    git-svn-id: svn+ssh://svn.mageia.org/svn/web/www/trunk@740 ecb8f77e-9c09-4d67-9c5c-b96076beb801

---
+++ en/about/media/index.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html dir="ltr" lang="en">
+<head>
+    <meta charset="utf-8">
+    <title>Mageia media files</title>
+    <meta name="description" content="Mageia">
+    <meta name="keywords" content="mageia">
+    <meta name="author" content="Mageia">
+    <link rel="stylesheet" type="text/css" href="/g/style/all.css">
+    <?php include '../../../analytics.php'; ?>
+</head>
+<body>
+    <?php include '../../../langs.php'; ?>
+    
+    <div id="doc" class="yui-t7">
+        <div id="hd" role="banner"><h1><a id="logo" href="/"><span>Mageia</span></a> <span class="lsep">|</span> <span class="subh">Media files</span></h1></div>
+        <div id="bd" role="main">
+            <div class="yui-g"><div class="para">
+                <h2>Logo</h2>
+                <p><a href="/g/media/logo/mageia-2011.png"><img src="/g/media/logo/t-mageia-2011.png"><br>PNG</a> or
+                        <a href="/g/media/logo/mageia-2011.svg">SVG</a>.</p>
+
+                <hr>
+
+                <h2>Mageia 1 CD/DVD cover</h2>
+                <p><a href="/g/media/covers/print-1.png"><img src="/g/media/covers/t-print-1.png" alt=""></a>
+                    <a href="/g/media/covers/print-2.png"><img src="/g/media/covers/t-print-2.png" alt=""></a>
+                    <a href="/g/media/covers/print-3.png"><img src="/g/media/covers/t-print-3.png" alt=""></a></p>
+            </div></div>
+        </div>
+    </div>
+</body>
+</html>
