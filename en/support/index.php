28150958a6ea591ba8d96aa2f7b4400f28b709d3
Romain d'Alverny <rda@mageia.org>
2011-06-30 15:10:36 +0000

    templatize support
    git-svn-id: svn+ssh://svn.mageia.org/svn/web/www/trunk@767 ecb8f77e-9c09-4d67-9c5c-b96076beb801

--- en/support/index.php
+++ en/support/index.php
@@ -1,79 +1,60 @@
-<!DOCTYPE html>
-<html dir="ltr" lang="en">
+<?php
+
+define('HLANG', true);
+require '../../langs.php';
+include 'locales.php';
+
+$_t = i18n::get_strings($_t, $locale, $i18n_fallback_rules);
+
+?><!DOCTYPE html>
+<html dir="ltr" lang="<?php echo $locale; ?>">
 <head>
     <meta charset="utf-8">
-    <title>Support for Mageia distribution</title>
-    <meta name="description" content="List of support resources for Mageia distribution.">
-    <meta name="keywords" content="mageia, linux, support, help, hardware, qa, test">
+    <title><?php echo $_t['page_title']; ?></title>
+    <meta name="description" content="<?php echo $_t['page_desc']; ?>">
+    <meta name="keywords" content="<?php echo $_t['page_kw']; ?>">
     <meta name="author" content="Mageia">
-    <link rel="stylesheet" type="text/css" href="/g/style/all.css" >
+    <link rel="stylesheet" type="text/css" href="/g/style/all.css">
     <?php include '../../analytics.php'; ?>
 </head>
 <body>
-    <?php include '../../langs.php'; ?>
-    
+    <?php echo $hsnav; ?>
+
     <div id="doc" class="yui-t7">
-        <div id="hd" role="banner"><h1><a id="logo" href="/en/"><span>Mageia</span></a> <span class="lsep">|</span> <span class="subh">Support</span></h1></div>
+        <div id="hd" role="banner"><h1><a id="logo" href="/<?php echo $locale; ?>/"><span>Mageia</span></a> <span class="lsep">|</span> <span class="subh"><?php echo $_t['h1_title']; ?></span></h1></div>
         <div id="bd" role="main">
             <div class="yui-g">
                 <div class="para donate">
                     <div class="qsidesummary">
                         <ul>
-                            <li><a href="#comm">Community Support</a></li>
-                            <li><a href="#pro">Professional Support</a></li>
-                            <li><a href="#hw">Hardware Requirements</a></li>
+                            <li><a href="#comm"><?php echo $_t['h2_comm_support']; ?></a></li>
+                            <li><a href="#pro"><?php echo $_t['h2_pro_support']; ?></a></li>
+                            <li><a href="#hw"><?php echo $_t['h2_hw_reqs']; ?></a></li>
                         </ul>
                     </div>
-                    <h2 id="comm">Community Support</h2>
-                    <p>If you need help, information or directions about the Mageia distribution
-                        you installed or about the project, you can try to reach us through:</p>
-                    <ul class="hl">
-                        <li><a href="http://forums.mageia.org/en/">Community support forums</a> in English
-                            &ndash; see <a href="https://forums.mageia.org/en/viewforum.php?f=20">other
-                                Mageia community forums</a> for support in your language;</li>
-                        <li>IRC channels where you can discuss live with other Mageia users and contributors:
-                            <a href="irc://irc.freenode.net/#mageia" hreflang="en">#mageia</a>
-                            or other localized <a href="http://mageia.org/wiki/doku.php?id=irc#channels_in_other_languages">channels</a>;
-                        </li>
-                        <li>our <a href="http://mageia.org/wiki/">Wiki</a> (still rough at the time, we're working on it!);</li>
-                        <li>our <a href="http://mageia.org/mailman/" hreflang="en">mailing-lists</a>;</li>
-                        <li>local events: follow our <a href="http://blog.mageia.org/en/">blog</a>
-                            and <a href="http://mageia.org/en/calendar/">calendar</a>.</li>
-                    </ul>
+                    <h2 id="comm"><?php echo $_t['h2_comm_support']; ?></h2>
+                    <p><?php echo $_t['comm_1']; ?></p>
+                    <ul class="hl"><?php foreach ($_t['comm_list'] as $commi):
+                        echo sprintf('<li>%s</li>', $commi);
+                    endforeach; ?></ul>
+                    <hr>
 
-                    <hr />
+                    <h2 id="pro"><?php echo $_t['h2_pro_support']; ?></h2>
+                    <p><?php echo $_t['pro_1']; ?></p>
+                    <p><?php echo sprintf($_t['pro_2'], '/wiki/doku.php?id=commercial_vendors'); ?></p>
+                    <hr>
 
-                    <h2 id="pro">Professional Support</h2>
-                    <p>Mageia.Org does not provide or endorse commercial/professional
-                        support or other services around the distribution.
-                        But there are organizations providing such services and more.</p>
-                    <p>Please check the <a href="/wiki/doku.php?id=commercial_vendors">commercial vendors</a> list.</p>
+                    <h2 id="hw"><?php echo $_t['h2_hw_reqs']; ?></h2>
+                    <p><?php echo $_t['hw_1']; ?></p>
 
-                    <hr />
+                    <p><?php echo $_t['hw_2']; ?></p>
+                    <ul class="hl"><?php foreach ($_t['hw_list'] as $hwi):
+                        echo sprintf('<li>%s</li>', $hwi);
+                    endforeach; ?></ul>
+                    <p><?php echo $_t['hw_some_hardware']; ?></p>
 
-                    <h2 id="hw">Hardware Requirements</h2>
-                    <p>Mageia software runs on most x86 computer systems available as of today, April 2011.</p>
-
-                    <p>You can follow this hardware requirements list:</p>
-                    <ul class="hl">
-                        <li>Processor: any AMD, Intel or VIA processor;</li>
-                        <li>Memory (RAM): 512MB minimum, 2GB recommended;</li>
-                        <li>Storage (HDD): 1GB for a minimal installation, 6GB for a full setup;</li>
-                        <li>Optical drive: CD or DVD depending on the ISO you use (network, USB key installation available);</li>
-                        <li>Graphic card: any ATI, Intel, Matrox, nVidia, SiS or VIA graphic card;</li>
-                        <li>Sound card: any AC97, HDA or Sound Blaster sound card.</li>
-                    </ul>
-                    <p>For some kinds of hardware (Wi-Fi chipsets, 3D graphic cards) to work properly,
-                        specific firmware or software may be needed, this is available in a specific
-                        online repository called "nonfree" (learn more).</p>
-
-                    <p class="dlinfo">We may set up a hardware compatibility list/directory
-                        as well as a hardware certification process; but that's
-                        still at the planning stage. Feel free to join/contact our
-                        <a href="/wiki/doku.php?id=web">Web</a> and <a href="/wiki/doku.php?id=qateam">QA</a> teams
-                        if you would like to help in this regard.</p>
-
-                    <hr />
+                    <p class="dlinfo"><?php echo sprintf($_t['hw_hcl_note'], '/wiki/doku.php?id=web', '/wiki/doku.php?id=qateam'); ?></p>
+                    <hr>
                 </div>
             </div>
         </div>
---
+++ en/support/locales.php
@@ -0,0 +1,107 @@
+<?php
+
+$_t = array(
+    'en' => array(
+        'page_title' => 'Support for Mageia distribution',
+        'page_desc' => 'List of support resources for Mageia distribution.',
+        'page_kw' => 'mageia, linux, support, help, hardware, qa, test',
+        'h1_title' => 'Support',
+        'h2_comm_support' => 'Community Support',
+        'h2_pro_support' => 'Professional Support',
+        'h2_hw_reqs' => 'Hardware Requirements',
+        
+        'comm_1' => 'If you need help, information or directions about the Mageia distribution
+            you installed or about the project, you can try to reach us through:',
+        'comm_list' => array(
+            '<a href="http://forums.mageia.org/en/">Community support forums</a> in English
+                &ndash; see <a href="https://forums.mageia.org/en/viewforum.php?f=20">other
+                Mageia community forums</a> for support in your language;',
+            'IRC channels where you can discuss live with other Mageia users and contributors:
+                <a href="irc://irc.freenode.net/#mageia" hreflang="en">#mageia</a>
+                or other localized <a href="http://mageia.org/wiki/doku.php?id=irc#channels_in_other_languages">channels</a>;',
+            'our <a href="http://mageia.org/wiki/">Wiki</a> (still rough at the time, we are working on it!);',
+            'our <a href="http://mageia.org/mailman/" hreflang="en">mailing-lists</a>;',
+            'local events: follow our <a href="http://blog.mageia.org/en/">blog</a>
+                and <a href="http://mageia.org/en/calendar/">calendar</a>.',
+        ),
+
+        'pro_1' => 'Mageia.Org does not provide or endorse commercial/professional
+            support or other services around the distribution.
+            But there are organizations providing such services and more.',
+        'pro_2' => 'Please check the <a href="%s">commercial vendors</a> list.',
+
+        'hw_1' => 'Mageia software runs on most x86 computer systems available as of today, April 2011.',
+        'hw_2' => 'You can follow this hardware requirements list:',
+        'hw_list' => array(
+            'Processor: any AMD, Intel or VIA processor;',
+            'Memory (RAM): 512MB minimum, 2GB recommended;',
+            'Storage (HDD): 1GB for a minimal installation, 6GB for a full setup;',
+            'Optical drive: CD or DVD depending on the ISO you use (network, USB key installation available);',
+            'Graphic card: any ATI, Intel, Matrox, nVidia, SiS or VIA graphic card;',
+            'Sound card: any AC97, HDA or Sound Blaster sound card.'
+        ),
+        'hw_some_hardware' => 'For some kinds of hardware (Wi-Fi chipsets, 3D graphic cards) to work properly,
+            specific firmware or software may be needed, this is available in a specific
+            online repository called "nonfree" (learn more).',
+
+        'hw_hcl_note' => 'We may set up a hardware compatibility list/directory
+            as well as a hardware certification process; but that\'s
+            still at the planning stage. Feel free to join/contact our
+            <a href="%s">Web</a> and <a href="%s">QA</a> teams
+            if you would like to help in this regard.'
+    ),
+    'fr' => array(
+        'page_title' => 'Support et Assisance pour Mageia',
+        'page_desc' => 'Obtenir du support pour Mageia via la communauté ou des professionnels',
+        'page_kw' => 'mageia, linux, support, aide, assistance, matériel, services, entreprise, administration, hardware, qa, test',
+        'h1_title' => 'Assistance',
+        'h2_comm_support' => 'Par la communauté',
+        'h2_pro_support' => 'Par des professionnels',
+        'h2_hw_reqs' => 'Configuration requise',
+        
+        'comm_1' => 'Si vous avez besoin d&rsquo;aide ou d&rsquo;information sur Mageia,
+            après ou avant l&rsquo;installation, ou sur le projet, vous pouvez nous
+            rejoindre (utilisateurs et contributeurs au projet) via&nbsp;:',
+        'comm_list' => array(
+            'le <a href="http://forums.mageia.org/fr/">forum communautaire francophone</a>
+                &ndash; il y a aussi d&rsquo; <a href="https://forums.mageia.org/">autres forums</a>
+                dans d&rsquo;autres langues&nbsp;;',
+            'le canal IRC <a href="irc://irc.freenode.net/#mageia-fr" hreflang="fr">#mageia-fr</a>
+                sur Freenode&nbsp;; là encore, il existe
+                d&rsquo;<a href="http://mageia.org/wiki/doku.php?id=irc#channels_in_other_languages">autres
+                canaux dans d&rsquo;autres langues</a>&nbsp;;',
+            'le <a href="http://mageia.org/wiki/">Wiki</a> (encore en anglais et un peu en bazar pour le moment,
+                mais on y travaille)&nbsp;;',
+            'les <a href="http://mageia.org/mailman/" hreflang="en">listes de discussion</a> (en anglais pour le moment)&nbsp;;',
+            'des événements locaux&nbsp;; surveillez le <a href="http://blog.mageia.org/fr/">blog</a>
+                et le <a href="http://mageia.org/en/calendar/">calendrier</a>.',
+        ),
+
+        'pro_1' => 'L&rsquo;association Mageia.Org ne propose pas de service commercial (assistance, formation, autre)
+                autour de la distribution&nbsp;; ni aux particuliers, ni aux professionnels.
+                Par contre, des sociétés peuvent fournir de tels services.',
+        'pro_2' => 'Vous pouvez les trouver et les contacter à partir de notre
+                <a href="%s" hreflang="en">liste de prestataires commerciaux</a> sur le Wiki (en anglais).',
+
+        'hw_1' => 'Mageia fonctionne avec la plupart des ordinateurs x86 (32 et 64 bits) disponibles à ce jour (juin 2011).',
+        'hw_2' => 'You can follow this hardware requirements list:',
+        'hw_list' => array(
+            'Processeur&nbsp;: n&rsquo;importe quel modèle AMD, Intel ou VIA&nbsp;;',
+            'Mémoire vive&nbsp;: 512&nbsp;Mo minimum, 2&nbsp;Go recommandé&nbsp;;',
+            'Disque dur&nbsp;: 1&nbsp;Go pour une installation a minima, 6&nbsp;Go pour une complète&nbsp;;',
+            'Lecteur optique&nbsp;: CD ou DVD selon l&rsquo;ISO que vous choisissez 
+                (vous pouvez aussi installer via le réseau ou avec une clé USB);',
+            'Carte vidéo&nbsp;: n&rsquo;importe ATI, Intel, Matrox, nVidia, SiS ou VIA&nbsp;;',
+            'Carte son&nbsp;: n&rsquo;importe AC97, HDA ou Sound Blaster.'
+        ),
+        'hw_some_hardware' => 'Certains composants (cartes Wi-Fi, cartes vidéo 3D) nécessitent
+            des pilotes logiciels spécifiques, non-libres, pour fonctionner correctement&nbsp;;
+            ils sont disponibles dans une librairie logicielle nommée "nonfree".',
+
+        'hw_hcl_note' => 'Nous allons peut-être monter une base de compatibilité 
+            et certification matérielles, ainsi qu&rsquo;un process de certification.
+            Mais ça n&rsquo;est encore qu&rsquo;un projet &ndash;
+            si cela vous intéresse et que vous pensez pouvoir y contribuer,
+            n&rsquo;hésitez pas à contacter les équipes <a href="%s">Web</a> et <a href="%s">QA</a>.'
+    ),
+);
