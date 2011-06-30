ff3f0e31df1501ce994f3e25f00829748e9d3896
José Melo <ze@mageia.org>
2011-06-30 19:41:39 +0000

    pt translation
    git-svn-id: svn+ssh://svn.mageia.org/svn/web/www/trunk@770 ecb8f77e-9c09-4d67-9c5c-b96076beb801

--- en/support/report-a-bug/index.php
+++ en/support/report-a-bug/index.php
@@ -1,10 +1,10 @@
 <!DOCTYPE html>
-<html dir="ltr" lang="en">
+<html dir="ltr" lang="pt">
 <head>
     <meta charset="utf-8">
-    <title>How to report a bug | Mageia</title>
-    <meta name="description" content="How to report a bug for Mageia.">
-    <meta name="keywords" content="mageia, linux, bug, report, procedure, support, help">
+    <title>Como reportar um erro | Mageia</title>
+    <meta name="description" content="Como reportar um erro para a Mageia.">
+    <meta name="keywords" content="mageia, linux, bug, erro, reportar, procedimento, suporte, ajuda">
     <meta name="author" content="Mageia">
     <link rel="stylesheet" type="text/css" href="/g/style/all.css" >
     <?php include '../../../analytics.php'; ?>
@@ -13,99 +13,99 @@
     <?php include '../../../langs.php'; ?>
     
     <div id="doc" class="yui-t7">
-        <div id="hd" role="banner"><h1><a id="logo" href="/"><span>Mageia</span></a> <span class="lsep">|</span> <span class="subh">How to Report a Bug</span></h1></div>
+        <div id="hd" role="banner"><h1><a id="logo" href="/"><span>Mageia</span></a> <span class="lsep">|</span> <span class="subh">Como Reportar um Erro</span></h1></div>
         <div id="bd" role="main">
             <div class="yui-g">
                 <div class="para donate">
                     <div class="qsidesummary">
                         <ol>
-                            <li><a href="#update">Update your system</a></li>
-                            <li><a href="#reproduce">Reproduce the bug</a></li>
-                            <li><a href="#go">Go to bugs.mageia.org</a></li>
-                            <li><a href="#search">Search for similar bugs</a></li>
-                            <li><a href="#file">File a new bug</a></li>
-                            <li><a href="#follow">Follow up</a></li>
-                            <li><a href="#thx">Thank you!</a></li>
-                            <li><a href="#more">More</a></li>
+                            <li><a href="#update">Actualizar o seu sistema</a></li>
+                            <li><a href="#reproduce">Reproduzir o erro</a></li>
+                            <li><a href="#go">Ir para bugs.mageia.org</a></li>
+                            <li><a href="#search">Procurar erros similares</a></li>
+                            <li><a href="#file">Preencher um novo erro</a></li>
+                            <li><a href="#follow">Dar seguimento</a></li>
+                            <li><a href="#thx">Obrigado!</a></li>
+                            <li><a href="#more">Mais</a></li>
                         </ol>
                     </div>
                     
-                    <h2 id="update">1. Update your system.</h2>
-                    <p>Make sure your system is up-to-date, 
-                        and double-check if you still encounter the same bug.</p>
-                    <p>If not, that means that an installed update may have fixed the bug
-                        &ndash; you may stop here.</p>
+                    <h2 id="update">1. Actualizar o seu sistema.</h2>
+                    <p>Certifique-se que o seu sistema está actualizado, 
+                        e volte a verificar se ainda encontra o mesmo erro.</p>
+                    <p>Se não encontrar, significa que a instalação de uma actualização corrigiu o erro
+                        &ndash; pode parar aqui.</p>
                     
                     <hr />
-                    <h2 id="reproduce">2. Reproduce the bug.</h2>
-                    <p>Make sure you can reproduce the bug, and how you do it.</p>
-                    <p>If not, it will be difficult for developers to identify and fix this bug.
-                        But if you still think this is important enough to be reported, continue.</p>
+                    <h2 id="reproduce">2. Reproduzir o erro.</h2>
+                    <p>Certifique-se que pode reproduzir o erro, e como o faz.</p>
+                    <p>Se não conseguir, será difícil para os desenvolvedores identificar e corrigir o erro.
+                        Mas se ainda pensa que este erro é importante o suficiente para ser reportado, continue.</p>
 
                     <hr />
-                    <h2 id="go">3. Go to <a href="https://bugs.mageia.org/">bugs.mageia.org</a>.</h2>
-                    <p>&hellip; and sign in with <a href="https://identity.mageia.org/">your Mageia account</a>.
-                        If you do not have an account yet,
-                        you can <a href="https://identity.mageia.org/register">register one</a>.</p>
+                    <h2 id="go">3. Ir para <a href="https://bugs.mageia.org/">bugs.mageia.org</a>.</h2>
+                    <p>&hellip; e autentique-se na sua <a href="https://identity.mageia.org/">conta Mageia</a>.
+                        Se ainda não tem uma conta,
+                        pode <a href="https://identity.mageia.org/register">registar uma</a>.</p>
                     
                     <hr />
-                    <h2 id="search">4. Search for a similar bug.</h2>
-                    <p>This bug may have been reported already. So search for it using the search box.</p>
-                    <p>If there is one such similar bug:</p>
+                    <h2 id="search">4. Procurar um erro similar.</h2>
+                    <p>Este erro pode já ter sido reportado. Como tal procure-o usando o campo de pesquisa.</p>
+                    <p>Se houver um erro similar:</p>
                     <ul>
-                        <li>has it been resolved already? does the proposed resolution work for you?</li>
-                        <li>is it still pending resolution? then try to see if
-                            you can add relevant information into the comments
-                            section of the bug (bottom of the page).</li>
-                        <li>do you want to be notified of updates relative to this bug? then add yourself in the Cc list.</li>
+                        <li>já foi resolvido? a resolução proposta funciona para si?</li>
+                        <li>a resolução ainda está pendente? então tente verificar se
+                            pode adicionar informações relevantes na secção de
+                            comentários do erro (no fundo da página).</li>
+                        <li>deseja ser notificado acerca de actualizações relativas ao erro? então adicione-se no campo Cc.</li>
                     </ul>
-                    <p>If you found no similar bug, or if the resolution doesn't work for you, you may continue.</p>
+                    <p>Se nã encontrar um erro similar, ou a resolução não funciona para si, pode continuar.</p>
 
                     <hr />
-                    <h2 id="file">5. File a new bug.</h2>
-                    <p>Choose the Product, Component, Version, Hardware Platform that best match
-                        the context of this bug. Put the name of the Source RPM of the package
-                        (see below for more details).</p>
+                    <h2 id="file">5. Preencher um novo erro.</h2>
+                    <p>Escolha o Produto, Componente, Versão, Plataforma de Material que melhor
+                       corresponde ao contexto deste erro. Indique o nome do pacote fonte RPM 
+                       (veja em baixo para mais detalhes).</p>
                     
-                    <p>Write a clear summary explaining the bug, in a few words.</p>
+                    <p>Escreva um sumário claro explicando o erro, em poucas palavras.</p>
 
-                    <p>Explain the bug in details, following the template provided to you in the report form.</p>
+                    <p>Explique o erro em detalhes, seguindo o modelo que lhe é fornecido no formulário.</p>
                     
                     
-                    <p>Attach the documents that seem relevant and informative about this bug.</p>
+                    <p>Anexe os documentos e informação que lhe pareçam relevantes acerca deste erro.</p>
 
-                    <p>Assign the bug to the packager maintainer (or let the triage team add it)</p>
+                    <p>Atribua o erro ao mantenedor do pacote (ou deixe a equipa de triagem adicioná-lo)</p>
                     
-                    <p>Post your bug.</p>
+                    <p>Publique o seu erro.</p>
                     
                     <hr />
-                    <h3>Document your bug!</h3>
-                    <p>It is crucial that you gather information about your bug:</p>
+                    <h3>Documente o seu erro!</h3>
+                    <p>É crucial que reúna informação acerca do seu erro:</p>
                     <ul>
-                        <li>what is the software involved? what is the package, source package of it?</li>
-                        <li>who is the maintainer?</li>
-                        <li>do you have a log file or a screenshot that may help demonstrate the problem?</li>
+                        <li>qual é o programa envolvido? qual é o pacote, o pacote fonte?</li>
+                        <li>quém é o mantenedor?</li>
+                        <li>tem um ficheiro de registo ou uma captura de ecrã que possa ajudar a demonstrar o problema?</li>
                     </ul>
                     <p><code class="term">$ rpm -qi package</code></p>
-                    <p>will give you a lot of informations about a package already: source package name, maintainer, etc.</p>
+                    <p>irá dar-lhe já bastante informação acerca do pacote: nome do pacote fonte, mantenedor, etc.</p>
 
                     <hr />
-                    <h2 id="follow">6. Follow up.</h2>
-                    <p>Your bug will be reviewed by the Triage team and/or the maintainer of the package,
-                        or other contributors.
-                        It will help a lot that you answer their questions so they can get a better picture
-                        of the issue at hand and fix it more quickly.</p>
-                    <p>Similarly, your bug may be silently fixed in a later release.
-                        If so, consider closing your bug on your own.</p>
+                    <h2 id="follow">6. Dar seguimento.</h2>
+                    <p>O seu erro será revisto pela equipa de Triagem e/ou pelo mantenedor do pacote,
+                        ou outros contribuidores.
+                        Irá ajudar bastante que responda às questões para que possam ter uma melhor visão
+                        do problema e que o possam corrigir rapidamente.</p>
+                    <p>Similarmente, o seu erro pode ser silenciosamente corrigido numa versão posterior.
+                        Se assim for, considere em fechar o seu erro.</p>
 
                     <hr />
-                    <h2 id="thx">7. Thank you!</h2>
-                    <p>Thanks to take the time to report to us about your experience
-                        of Mageia. This will help make it a better operating system for everyone.</p>
+                    <h2 id="thx">7. Obrigado!</h2>
+                    <p>Obrigado por ter tempo para nos reportar acerca da sua experiência na Mageia.
+                       Isto ajudará a tornar a Mageia num melhor sistema operativo para toda a gente.</p>
 
                     <hr />
-                    <h2 id="more">More.</h2>
-                    <p>For a more detailed description of <a href="http://mageia.org/wiki/doku.php?id=qa_bugzilla#how-to_report_a_bug">how to report a bug</a>.</p>
+                    <h2 id="more">Mais.</h2>
+                    <p>Para uma descrição mais detalhada de <a href="http://mageia.org/wiki/doku.php?id=qa_bugzilla#how-to_report_a_bug">como reportar um erro</a>.</p>
                 </div>
             </div>
         </div>
