0e9b135d6bf3af33c4b4d290c953382e21b865e5
José Melo <ze@mageia.org>
2011-07-03 15:04:57 +0000

    pt translation
    git-svn-id: svn+ssh://svn.mageia.org/svn/web/www/trunk@775 ecb8f77e-9c09-4d67-9c5c-b96076beb801

--- en/1/notes/locales.php
+++ en/1/notes/locales.php
@@ -6,6 +6,11 @@ $_t = array(
 		'page_desc' => 'Notes of the Mageia 1 release',
 		'page_kw' => 'mageia, release notes, Mga1',
 		'page_h1' => 'Release Notes'
+	),
+    'pt' => array(
+		'page_title' => 'Notas de Lançamento da Mageia 1',
+		'page_desc' => 'Notas de lançamento da Mageia 1',
+		'page_kw' => 'mageia, notas de lançamento, Mga1',
+		'page_h1' => 'Notas de Lançamento'
 	)
 );
-
