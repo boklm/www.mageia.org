3bb5db6083a7374bd661b0787c1ea30aa989b055
Oliver Burger <obgr_seneca@mageia.org>
2012-01-12 15:44:37 +0000

    updated Czech translation
    git-svn-id: svn+ssh://svn.mageia.org/svn/web/www/trunk@959 ecb8f77e-9c09-4d67-9c5c-b96076beb801

--- cs/donate/index.php
+++ cs/donate/index.php
@@ -1,10 +1,10 @@
 <!DOCTYPE html>
 <html dir="ltr" lang="cs">
 <head>
-    <meta charset="utf-8" />
+    <meta charset="utf-8">
     <title>Dary pro Mageiu</title>
-    <meta name="description" content="Mageia Spenden Kampagne">
-    <meta name="keywords" content="mageia, spenden, donate, donation">
+    <meta name="description" content="Mageia is new community-based Linux distribution.">
+    <meta name="keywords" content="mageia, donate, donation, donors, help">
     <meta name="author" content="Mageia">
     <link rel="stylesheet" type="text/css" href="/g/style/all.css">
     <?php include '../../analytics.php'; ?>
@@ -21,13 +21,14 @@
                         <ul>
                             <li><a href="#why">Proč dát dar?</a></li>
                             <li><a href="#status">Přehled darů</a></li>
-                            <li><a href="#d-paypal">Dary</a> (PayPal, úhrada převodem, šek)</li>
+                            <li><a href="#d-paypal">Dary</a> (PayPal, bank, check)</li>
                             <li><a href="/en/thank-you/#fund">Srdečné díky!</a></li>
                         </ul>
                     </div>
-                    <h2><a name="why">Proč dát dar?</a></h2>
+                    <h2 id="why">Proč dát dar?</h2>
                     <p>Mageia.Org je organizace, která se neorientuje na zisk, 
-                        ale spravuje distribuci Mageia. Jako taková může od společenství přijímat dary, které Mageiu podpoří v mnoha
+                        ale spravuje distribuci Mageia.
+                        Jako taková může od společenství přijímat dary, které Mageiu podpoří v mnoha
                         různých oblastech:</p>
                     <ul>
                         <li>vybavení servery a hostování serverů;</li>
@@ -38,53 +39,51 @@
                         <li>a konečně i právní poradenství;</li>
                         <li>a mnohé další.</li>
                     </ul>
+                    <hr />
+                    <h2 id="status">Kde je přehled o darech?</h2>
+		    <p>Dostali jsme mnoho darů a díky všem našim laskavým dárcům (<a href="/en/about/reports/<?php echo date('Y'); ?>">máme v současnosti částku</a> <strong><?php echo $g_amount_remain; ?></strong>)!</p>
 
-                    <h2><a name="status">Kde je přehled o darech?</a></h2>
-                    <p>Díky <a href="/en/thank-you/">všem našim dárcům</a>
-                        máme v současnosti částku
-                        <strong><?php echo $g_donate_amount; ?></strong>!
-                        (aktualizováno každý týden)</p>
-
-                    <p>Každý měsíc je o přijatých darech a jejich použití vydávána zpráva.
-                        Věříme, že je nezbytné, abychom odkrývali naše finanční aktivity.
+		    <p>Věříme, že je nezbytné, abychom odkrývali naše finanční aktivity. Více se o tom, jak jsou používány tyto obdržené fondy, dočtete ve
+			<a href="/en/about/reports/">finančních</a>.
                         Jak se dá dočíst v protokolu naší zakladatelské schůze,
                         finance Mageia.Org spravuje Damien Lallement.</p>
+                        
+                    <hr />
                 </div>
             </div>
             <div class="yui-g"><div class="para">
-                <h2><a name="d-paypal">Darování přes PayPal,</a></h2>
-                <?php include '../../paypal.inc.php'; echo html_paypal_form('cs'); ?>
+                <h2 id="d-paypal">Darování přes PayPal,</h2>
+                <?php include '../../paypal.inc.php'; echo html_paypal_form('en'); ?>
             </div></div>
             <div class="yui-g">
                 <div class="yui-u first"><div class="para" style="padding-right: 0;">
                     <h3>bankovním převodem,</h3>
-                    <p>za použití následujících údajů o kontě (IBAN-BLZ (kód BIC)):</p>
+                    <p>pomocí následujícího kódu banky (IBAN-BIC):</p>
                     <pre class="donate-coord">
-<u>V prospěch:</u>
+<u>Ve prospěch:</u>
 
 <?php echo $G_coord_assos; ?>
 
 
-<u>Bankovní konto:</u>
+<u>Bankovní účet:</u>
 
 <?php echo $G_coord_assos_bank; ?>
 
 </pre>
                 </div></div>
                 <div class="yui-u"><div class="para">
-                    <h3>nebo šekem (pouze €).</h3>
+                    <h3>nebo šekem (pouze &euro;).</h3>
                     <p>Své dary můžete posílat šekem na tuto adresu:</p>
                     <pre class="donate-coord">
-<?php echo $G_coord_assos; ?> 
+<?php echo $G_coord_assos; ?>
 </pre>
-                    <p>Použijte, prosím, "Mageia.Org" v řádku "Příkaz k úhradě...".</p>
+                    <p>Použijte "Mageia.Org" v řádku "Příkaz k úhradě...".</p>
                 </div><div class="para">
                     <p><strong>Dejte, prosím pozor na toto:</strong></p>
                     <ul>
-                        <li>Paypal si bere za každý dar malý poplatek (asi 2%),
-                            takže se k nám dostane o něco méně z toho, co zaplatíte;</li>
-                        <li>jako měnu prosíme používejte eura (€), neboť náklady za převod jdou k tíži Mageii;</li>
-                        <li>v případě, že byste chtěli darovat anonymně, potom, prosím, ke svému daru připojte nějaký odkaz.</li>
+                        <li>Paypal si bere za každý dar malý poplatek (kolem 2%), takže dostaneme o něco méně, než pošlete;</li>
+                        <li>náklady za převod jdou k tíži Mageii, proto jako měnu používejte, prosím, &euro;;</li>
+                        <li>v případě, že byste chtěli darovat anonymně, připojte, prosím, poznámku.</li>
                     </ul>
                 </div></div>
             </div>
