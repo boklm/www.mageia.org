1c3302da69213e69db3d550a9b5e30ac7b083a66
Romain d'Alverny <rda@mageia.org>
2012-02-21 19:02:02 +0000

    testing functions availability
    git-svn-id: svn+ssh://svn.mageia.org/svn/web/www/trunk@998 ecb8f77e-9c09-4d67-9c5c-b96076beb801

--- en/downloads/debug/index.php
+++ en/downloads/debug/index.php
@@ -1,6 +1,10 @@
 <?php
 
 $ips = array();
+$functions = array(
+    'geoip_country_code_by_name',
+    'geoip_continent_code_by_name'
+);
 
 if (isset($_SERVER['HTTP_X_FORWARDED_FOR'])
     && $str = $_SERVER['HTTP_X_FORWARDED_FOR'])
@@ -12,9 +16,17 @@ if (isset($_SERVER['HTTP_X_FORWARDED_FOR'])
 else
     $ips['ip'] = $_SERVER['REMOTE_ADDR'];
 
-echo '<html><body><pre>Testing IP, country, continent',
-    "\n------------------------------\n\n";
+echo '<html><body>';
 
+echo '<h3>Testing functions:</h3>';
+echo '<ul>';
+foreach ($functions as $f) {
+    echo sprintf('<li>%s: %s available</li>',
+        $f, function_exists($f) ? 'is' : 'is not');
+}
+echo '</ul>';
+
+echo '<h3>Testing IP, country, continent</h3>';
 foreach ($ips as $label => $ip) {
     echo ' * ', $label, ': ', $ip, ' => ';
     echo geoip_country_code_by_name($ip), ', ';
