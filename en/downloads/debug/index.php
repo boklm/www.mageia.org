39c69ca4dc76abc94a5d132d69e3abc1b86a1544
Romain d'Alverny <rda@mageia.org>
2012-02-21 19:13:37 +0000

    more debug output
    git-svn-id: svn+ssh://svn.mageia.org/svn/web/www/trunk@999 ecb8f77e-9c09-4d67-9c5c-b96076beb801

--- en/downloads/debug/index.php
+++ en/downloads/debug/index.php
@@ -24,13 +24,21 @@ foreach ($functions as $f) {
     echo sprintf('<li>%s: %s available</li>',
         $f, function_exists($f) ? 'is' : 'is not');
 }
+echo '<li>DB version: ', geoip_database_info(), '</li>';
+
 echo '</ul>';
 
 echo '<h3>Testing IP, country, continent</h3>';
 foreach ($ips as $label => $ip) {
     echo ' * ', $label, ': ', $ip, ' => ';
     echo geoip_country_code_by_name($ip), ', ';
-    echo geoip_continent_code_by_name($ip), "\n";
+    echo geoip_continent_code_by_name($ip), ', ';
+    echo geoip_record_by_name($ip), ', ';
+    echo geoip_id_by_name($ip), ', '; // GEOIP_UNKNOWN_SPEED GEOIP_DIALUP_SPEED GEOIP_CABLEDSL_SPEED GEOIP_CORPORATE_SPEED
+    echo geoip_isp_by_name($ip), ', ';
+    echo geoip_org_by_name($ip), ', ';
+    echo implode(', ', geoip_record_by_name($ip));
+    echo "\n";
 }
 
 echo '----</pre></body></html>';
