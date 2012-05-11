683110fdf6883936eb77b3e9f0e03ea30e862bb2
Romain d'Alverny <rda@mageia.org>
2012-05-11 12:08:07 +0000

    site map
    git-svn-id: svn+ssh://svn.mageia.org/svn/web/www/trunk@1067 ecb8f77e-9c09-4d67-9c5c-b96076beb801

---
+++ en/map/locales.php
@@ -0,0 +1,46 @@
+<?php
+
+
+$_t = array(
+    'en' => array(
+        'welcome!' => '',
+        
+        
+    ),
+    'fr' => array(
+        'welcome!' => '',
+        'Mageia.org site map' => 'Plan du site Mageia.org',
+        'mageia, web site, map, sitemap' => 'mageia, site web, carte, plan',
+        'Site Map' => 'Plan du site',
+        'About Mageia' => 'À propos de Mageia',
+        'Projects'     => 'Projets',
+        'News'         => 'Actualités',
+        'Downloads'    => 'Téléchargements',
+        '2 (May 2012)' => '2 (mai 2012)',
+        '1 (June 2011)' => '1 (juin 2011)',
+        'Download mirrors' => 'Serveurs miroirs',
+        'Support'      => 'Aide',
+        'Community'    => 'Communauté',
+        'Software updates' => 'Mises à jour',
+        'Product lifecycle' => 'Durée de vie',
+        'Calendar' => 'Calendrier',
+        'Contribute' => 'Contribuer',
+        'About you' => 'Et vous',
+        'Your Mageia id' => 'Votre compte Mageia',
+        'Privacy policy' => 'Respect de la vie privée',
+        'How to contribute?' => 'Comment contribuer&nbsp;?',
+        'Why contribute?' => 'Pourquoi contribuer&nbsp;?',
+        'Donate' => 'Dons',
+        'Supporters' => 'Soutiens',
+        'Governance' => 'Gouvernance',
+        'Board' => 'Bureau',
+        'Council' => 'Conseil',
+        'Teams' => 'Équipes',
+        'Values' => 'Principes',
+        'Code of conduct' => 'Code de conduite',
+        'Constitution' => 'Statuts',
+        'Members' => 'Membres',
+        'Reports' => 'Rapports',
+        'Timeline' => 'Chronologie'
+    )
+);
