2c3b115262deb30dba2d2bed6e01c0d91f3b6b4c
Romain d'Alverny <rda@mageia.org>
2012-05-11 10:10:25 +0000

    donate section is now localized (en,fr for now)
    git-svn-id: svn+ssh://svn.mageia.org/svn/web/www/trunk@1057 ecb8f77e-9c09-4d67-9c5c-b96076beb801

--- en/donate/index.php
+++ en/donate/index.php
@@ -1,90 +1,101 @@
-<!DOCTYPE html>
-<html dir="ltr" lang="en">
+<?php
+define('HLANG', true);
+
+require '../../langs.php';
+include 'locales.php';
+
+$_t = i18n::get_strings($_t, $locale, $i18n_fallback_rules);
+
+?><!DOCTYPE html>
+<html dir="ltr" lang="<?php echo $locale; ?>">
 <head>
     <meta charset="utf-8">
-    <title>Mageia Donation Campaign</title>
-    <meta name="description" content="Mageia is new community-based Linux distribution.">
-    <meta name="keywords" content="mageia, donate, donation, donors, help">
+    <title><?php _e('Donate to Mageia')?></title>
+    <meta name="description" content="">
+    <meta name="keywords" content="<?php _e('mageia, donate, donation, donors, help'); ?>">
     <meta name="author" content="Mageia">
     <link rel="stylesheet" type="text/css" href="/g/style/all.css">
     <?php include '../../analytics.php'; ?>
 </head>
-<body>
-    <?php include '../../langs.php'; ?>
-    
+<body class="contribute donate">
+    <?php echo $hsnav; ?>
+    <h1 id="mgnavt"><?php _e('Donate to Mageia')?></h1>
     <div id="doc" class="yui-t7">
-        <div id="hd" role="banner"><h1><a id="logo" href="/"><span>Mageia</span></a> <span class="lsep">|</span> <span class="subh">Donation Campaign</span></h1></div>
         <div id="bd" role="main">
             <div class="yui-g">
-                <div class="para donate">
+                <div class="para donate" style="padding-top: 2em;">
                     <div style="float: right; border: 1px solid #aaa; padding: 0 0 1em 0; margin: 0 0 2em 1em;">
                         <ul>
-                            <li><a href="#why">Why donate?</a></li>
-                            <li><a href="#status">Donation Tracks</a></li>
-                            <li><a href="#d-paypal">Donate</a> (PayPal, bank, check)</li>
-                            <li><a href="/en/thank-you/#fund">Thank you!</a></li>
+                            <li><a href="#why"><?php _e('Why donate?');?></a></li>
+                            <li><a href="#status"><?php _e('Donation Tracks');?></a></li>
+                            <li><a href="#d-paypal"><?php _e('Donate');?></a> (<?php _e('PayPal, bank, check');?>)</li>
+                            <li><a href="/en/thank-you/#fund"><?php _e('Thank you!');?></a></li>
                         </ul>
                     </div>
-                    <h2 id="why">Why donate?</h2>
-                    <p>Mageia.Org is a not-for-profit association to manage the Mageia distribution.
-                        As a not-for-profit association, it can receive donations from the community
-                        to help in a lot of differents fields:</p>
-                    <ul>
-                        <li>hardware and hosting for servers;</li>
-                        <li>domain names;</li>
-                        <li>registrations of Mageia trademark;</li>
-                        <li>goodies for spreading Mageia;</li>
-                        <li>administrative expenses;</li>
-                        <li>eventually, legal counsel and </li>
-                        <li>etc.</li>
+                    <h2 id="why"><?php _e('Why donate?'); ?></h2>
+                    <p><?php _e('why_donate_txt')?></p>
+                    <ul><?php foreach (_t('why_donate_items') as $e)
+                        echo sprintf('<li>%s</li>', $e);
+                        ?>
                     </ul>
                     <hr />
-                    <h2 id="status">How to track donation?</h2>
-		    <p>Currently, we received a lot of generous donations (<a href="/en/about/reports/<?php echo date('Y'); ?>">remains</a> <strong><?php echo $g_amount_remain; ?></strong>)
-                        thanks to all of our <a href="/en/thank-you/">donors</a>!</p>
+                    <h2 id="status"><?php _e('How to track donation?')?></h2>
+                    <p><?php echo sprintf(_t('how_to_track_txt'),
+                        $g_donate_amount, $g_amount_remain, '/en/about/reports/' . date('Y') . '/')?></p>
 
-		    <p>We believe public accountability is crucial. You can know more about how are used the funds received by reading our 
-			<a href="/en/about/reports/">financial reports</a>.
-                        As stated in the minutes of The First General Constitutive Assembly, the treasurer of Mageia.Org is Damien Lallement.</p>
-                        
+<!--
                     <hr />
+                    <pRE><mark>
+TODO: projected funds/provisions: high level, then detailed budget.
+People can choose how to distribute their donation among those:
+ * infrastructure:
+   - servers
+   - ...
+ * communication
+   - Web
+   - print
+ * give back to other projects/associations (April, FS projects)
+ * employment in mageia.org
+ * IP/brand
+ * ?
+ * other
+                    </mark></pre>
+-->
                 </div>
             </div>
             <div class="yui-g"><div class="para">
-                <h2 id="d-paypal">Donate via PayPal,</h2>
-                <?php include '../../paypal.inc.php'; echo html_paypal_form('en'); ?>
+                <h2 id="d-paypal"><?php _e('Donate via PayPal,'); ?></h2>
+                <?php include '../../paypal.inc.php'; echo html_paypal_form($locale); ?>
             </div></div>
             <div class="yui-g">
                 <div class="yui-u first"><div class="para" style="padding-right: 0;">
-                    <h3>via bank transfer,</h3>
-                    <p>using the following
-                        Bank Identifier Code (IBAN-BIC):</p>
+                    <h3><?php _e('via bank transfer,'); ?></h3>
+                    <p><?php _e('using the following Bank Identifier Code (IBAN-BIC):') ?></p>
                     <pre class="donate-coord">
-<u>In favor of:</u>
+<u><?php _e('In favor of:'); ?></u>
 
 <?php echo $G_coord_assos; ?>
 
 
-<u>Bank Account:</u>
+<u><?php _e('Bank Account:') ?></u>
 
 <?php echo $G_coord_assos_bank; ?>
 
 </pre>
                 </div></div>
                 <div class="yui-u"><div class="para">
-                    <h3>or via check (&euro; only).</h3>
-                    <p>You can send your donation by check to this address:</p>
+                    <h3><?php _e('or via check (&euro; only).')?></h3>
+                    <p><?php _e('You can send your donation by check to this address:')?></p>
                     <pre class="donate-coord">
 <?php echo $G_coord_assos; ?>
 </pre>
-                    <p>Use "Mageia.Org" in the "Pay to the order of..." line.</p>
+                    <p><?php _e('Use "Mageia.Org" in the "Pay to the order of..." line.');?></p>
                 </div><div class="para">
-                    <p><strong>Please note that:</strong></p>
+                    <p><strong><?php _e('Please note that:')?></strong></p>
                     <ul>
-                        <li>Paypal withdraws a small commission for each donation (around 2%) so we get a little bit less
-                            than you send;</li>
-                        <li>conversion rates are charged to Mageia, so please use &euro; as currency;</li>
-                        <li>if you want to do an anonymous donation, please add a comment.</li>
+                        <li><?php _e('Paypal withdraws a small commission for each donation (around 2%) so we get a little bit less than you send;')?></li>
+                        <li><?php _e('conversion rates are charged to Mageia, so please use &euro; as currency;')?></li>
+                        <li><?php _e('if you want to do an anonymous donation, please add a comment.')?></li>
                     </ul>
                 </div></div>
             </div>
---
+++ en/donate/locales.php
@@ -0,0 +1,65 @@
+<?php
+
+$_t = array(
+    'en' => array(
+        'why_donate_txt' => 'Mageia.Org is a not-for-profit association to manage the Mageia distribution.
+            As a not-for-profit association, it can receive donations from the community
+            to help in a lot of differents fields:',
+        'why_donate_items' => array(
+            'hardware and hosting for servers;',
+            'domain names;',
+            'registrations of Mageia trademark;',
+            'goodies for spreading Mageia;',
+            'administrative expenses;',
+            'eventually, legal counsel and',
+            'etc.'
+        ),
+        'how_to_track_txt' => 'Currently, we received a lot of generous donations
+            (%s, remains %s)
+            thanks to all of our <a href="/en/thank-you/">donors</a>!
+            We believe public accountability is crucial. You can know more about how are used the funds received by reading our 
+            <a href="%s">financial reports</a>.'
+    ),
+    'fr' => array(
+        'Donate to Mageia' => 'Donnez à Mageia',
+        'Why donate?' => 'Pourquoi donner&nbsp;?',
+        'Donation Tracks' => 'Suivi des dons',
+        'Donate' => 'Faire un don',
+        'Thank you!' => 'Merci&nbsp;!',
+        'PayPal, bank, check' => 'PayPal, virement bancaire, chèque',
+        'why_donate_txt' => 'Mageia.Org est une association à but non lucratif créée pour gérer la distribution Mageia.
+           Comme association à but non lucratif, elle peut recevoir des dons de la communauté pour aider dans différents domaines&nbsp;:',
+        'why_donate_items' => array(
+            'matériel et hébergement pour les serveurs&nbsp;;',
+            'noms de domaines&nbsp;;',
+            'enregistrements de la marque &nbsp;;',
+            'goodies pour faire connaître Mageia&nbsp;;',
+            'dépenses administratives&nbsp;;',
+            'éventuellement, conseil&nbsp;;',
+            'etc.'
+        ),
+        'How to track donation?' => 'Comment suivre les dons&nbsp;?',
+        'how_to_track_txt' => 'Actuellement, nous avons reçu %s (dont restent %s à ce jour)
+            grâce à tous nos <a href="/en/thank-you/" hreflang="en">donateurs</a>&nbsp;!
+            (mise à jour toutes les semaines).
+            Un <a href="%s">rapport</a> sur les fonds reçus et la façon dont ils ont été utilisés va être mis en place. 
+           Nous pensons que des comptes publics sont d&rsquo;une importance cruciale.',
+        
+        
+        'Donate via PayPal,' => 'Donner via PayPal,',
+        'via bank transfer,' => 'via un virement bancaire,',
+        'or via check (&euro; only).' => 'ou par chèque (€ uniquement).',
+        'using the following Bank Identifier Code (IBAN-BIC):' => 'en utilisant le code d&nbsp;identifiant bancaire (IBAN-BIC) suivant :',
+        'You can send your donation by check to this address:' => 'Vous pouvez envoyer vos dons par chèque à cette adresse :',
+        'In favor of:' => 'En faveur de :',
+        'Bank Account:' => 'Compte bancaire :',
+        'Use "Mageia.Org" in the "Pay to the order of..." line.' => 'Faites le chèque à l&nbsp;ordre de "Mageia.Org".',
+        'Please note that:' => 'Merci de noter que :',
+        'Paypal withdraws a small commission for each donation (around 2%) so we get a little bit less than you send;'
+            => 'Paypal prélève une petite commission (environ 2%) sur chaque don donc nous recevons un peu moins que ce que vous nous avez envoyé&nbsp;;',
+        'conversion rates are charged to Mageia, so please use &euro; as currency;' => 'Les frais de change sont à la charge de Mageia donc, s&nbsp;il vous plaît, utilisez l&rsquo;euro (€, EUR) comme devise&nbsp;;',
+        'if you want to do an anonymous donation, please add a comment.' => 'Si vous souhaitez faire un don anonyme, ajoutez-le en commentaire.'
+        
+    )
+    
+);
