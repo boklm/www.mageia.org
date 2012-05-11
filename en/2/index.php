5c116ddf15c2381ec05a9166a5de8b9896ef0f1a
Romain d'Alverny <rda@mageia.org>
2012-05-11 16:51:31 +0000

    updated layout and boot-nonfree
    git-svn-id: svn+ssh://svn.mageia.org/svn/web/www/trunk@1078 ecb8f77e-9c09-4d67-9c5c-b96076beb801

--- en/2/index.php
+++ en/2/index.php
@@ -6,10 +6,9 @@ include 'locales.php';
 
 $_t = i18n::get_strings($_t, $locale, $i18n_fallback_rules);
 
-$_t['page_h1'] = sprintf($_t['page_h1'], '<em class="tag">Mageia 2 rc</em>');
-$_t['page_title'] = sprintf($_t['page_title'], 'Mageia 2 rc');
+$_t['page_title'] = sprintf($_t['page_title'], 'Mageia 2', 'release candidate');
+$_t['page_h1']    = sprintf($_t['page_h1'], 'Mageia 2', 'release candidate');
 
-require '../downloads/lib.php';
 require '../downloads/get/lib.php';
 ?>
 <!DOCTYPE html>
@@ -24,54 +23,18 @@ require '../downloads/get/lib.php';
     <link rel="stylesheet" href="/g/style/all.css">
     <?php include '../../analytics.php'; ?>
 </head>
-<body class="release">
+<body class="release downloads">
     <?php echo $hsnav; ?>
+    <h1 id="mgnavt"><?php _e('page_h1')?></h1>
     <div id="doc4" class="yui-t7">
-        <?php include 'nav.php'; ?>
-        <div id="hd" role="banner"><h1><a id="logo" href="/<?php echo $locale; ?>/"><span>Mageia</span></a> <span class="lsep">|</span>
-            <span class="subh">2 <em class="tag">release candidate</em></span></h1></div>
         <div id="bd" role="main">
             <div class="yui-g">
-                <div class="para">
-                    <p class="green-sign" style="margin-top: -13px;">
-                    This is a <em>candidate release</em> &ndash;
-                        please check it out and report any outstanding issue<br>
-                        DO NOT use it for production, the final stable release is coming on May 22<sup>nd</sup>.</p>
-                </div>
+                <div class="para"><p class="green-sign" style="margin-top: -13px;"><?php _e('rc-warning');?></p></div>
             </div>
             <div class="yui-ge bb1">
                 <div class="yui-u first rb1">
                     <div class="para">
-                        <h2>Download and test Mageia 2 rc</h2>
-                        <p>You can check the <a href="https://wiki.mageia.org/en/Mageia_2_rc">release notes on the wiki</a>:
-                            what's new, what to expect, what to test, how to report and contribute.</p>
-                        <?php /*
-                        <p>Major features planned for the final release notably include:</p>
-                        <ul>
-                            <li>KDE 4.8.x, GNOME 3.4.x, Sugar 0.95,</li>
-                            <li>glibc 2.14.1, kernel 3.3, systemd,</li>
-                            <li>MariaDB 5.5, PostgreSQL 8.4.10, 9.0.6 and 9.1.2,</li>
-                            <br>
-                            <li>upgradable from Mageia 1 (which was from Mandriva 2010.0 and 2010.1)</li>
-                            <li>stable, secure, maintained,</li>
-                            <li>and many others.</li>
-                        </ul>
-                        <p>Major benefits from Mageia 2:</p>
-                        <ul>
-                            <li>.</li>
-                        </ul>
-                        */ ?>
-                    </div>
-                    <div class="para">
-                        <h3>Full, exclusively Free Software DVDs and dual arch CD</h3>
-                        <ul>
-                            <li><a rel="nofollow" href="../downloads/get/?q=Mageia-2-rc-i586-DVD.iso">Mageia 2 rc 32bit DVD</a>,
-                              <a rel="nofollow" href="../downloads/get/?q=Mageia-2-rc-i586-DVD.iso&amp;torrent=1">torrent</a></li>
-                            <li><a rel="nofollow" href="../downloads/get/?q=Mageia-2-rc-x86_64-DVD.iso">Mageia 2 rc 64bit DVD</a>,
-                              <a rel="nofollow" href="../downloads/get/?q=Mageia-2-rc-x86_64-DVD.iso&amp;torrent=1">torrent</a></li>
-                            <li><a rel="nofollow" href="../downloads/get/?q=Mageia-2-rc-dual-CD.iso">Mageia 2 rc dual CD</a>,
-                              <a rel="nofollow" href="../downloads/get/?q=Mageia-2-rc-dual-CD.iso&amp;torrent=1">torrent</a></li>
-                        </ul>
+                        <h2>Mageia 2 rc Free Software Installation Flavours</h2>
                         <p class="dlinfo warn">Note that these DVD and CD ISOs are made of Free Software exclusively.
                             As a consequence, proprietary Wi-Fi and video drivers are not included.
                             If you need WiFi or specific video drivers <em>at install time</em>,
@@ -83,19 +46,49 @@ require '../downloads/get/lib.php';
                         <p class="dlinfo">Up to 167 locales are supported:
                             Deutsch, English, español, français, italiano, português, svenska, nederlands,
                             polski, dansk, Русский and so much more! See the comprehensive list.</p>
+                        <table class="fr-table dlt">
+                        <tbody>
+                            <tr>
+                                <th>DVD<br><span class="dlinfo">(3.4GB)</span></th>
+                                <td><a rel="nofollow" href="../downloads/get/?q=Mageia-2-rc-i586-DVD.iso" title="Mageia 2 rc 32bit DVD">32bit</a>,
+                                    <a rel="nofollow" href="../downloads/get/?q=Mageia-2-rc-i586-DVD.iso&amp;torrent=1" title="Mageia 2 rc 32bit DVD">torrent</a></td>
+                                <td><a rel="nofollow" href="../downloads/get/?q=Mageia-2-rc-x86_64-DVD.iso" title="Mageia 2 rc 64bit DVD">64bit</a>,
+                                    <a rel="nofollow" href="../downloads/get/?q=Mageia-2-rc-x86_64-DVD.iso&amp;torrent=1" title="Mageia 2 rc 64bit DVD">torrent</a></td>
+                            </tr>
+                            <tr>
+                                <th>CD<br><span class="dlinfo">(700MB)</span></th>
+                                <td colspan="2"><a rel="nofollow" href="../downloads/get/?q=Mageia-2-rc-dual-CD.iso" title="Mageia 2 rc dual CD">dualarch</a>,
+                                    <a rel="nofollow" href="../downloads/get/?q=Mageia-2-rc-dual-CD.iso&amp;torrent=1" title="Mageia 2 rc dual CD">torrent</a></td>
+                            </tr>
+                        </tbody>
+                        </table>
                     </div>
+                    <hr>
                     <div class="para">
-                        <h3>Wired network-based install CD</h3>
-                        <ul>
-                            <li><a rel="nofollow" href="../downloads/get/?q=Mageia-2-rc-Boot-i586-CD.iso">32bit</a></li>
-                            <li><a rel="nofollow" href="../downloads/get/?q=Mageia-2-rc-Boot-x86_64-CD.iso">64bit</a></li>
-                        </ul>
-                        <p class="dlinfo">Good to download (about 40 MB) and boot quickly into install mode
-                          from <em>wired</em> network or local disk.</p>
+                        <h2>Mageia 2 rc Wired Network-based Installation CD</h2>
+                        <p class="dlinfo">Download quickly (about 40 MB) and immediately boot into install mode
+                          from <em>wired</em> network or a local disk.</p>
+
+                        <table class="fr-table dlt">
+                        <tbody>
+                          <tr>
+                              <th>Pure Free Software</th>
+                              <td><a rel="nofollow" href="../downloads/get/?q=Mageia-2-rc-Boot-i586-CD.iso">32bit</a></td>
+                              <td><a rel="nofollow" href="../downloads/get/?q=Mageia-2-rc-Boot-x86_64-CD.iso">64bit</a></td>
+                          </tr>
+                          <tr>
+                              <th>With added nonfree firmware<br>
+                                  <span class="dlinfo">needed for some disc controllers, some network cards, etc.</span></th>
+                              <td><a rel="nofollow" href="../downloads/get/?q=Mageia-2-rc-Boot-nonfree-i586-CD.iso">32bit</a></td>
+                              <td><a rel="nofollow" href="../downloads/get/?q=Mageia-2-rc-Boot-nonfree-x86_64-CD.iso">64bit</a></td>
+                          </tr>
+                        </tbody>
+                        </table>
+
                     </div>
+                    <hr>
                     <div class="para">
-                        <h3>LiveCDs, for a live test or full install</h3>
-                        <!--<p>beta2 LiveCDs still have blocking issues; will be available later.</p>-->
+                        <h2>Mageia 2 rc LiveCDs</h2>
                         <table class="dlt2">
                             <thead><tr><th></th><th>GNOME</th><th>KDE</th></tr></thead>
                             <tbody>
@@ -175,7 +168,8 @@ require '../downloads/get/lib.php';
                 <div class="yui-u">
                     <div class="para">
                         <h3><a href="https://wiki.mageia.org/en/Mageia_2_rc">Release notes</a></h3>
-                        <p>are <a href="https://wiki.mageia.org/en/Mageia_2_rc">on the wiki</a>!</p>
+                        <p>You can check the <a href="https://wiki.mageia.org/en/Mageia_2_rc">release notes on the wiki</a>:
+                            what's new, what to expect, what to test, how to report and contribute.</p>
                     </div>
                     <div class="para">
                         <h3>Release plan</h3>
