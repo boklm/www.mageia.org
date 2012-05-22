d7d9adef121b118b473324463f65a20c573f0b90
Romain d'Alverny <rda@mageia.org>
2012-05-22 21:27:37 +0000

    start i18n
    git-svn-id: svn+ssh://svn.mageia.org/svn/web/www/trunk@1158 ecb8f77e-9c09-4d67-9c5c-b96076beb801

--- en/2/download_index.php
+++ en/2/download_index.php
@@ -32,7 +32,7 @@ require '../downloads/get/lib.php';
             <div class="yui-ge bb1">
                 <div class="yui-u first rb1">
                     <div class="para">
-                        <h2>Free Software Installation Flavours</h2>
+                        <h2><?php _e('Free Software Installation Flavours')?></h2>
                         <table class="fr-table dlt2">
                         <thead>
                             <tr>
@@ -66,26 +66,31 @@ require '../downloads/get/lib.php';
                         
                         <p class="dlinfo">Notes:</p>
                         <ul class="dlinfo hl">
-                            <li>Up to 167 locales are supported:
+                            <li><?php _e('Up to 167 locales are supported:'); ?>
                                 Deutsch, English, español, français, italiano, português, svenska, nederlands,
-                                polski, dansk, Русский and so much more! See the comprehensive list.</li>
-                            <li>These DVD and CD ISOs are made of Free Software exclusively.
-                                As a consequence, <span class="warn">proprietary Wi-Fi and video drivers are not included.
-                                    If you need WiFi or specific video drivers <em>at install time</em>,
-                                    you should go with below LiveCDs instead.</span>
-                                    You may, if you will, add nonfree software repository <em>after</em> the installation.</li>
-                            <li>Please note that there is a <span class="warn">problem with notebooks using Intel, AMD/ATI and nVidia graphic cards.</span>.
-                                <a href="https://wiki.mageia.org/en/Mageia_2_Errata#Hardware_Issues">See the errata
-                                about this</a>.</p>
+                                polski, dansk, Русский
+                                <?php _e('and so much more!'); ?>
+                                <a href="https://wiki.mageia.org/en/Mageia_2_supported_locales" hreflang="en"><?php _e('See the comprehensive liste')?></a>.</li>
+                            <li><?php
+                                _e('These DVD and CD ISOs are made of Free Software exclusively.');
+                                _e('As a consequence, <span class="warn">proprietary Wi-Fi and video drivers are not included.');
+                                _e('If you need WiFi or specific video drivers <em>at install time</em>, you should go with below LiveCDs instead.</span>');
+                                _e('You may, if you will, add nonfree software repository <em>after</em> the installation.');
+                            ?></li>
+                            <li><?php
+                                _e('Please note that there is a <span class="warn">problem with notebooks using Intel, AMD/ATI and nVidia graphic cards.</span>.');
+                                echo sprintf('<a href="https://wiki.mageia.org/en/Mageia_2_Errata#Hardware_Issues">%s</a>',
+                                    _t('See the errata about this'));
+                                ?></li>
                         </ul>
                     </div>
 
                     <div class="para">
                         <h2>LiveCDs</h2>
-                        <p class="dlinfo">Use LiveCDs for fresh new installs ONLY.
-                            <span class="warn">DO NOT use those LiveCDs to upgrade from Mageia 1!</span>
-                            Use above DVD or CD and see the
-                            <a href="https://wiki.mageia.org/en/Mageia_2_Release_Notes#Upgrading_from_Mageia_1" hreflang="en">upgrade guide</a>.</p>
+                        <p class="dlinfo"><?php _e('Use LiveCDs for fresh new installs ONLY.');?>
+                            <span class="warn"><?php _e('DO NOT use those LiveCDs to upgrade from Mageia 1!'); ?></span>
+                            <?php _e('Use above DVD or CD and see the <a href="%s" hreflang="en">upgrade guide</a>.',
+                                array('https://wiki.mageia.org/en/Mageia_2_Release_Notes#Upgrading_from_Mageia_1'));?></p>
                         <table class="dlt2">
                             <thead><tr><th></th><th style="color: black;">GNOME</th><th style="color: black;">KDE</th></tr></thead>
                             <tbody>
@@ -101,7 +106,7 @@ require '../downloads/get/lib.php';
                                       <a rel="nofollow" href="../downloads/get/?q=Mageia-2-LiveCD-KDE4-Europe1-Americas-x86_64-CD.iso&amp;torrent=1">torrent</a></td>
                                 </tr>
                                 <tr>
-                                    <td colspan="3"><p class="dlinfo">Included locales: <?php
+                                    <td colspan="3"><p class="dlinfo"><?php _e('Included locales:');
                                         $product = get_info_for_product('Mageia-2-LiveCD-GNOME-Europe1-Americas-i586-CD.iso');
                                         echo $product['langs'];
                                     ?></p></td>
@@ -118,7 +123,7 @@ require '../downloads/get/lib.php';
                                           <a rel="nofollow" href="../downloads/get/?q=Mageia-2-LiveCD-KDE4-Europe2-x86_64-CD.iso&amp;torrent=1">torrent</a></td>
                                 </tr>
                                 <tr>
-                                    <td colspan="3"><p class="dlinfo">Included locales: <?php
+                                    <td colspan="3"><p class="dlinfo"><?php _e('Included locales:');
                                         $p = get_info_for_product('Mageia-2-LiveCD-GNOME-Europe2-i586-CD.iso');
                                         echo $p['langs'];
                                     ?></p></td>
@@ -135,7 +140,7 @@ require '../downloads/get/lib.php';
                                           <a rel="nofollow" href="../downloads/get/?q=Mageia-2-LiveCD-KDE4-Africa-India-x86_64-CD.iso&amp;torrent=1">torrent</a></td>
                                 </tr>
                                 <tr>
-                                    <td colspan="3"><p class="dlinfo">Included locales: <?php
+                                    <td colspan="3"><p class="dlinfo"><?php _e('Included locales:');
                                         $p = get_info_for_product('Mageia-2-LiveCD-GNOME-Africa-India-i586-CD.iso');
                                         echo $p['langs'];
                                     ?></p></td>
@@ -152,20 +157,19 @@ require '../downloads/get/lib.php';
                                           <a rel="nofollow" href="../downloads/get/?q=Mageia-2-LiveCD-KDE4-Asia-no-India-x86_64-CD.iso&amp;torrent=1">torrent</a></td>
                                 </tr>
                                 <tr>
-                                    <td colspan="3"><p class="dlinfo">Included locales: <?php
+                                    <td colspan="3"><p class="dlinfo"><?php _e('Included locales:');
                                         $p = get_info_for_product('Mageia-2-LiveCD-GNOME-Asia-no-India-i586-CD.iso');
                                         echo $p['langs'];
                                     ?></p></td>
                                 </tr>
                             </tbody>
                         </table>
-                        <p class="dlinfo">Each download is approximately 700MB.</p>
+                        <p class="dlinfo"><?php _e('Each download is approximately 700MB.');?></p>
                     </div>
                     
                     <div class="para">
-                        <h2>Wired Network-based Installation CD</h2>
-                        <p class="dlinfo">Download quickly (about 40 MB) and immediately boot into install mode
-                          from <em>wired</em> network or a local disk.</p>
+                        <h2><?php _e('Wired Network-based Installation CD')?></h2>
+                        <p class="dlinfo"><?php _e('Download quickly (about 40 MB) and immediately boot into install mode from <em>wired</em> network or a local disk.')?></p>
 
                         <table class="fr-table dlt2">
                         <thead>
@@ -176,13 +180,13 @@ require '../downloads/get/lib.php';
                         </thead>
                         <tbody>
                           <tr>
-                              <th>Pure Free Software CD</th>
+                              <th><?php _e('Pure Free Software CD')?></th>
                               <td><a rel="nofollow" href="../downloads/get/?q=Mageia-2-Boot-i586-CD.iso">32bit</a></td>
                               <td><a rel="nofollow" href="../downloads/get/?q=Mageia-2-Boot-x86_64-CD.iso">64bit</a></td>
                           </tr>
                           <tr>
-                              <th>Same + nonfree firmware<br>
-                                  <span class="dlinfo">needed for some disc controllers, some network cards, etc.</span></th>
+                              <th><?php _e('Same + nonfree firmwaree')?><br>
+                                  <span class="dlinfo"><?php _e('needed for some disc controllers, some network cards, etc.')?></span></th>
                               <td><a rel="nofollow" href="../downloads/get/?q=Mageia-2-Boot-nonfree-i586-CD.iso">32bit</a></td>
                               <td><a rel="nofollow" href="../downloads/get/?q=Mageia-2-Boot-nonfree-x86_64-CD.iso">64bit</a></td>
                           </tr>
@@ -195,23 +199,25 @@ require '../downloads/get/lib.php';
                 <div class="yui-u">
                     <div class="para" style="padding-left: 1em;">
                         <h2>Mageia 2</h2>
-                        <p>May 22<sup>nd</sup> 2012</p>
+                        <?php _h('May 22<sup>nd</sup> 2012')?>
                         <ul class="hl">
                             <li><a href="https://wiki.mageia.org/en/Mageia_2_Release_Notes">Release notes</a></li>
                             <li><a href="https://wiki.mageia.org/en/Mageia_2_Errata">Errata</a>.</li>
                         </ul>
                         <br>
                         <br>
-                        <h3>Upgrading<br>from Mageia 1?</h3>
-                        <p>Then:</p>
-                        <ul class="hl">
-                            <li><strong>do not</strong> use LiveCDs;</li>
-                            <li>see the <a href="https://wiki.mageia.org/en/Mageia_2_Release_Notes#Upgrading_from_Mageia_1" hreflang="en">upgrade guide</a>.</li>
-                        </ul>
+                        <?php _h('Upgrading<br>from Mageia 1?', null, 'h3')?>
+                        <ul class="hl"><?php
+                            _e('<strong>do not</strong> use LiveCDs;', null, 'li');
+                            _e('see the <a href="%s" hreflang="en">upgrade guide</a>',
+                                array('https://wiki.mageia.org/en/Mageia_2_Release_Notes#Upgrading_from_Mageia_1'), 'li');
+                        ?></ul>
                         <br>
                         <br>
-                        <h3>Looking for Mageia 1?</h3>
-                        <p>It is <a href="../downloads/1/">here now</a>.</p>
+                        <?php
+                            _h('Looking for Mageia 1?', null, 'h3');
+                            _h('It is <a href="%s">here now</a>.', array('../downloads/1/'));
+                        ?>
                     </div>
                     <hr>
                 </div>
