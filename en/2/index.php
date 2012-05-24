c4c56b7f4ac3cae055a5937cfda27ddee13b16d9
Romain d'Alverny <rda@mageia.org>
2012-05-24 16:06:16 +0000

    removed useless code, use new lang library
    git-svn-id: svn+ssh://svn.mageia.org/svn/web/www/trunk@1185 ecb8f77e-9c09-4d67-9c5c-b96076beb801

--- en/2/index.php
+++ en/2/index.php
@@ -3,74 +3,17 @@
 define('HLANG', true);
 require '../../langs.php';
 include 'locales.php';
-
-$_t = array(
-    'de' => array(
-        'blog_url' => 'http://blog.mageia.org/de/',
-        'planet_url' => 'http://planet.mageia.org/de/',
-        'forum_url' => 'http://forums.mageia.org/de/',
-    ),
-    'el' => array(
-        'blog_url' => 'http://blog.mageia.org/el/',
-    ),
-    'en' => array(
-        'page_title' => 'Mageia Community',
-        'page_h1' => 'Mageia Community Central',
-        'blog_url' => 'http://blog.mageia.org/en/',
-        'planet_url' => 'http://planet.mageia.org/en/',
-        'forum_url' => 'https://forum.mageia.org/en/',
-    ),
-    'es' => array(
-        'blog_url' => 'http://blog.mageia.org/en/',
-        'planet_url' => 'http://planet.mageia.org/es/'
-    ),
-    'fr' => array(
-        'page_title' => 'Communauté Mageia',
-        'page_h1' => 'Portail de la communauté Mageia',
-        'News' => 'Actualités',
-        'How to contribute?' => 'Comment contribuer&nbsp;?',
-        'Toolbox' => 'Boîte à outils',
-        'blog_url' => 'http://blog.mageia.org/fr/',
-        'planet_url' => 'http://planet.mageia.org/fr/',
-        'forum_url' => 'http://forum.mageia.org/fr/'
-    ),
-    'it' => array(
-        'blog_url' => 'http://blog.mageia.org/it/',
-        'planet_url' => 'http://planet.mageia.org/it/'
-    ),
-    'nl' => array(
-        'blog_url' => 'http://blog.mageia.org/nl/',
-    ),
-    'pl' => array(
-        'blog_url' => 'http://blog.mageia.org/pl/',
-    ),
-    'pt' => array(
-        'blog_url' => 'http://blog.mageia.org/pt/',
-    ),
-    'ro' => array(
-        'blog_url' => 'http://blog.mageia.org/ro/',
-    ),
-    'ru' => array(
-        'blog_url' => 'http://blog.mageia.org/ru/'
-    ),
-    'tr' => array(
-        'blog_url' => 'http://blog.mageia.org/tr/'
-    ),
-    'uk' => array(
-        'blog_url' => 'http://blog.mageia.org/uk/'
-    )
-);
-
 $_t = i18n::get_strings($_t, $locale, $i18n_fallback_rules);
-include '../../lib/news.php';
 
+_lang_load($locale, '2');
 ?>
 <!DOCTYPE html>
 <html lang="<?php echo $locale; ?>">
 <head>
     <meta charset="utf-8">
-    <title><?php _e('page_title')?></title>
+    <title><?php _e('Mageia 2')?></title>
     <link rel="stylesheet" href="/g/style/all.css">
+    <meta name="description" content="<?php _e('Mageia 2 is the new, solid, stable Linux distribution from the Mageia project.')?>">
     <style>
     .para { text-align: left; float: left; width: 250px; display: block; border-right: 1px solid #eef; border-bottom: 1px solid #eef; }
     hr { margin-top: 2em; }
@@ -78,7 +21,7 @@ include '../../lib/news.php';
 </head>
 <body class="downloads">
     <?php echo $hsnav; ?>
-    <h1 id="mgnavtitle"><a href="../2/">Mageia 2</a> &raquo; <?php _e('for your server')?></h1>
+    <h1 id="mgnavtitle"><a href="../2/">Mageia 2</a></h1>
     <?php include '../2/nav.php'; ?>
     <div class="para" style="width: 600px;">
         <section>
