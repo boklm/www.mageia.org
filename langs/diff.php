d6628a529e44af8263b4a94b2ca6bfb8c6373853
Romain d'Alverny <rda@mageia.org>
2012-05-24 16:06:13 +0000

    output can be better c/pasted into editor
    git-svn-id: svn+ssh://svn.mageia.org/svn/web/www/trunk@1183 ecb8f77e-9c09-4d67-9c5c-b96076beb801

--- langs/diff.php
+++ langs/diff.php
@@ -37,16 +37,21 @@ if (false === strstr($source_file, '/langs/') ||
 $diff = _lang_diff($source_file, $target_file);
 
 $s = '<a href="report.php">&laquo; back to langs report</a>';
-$s .= "<h1>Diff {$source_file} ({$diff['a']}) against {$target_file} ({$diff['b']})</h1>";
+$s .= sprintf('<h1>Differences between %s source and %s target</h1>',
+    substr($source_file, strpos($source_file, '/langs/')+7), $target_lang);
 
 foreach (array('missing', 'notrans', 'extra') as $type) {
 
     if (count($diff[$type]) > 0) {
         $s .= sprintf('<h2>%d %s:</h2>', count($diff[$type]), $type);
+        $s .= '<pre>';
         foreach ($diff[$type] as $l)
-            $s .= sprintf('<blockquote><p><pre>%s</pre></p></blockquote>', htmlentities($l));
+            $s .= sprintf(";%s\n\n", htmlspecialchars($l));
+        $s .= '</pre>';
     }
 }
+
+header('Content-Type: text/html;charset=utf-8');
 
 ?><!DOCTYPE html>
 <html lang="en">
@@ -54,7 +59,7 @@ foreach (array('missing', 'notrans', 'extra') as $type) {
     <meta charset="utf-8">
     <meta name="robot" content="noindex,nofollow,nosnippet">
     <style>
-    blockquote { background: #eee; padding: 0.1em 1em; }
+    pre { background: #eee; padding: 0.6em; }
     </style>
 </head>
 <body>
