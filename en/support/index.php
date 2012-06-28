6406cc29166096f6d9e87a994169f24b1f9915bf
Romain d'Alverny <rda@mageia.org>
2012-06-28 13:30:43 +0000

    update display + add entry about bugs reports CCBUG 6607
    git-svn-id: svn+ssh://svn.mageia.org/svn/web/www/trunk@1326 ecb8f77e-9c09-4d67-9c5c-b96076beb801

--- en/support/index.php
+++ en/support/index.php
@@ -10,9 +10,9 @@ $_t = i18n::get_strings($_t, $locale, $i18n_fallback_rules);
 <html dir="ltr" lang="<?php echo $locale; ?>">
 <head>
     <meta charset="utf-8">
-    <title><?php echo $_t['page_title']; ?></title>
-    <meta name="description" content="<?php echo $_t['page_desc']; ?>">
-    <meta name="keywords" content="<?php echo $_t['page_kw']; ?>">
+    <title><?php _e('page_title')?></title>
+    <meta name="description" content="<?php _e('page_desc')?>">
+    <meta name="keywords" content="<?php _e('page_kw')?>">
     <meta name="author" content="Mageia">
     <link rel="stylesheet" type="text/css" href="/g/style/all.css">
     <?php include '../../analytics.php'; ?>
@@ -26,50 +26,63 @@ $_t = i18n::get_strings($_t, $locale, $i18n_fallback_rules);
                 <div class="para donate" style="padding-top: 2em;">
                     <div class="qsidesummary">
                         <ul>
-                            <li><a href="#comm"><?php echo $_t['h2_comm_support']; ?></a></li>
-                            <li><a href="#pro"><?php echo $_t['h2_pro_support']; ?></a></li>
-                            <li><a href="#updates"><?php echo $_t['h2_updates']; ?></a></li>
+                            <li><a href="#comm"><?php _e('h2_comm_support')?></a></li>
+                            <li><a href="#pro"><?php _e('h2_pro_support')?></a></li>
+                            <li><a href="#updates"><?php _e('h2_updates')?></a></li>
                             <li><a href="#lifecycle"><?php _e('Lifecycle')?></a></li>
-                            <li><a href="#hw"><?php echo $_t['h2_hw_reqs']; ?></a></li>
+                            <li><a href="#hw"><?php _e('h2_hw_reqs')?></a></li>
+                            <li><a href="#bugs"><?php _e('Bugs Reports')?></a></li>
                         </ul>
                     </div>
 
-                    <h2 id="comm"><?php echo $_t['h2_comm_support']; ?></h2>
-                    <p><?php echo $_t['comm_1']; ?></p>
-                    <ul class="hl"><?php foreach ($_t['comm_list'] as $commi):
-                        echo sprintf('<li>%s</li>', $commi);
-                    endforeach; ?></ul>
-                    <hr>
+                    <?php
+                        _h('h2_comm_support', null, 'h2 id="comm"');
+                        _h('comm_1');
 
-                    <h2 id="pro"><?php echo $_t['h2_pro_support']; ?></h2>
-                    <p><?php echo $_t['pro_1']; ?></p>
-                    <p><?php echo sprintf($_t['pro_2'], 'https://wiki.mageia.org/en/Commercial_vendors'); ?></p>
-                    <hr>
+                        echo '<ul class="hl">';
+                        foreach ($_t['comm_list'] as $commi)
+                            _h($commi, null, 'li');
+                        echo '</ul>';
+                        echo '<hr>';
 
-                    <h2 id="updates"><?php echo $_t['h2_updates']; ?></h2>
-                    <p><?php echo sprintf($_t['updates_1'], '<a href="../1/">Mageia 1</a>', '<a href="../2/">Mageia 2</a>'); ?></p>
-                    <p><?php echo $_t['updates_3']; ?></p>
-                    <p><?php echo sprintf($_t['updates_2'], '<a href="https://ml.mageia.org/wwsympa-wrapper.fcgi/info/updates-announce">updates-announce</a>'); ?></p>
-                    <hr>
-                    <h3><?php _e('Lifecycle')?></h3>
-                    <p><?php _e('Mageia releases are supported at least for 18 months.');?></p>
-                    <ul>
-                        <li><?php _e('Mageia 1 will be supported until December 2012.')?></li>
-                        <li><?php _e('Mageia 2 may have a longer lifecycle, this will be decided once released in May.')?></li>
-                    </ul>
-                    <hr>
+                        _h('h2_pro_support', null, 'h2 id="pro"');
+                        _h('pro_1');
+                        _h('pro_2', array('https://wiki.mageia.org/en/Commercial_vendors'));
+                        echo '<hr>';
 
-                    <h2 id="hw"><?php echo $_t['h2_hw_reqs']; ?></h2>
-                    <p><?php echo $_t['hw_1']; ?></p>
+                        _h('h2_updates', null, 'h2 id="updates"');
+                        _h('updates_1', array('<a href="../1/">Mageia 1</a>', '<a href="../2/">Mageia 2</a>'));
+                        _h('updates_3');
+                        _h('updates_2', array('<a href="https://ml.mageia.org/wwsympa-wrapper.fcgi/info/updates-announce">updates-announce</a>'));
+                        echo '<hr>';
 
-                    <p><?php echo $_t['hw_2']; ?></p>
-                    <ul class="hl"><?php foreach ($_t['hw_list'] as $hwi):
-                        echo sprintf('<li>%s</li>', $hwi);
-                    endforeach; ?></ul>
-                    <p><?php echo $_t['hw_some_hardware']; ?></p>
+                        _h('Lifecycle', null, 'h3');
+                        _h('Mageia releases are supported at least for 18 months.');
+                        echo '<ul class="hl">';
+                        _h('Mageia 1 will be supported until December 2012.', null, 'li');
+                        _h('Mageia 2 may have a longer lifecycle, this will be decided once released in May.', null, 'li');
+                        echo '</ul>';
+                        echo '<hr>';
 
-                    <p class="dlinfo"><?php echo sprintf($_t['hw_hcl_note'], 'https://wiki.mageia.org/en/Web_team', 'https://wiki.mageia.org/en/QA_Team'); ?></p>
-                    <hr>
+                        _h('h2_hw_reqs', null, 'h2 id="hw"');
+                        _h('hw_1');
+                        _h('hw_2');
+                        echo '<ul class="hl">';
+                        foreach ($_t['hw_list'] as $hwi):
+                            echo _h($hwi, null, 'li');
+
+                        endforeach;
+                        echo '</ul>';
+
+                        _h('hw_some_hardware');
+                        _h('hw_hcl_note', array('https://wiki.mageia.org/en/Web_team', 'https://wiki.mageia.org/en/QA_Team'), 'p class="dlinfo"');
+                        echo '<hr>';
+                        
+                        _h('Bugs Reports', null, 'h2 id="bugs"');
+                        _h('You may check and report bugs on <a href="%s">our Bugzilla (bugs.mageia.org)</a>',
+                            array('https://bugs.mageia.org/'));
+                        echo '<hr>';
+                    ?>
                 </div>
             </div>
         </div>
