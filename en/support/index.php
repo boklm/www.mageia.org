5bbb96d419c0e8548c5e15357b873853ec1e37d5
Romain d'Alverny <rda@mageia.org>
2012-06-30 16:30:12 +0000

    localization of support page (Rémi)
    git-svn-id: svn+ssh://svn.mageia.org/svn/web/www/trunk@1345 ecb8f77e-9c09-4d67-9c5c-b96076beb801

--- en/support/index.php
+++ en/support/index.php
@@ -2,58 +2,59 @@
 
 define('HLANG', true);
 require '../../langs.php';
-include 'locales.php';
-
-$_t = i18n::get_strings($_t, $locale, $i18n_fallback_rules);
+_lang_load($locale, 'support');
 
 ?><!DOCTYPE html>
 <html dir="ltr" lang="<?php echo $locale; ?>">
 <head>
     <meta charset="utf-8">
-    <title><?php _e('page_title')?></title>
-    <meta name="description" content="<?php _e('page_desc')?>">
-    <meta name="keywords" content="<?php _e('page_kw')?>">
+    <title><?php _e('Support for Mageia distribution')?></title>
+    <meta name="description" content="<?php _e('List of support resources for Mageia distribution.')?>">
+    <meta name="keywords" content="<?php _e('mageia, linux, support, help, hardware, qa, test')?>">
     <meta name="author" content="Mageia">
     <link rel="stylesheet" type="text/css" href="/g/style/all.css">
     <?php include '../../analytics.php'; ?>
 </head>
 <body class="support">
     <?php echo $hsnav; ?>
-    <h1 id="mgnavt"><?php _e('h1_title')?></h1>
+    <h1 id="mgnavt"><?php _e('Support')?></h1>
     <div id="doc" class="yui-t7">
         <div id="bd" role="main">
             <div class="yui-g">
                 <div class="para donate" style="padding-top: 2em;">
                     <div class="qsidesummary">
                         <ul>
-                            <li><a href="#comm"><?php _e('h2_comm_support')?></a></li>
-                            <li><a href="#pro"><?php _e('h2_pro_support')?></a></li>
-                            <li><a href="#updates"><?php _e('h2_updates')?></a></li>
+                            <li><a href="#comm"><?php _e('Community Support')?></a></li>
+                            <li><a href="#pro"><?php _e('Professional Support')?></a></li>
+                            <li><a href="#updates"><?php _e('Updates')?></a></li>
                             <li><a href="#lifecycle"><?php _e('Lifecycle')?></a></li>
-                            <li><a href="#hw"><?php _e('h2_hw_reqs')?></a></li>
+                            <li><a href="#hw"><?php _e('Hardware Requirements')?></a></li>
                             <li><a href="#bugs"><?php _e('Bugs Reports')?></a></li>
                         </ul>
                     </div>
 
                     <?php
-                        _h('h2_comm_support', null, 'h2 id="comm"');
-                        _h('comm_1');
+                        _h('Community Support', null, 'h2 id="comm"');
+                        _h('If you need help, information or directions about the Mageia distribution you installed or about the project, you can try to reach us through:');
 
                         echo '<ul class="hl">';
-                        foreach ($_t['comm_list'] as $commi)
-                            _h($commi, null, 'li');
+                        _h('<a href="http://forums.mageia.org/en/">Community support forums</a> in English &ndash; see <a href="https://forums.mageia.org/en/viewforum.php?f=20">other Mageia community forums</a> for support in your language;', null, 'li');
+                        _h('IRC channels where you can discuss live with other Mageia users and contributors: <a href="irc://irc.freenode.net/#mageia" hreflang="en">#mageia</a> or other localized <a href="https://wiki.mageia.org/en/IRC#channels_in_other_languages">channels</a>;', null, 'li');
+                        _h('our <a href="https://wiki.mageia.org/en/">Wiki</a> (still rough at the time, we are working on it!);', null, 'li');
+                        _h('our <a href="http://www.mageia.org/mailman/" hreflang="en">mailing-lists</a>;', null, 'li');
+                        _h('local events: follow our <a href="http://blog.mageia.org/en/">blog</a> and <a href="http://www.mageia.org/en/calendar/">calendar</a>.', null, 'li');
                         echo '</ul>';
                         echo '<hr>';
 
-                        _h('h2_pro_support', null, 'h2 id="pro"');
-                        _h('pro_1');
-                        _h('pro_2', array('https://wiki.mageia.org/en/Commercial_vendors'));
+                        _h('Professional Support', null, 'h2 id="pro"');
+                        _h('Mageia.Org does not provide or endorse commercial/professional support or other services around the distribution. But there are organizations providing such services and more.');
+                        _h('Please check the <a href="%s">commercial vendors</a> list', array('https://wiki.mageia.org/en/Commercial_vendors'));
                         echo '<hr>';
 
-                        _h('h2_updates', null, 'h2 id="updates"');
-                        _h('updates_1', array('<a href="../1/">Mageia 1</a>', '<a href="../2/">Mageia 2</a>'));
-                        _h('updates_3');
-                        _h('updates_2', array('<a href="https://ml.mageia.org/wwsympa-wrapper.fcgi/info/updates-announce">updates-announce</a>'));
+                        _h('Updates', null, 'h2 id="updates"');
+                        _h('Updates are available for %s and %s (security and bug fixes) and published on an ongoing basis.', array('<a href="../1/">Mageia 1</a>', '<a href="../2/">Mageia 2</a>'));
+                        _h('You can install these from the Mageia Control Center.');
+                        _h('You may subscribe to our %s announce list to be notified of these.', array('<a href="https://ml.mageia.org/wwsympa-wrapper.fcgi/info/updates-announce">updates-announce</a>'));
                         echo '<hr>';
 
                         _h('Lifecycle', null, 'h3');
@@ -64,20 +65,23 @@ $_t = i18n::get_strings($_t, $locale, $i18n_fallback_rules);
                         echo '</ul>';
                         echo '<hr>';
 
-                        _h('h2_hw_reqs', null, 'h2 id="hw"');
-                        _h('hw_1');
-                        _h('hw_2');
+                        _h('Hardware Requirements', null, 'h2 id="hw"');
+                        _h('Mageia software runs on most x86 computer systems available as of today, April 2011.');
+                        _h('You can follow this hardware requirements list:');
                         echo '<ul class="hl">';
-                        foreach ($_t['hw_list'] as $hwi):
-                            echo _h($hwi, null, 'li');
 
-                        endforeach;
+                        _h('Processor: any AMD, Intel or VIA processor;', null, 'li');
+                        _h('Memory (RAM): 512MB minimum, 2GB recommended;', null, 'li');
+                        _h('Storage (HDD): 1GB for a minimal installation, 6GB for a full setup;', null, 'li');
+                        _h('Optical drive: CD or DVD depending on the ISO you use (network, USB key installation available);', null, 'li');
+                        _h('Graphic card: any ATI, Intel, Matrox, nVidia, SiS or VIA graphic card;', null, 'li');
+                        _h('Sound card: any AC97, HDA or Sound Blaster sound card.', null, 'li');
                         echo '</ul>';
 
-                        _h('hw_some_hardware');
-                        _h('hw_hcl_note', array('https://wiki.mageia.org/en/Web_team', 'https://wiki.mageia.org/en/QA_Team'), 'p class="dlinfo"');
+                        _h('For some kinds of hardware (Wi-Fi chipsets, 3D graphic cards) to work properly, specific firmware or software may be needed, this is available in a specific online repository called "nonfree" (learn more).');
+                        _h('We may set up a hardware compatibility list/directory as well as a hardware certification process; but that\'s still at the planning stage. Feel free to join/contact our <a href="%s">Web</a> and <a href="%s">QA</a> teams if you would like to help in this regard.', array('https://wiki.mageia.org/en/Web_team', 'https://wiki.mageia.org/en/QA_Team'), 'p class="dlinfo"');
                         echo '<hr>';
-                        
+
                         _h('Bugs Reports', null, 'h2 id="bugs"');
                         _h('You may check and report bugs on <a href="%s">our Bugzilla (bugs.mageia.org)</a>.',
                             array('https://bugs.mageia.org/'));
