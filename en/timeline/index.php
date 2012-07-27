f186eb8aa091a36a55a12aa37cae6fb260051903
Filip Komar <filip@mageia.org>
2012-07-27 11:34:30 +0000

    lang files translation system for timeline page
    git-svn-id: svn+ssh://svn.mageia.org/svn/web/www/trunk@1503 ecb8f77e-9c09-4d67-9c5c-b96076beb801

--- en/timeline/index.php
+++ en/timeline/index.php
@@ -2,42 +2,14 @@
 
 define('HLANG', true);
 require '../../langs.php';
-
-$_t = array(
-    'page_h1' => 'Mageia Timeline',
-    'page_title' => 'Mageia Timeline',
-);
-
-$events = array(
-    '2010' => array(
-        'September' => array(
-            'Mageia starts as a <a href="/en/about/2010-sept-announcement.html">fork of Mandriva Linux</a>.',
-            'Mageia.Org is registered in Paris, France.'
-        ),
-        'End of year' => array(
-            'With an incredible response, first donations and discussions abound.',
-            'Project gets structured, governance takes slowly shape (first board, teams).'
-        )
-    ),
-    '2011' => array(
-        'January' => 'Build system is ready to run for the first alpha ISOs.',
-        'February' => 'First General Assembly during FOSDEM in Brussels, Belgium.',
-        'June' => '<a href="/1/">Mageia 1</a> is released.',
-    ),
-    '2012' => array(
-        'February' => 'Second General Assembly during FOSDEM in Brussels, Belgium.',
-        'May' => array(
-            '<a href="/2/">Mageia 2</a> is released.',
-        )
-    )
-);
+_lang_load($locale, 'timeline');
 
 ?>
 <!DOCTYPE html>
 <html lang="<?php echo $locale; ?>">
 <head>
     <meta charset="utf-8">
-    <title><?php _e('page_title')?></title>
+    <title><?php _e('Mageia Timeline')?></title>
     <link rel="stylesheet" href="/g/style/all.css">
     <style>
     .para { text-align: left; float: left; width: 250px; display: block; border-right: 1px solid #eef; border-bottom: 1px solid #eef; }
@@ -52,33 +24,75 @@ $events = array(
     .tlim h3 { color: #777; font-size: 130%; }
     .tlie { margin-left: 4em; }
     </style>
+    <link rel="canonical" href="/<?php echo $locale; ?>/timeline/">
     <?php include '../../analytics.php'; ?>
 </head>
 <body class="about">
     <?php echo $hsnav; ?>
-    <h1 id="mgnavtitle"><?php _e('page_h1')?></h1>
+    <h1 id="mgnavtitle"><?php _e('Mageia Timeline')?></h1>
     <div class="para" style="width: 800px;">
         <section>
-            <ul class="hl" id="tl0"><?php
-                $events = array_reverse($events, true);
-                foreach ($events as $year => $dates):
-                    $li = array();
-                    $dates = array_reverse($dates, true);
-                    foreach ($dates as $dt => $ev) {
-                        if (!is_array($ev))
-                            $ev = array($ev);
-
-                        $s = array();
-                        foreach ($ev as $evit) {
-                            $s[] = sprintf('<li class="tlie">%s</li>', $evit);
-                        }
-                        $li[] = sprintf('<li class="tlim"><h3>%s</h3><ul class="hl">%s</ul></li>', $dt, implode($s));
-                    }
-                    echo sprintf('<li class="tliy"><h2>%s</h2><ul class="hl">%s</ul></li>', $year, implode($li));
-
-                endforeach;
-            ?></ul>
-            
+            <ul class="hl" id="tl0">
+                <li class="tliy">
+                    <h2>2012</h2>
+                    <ul class="hl">
+                        <li class="tlim">
+                            <h3><?php _e('May')?></h3>
+                            <ul class="hl">
+                                <li class="tlie"><?php _e('<a href="/2/">Mageia 2</a> is released.')?></li>
+                            </ul>
+                        </li>
+                        <li class="tlim">
+                            <h3><?php _e('February')?></h3>
+                            <ul class="hl">
+                                <li class="tlie"><?php _e('Second General Assembly during FOSDEM in Brussels, Belgium.')?></li>
+                            </ul>
+                        </li>
+                    </ul>
+                </li>
+                <li class="tliy">
+                    <h2>2011</h2>
+                    <ul class="hl">
+                        <li class="tlim">
+                            <h3><?php _e('June')?></h3>
+                            <ul class="hl">
+                                <li class="tlie"><?php _e('<a href="/1/">Mageia 1</a> is released.')?></li>
+                            </ul>
+                        </li>
+                        <li class="tlim">
+                            <h3><?php _e('February')?></h3>
+                            <ul class="hl">
+                                <li class="tlie"><?php _e('First General Assembly during FOSDEM in Brussels, Belgium.')?></li>
+                            </ul>
+                        </li>
+                        <li class="tlim">
+                            <h3><?php _e('January')?></h3>
+                            <ul class="hl">
+                                <li class="tlie"><?php _e('Build system is ready to run for the first alpha ISOs.')?></li>
+                            </ul>
+                        </li>
+                    </ul>
+                </li>
+                <li class="tliy">
+                    <h2>2010</h2>
+                    <ul class="hl">
+                        <li class="tlim">
+                            <h3><?php _e('End of year')?></h3>
+                            <ul class="hl">
+                                <li class="tlie"><?php _e('With an incredible response, first donations and discussions abound.')?></li>
+                                <li class="tlie"><?php _e('Project gets structured, governance takes slowly shape (first board, teams).')?></li>
+                            </ul>
+                        </li>
+                        <li class="tlim">
+                            <h3><?php _e('September')?></h3>
+                            <ul class="hl">
+                                <li class="tlie"><?php _e('Mageia starts as a <a href="/en/about/2010-sept-announcement.html">fork of Mandriva Linux</a>.')?></li>
+                                <li class="tlie"><?php _e('Mageia.Org is registered in Paris, France.')?></li>
+                            </ul>
+                        </li>
+                    </ul>
+                </li>
+            </ul>
         </section>
     </div>
 </body>
