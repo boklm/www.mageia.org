98c2a2338a82faa857c7d06ff6548df4f3ec27e9
Filip Komar <filip@mageia.org>
2012-08-07 16:53:31 +0000

    thank-you page switched to lang tr. system
    git-svn-id: svn+ssh://svn.mageia.org/svn/web/www/trunk@1598 ecb8f77e-9c09-4d67-9c5c-b96076beb801

--- en/thank-you/index.php
+++ en/thank-you/index.php
@@ -1,14 +1,14 @@
 <?php
 define('HLANG', true);
 require '../../langs.php';
-include 'locales.php';
 
-$_t = i18n::get_strings($_t, $locale, $i18n_fallback_rules);
+_lang_load($locale, "thank-you");
+
 
 include '../../donators.inc.php';
 
 ?><!DOCTYPE html>
-<html dir="ltr" lang="en">
+<html dir="ltr" lang="<?php echo $locale ?>">
 <head>
     <meta charset="utf-8">
     <title><?php _e('Thank you for making Mageia possible.')?></title>
@@ -24,9 +24,9 @@ include '../../donators.inc.php';
     <div id="doc4" class="yui-t7">
         <div id="bd" role="main">
             <div class="yui-g"><div class="para donate" style="padding-top: 2em;">
-                <p><?php _e('expl1')?></p>
-                <p><?php _e('expl2')?></p>
-                <p><?php _e('expl3')?></p>
+                <p><?php _e('Since September 2010, Mageia would not have come to a reality without the enthusiasm, advice and involvement of hundreds of people.')?></p>
+                <p><?php _e('It would not have either, without help and donation from many people and organizations, that provided Mageia.Org with advice, encouragements, trust, facilities, hosting, server and development hardware, mirroring facilities, network bandwidth, money and finally, care.')?></p>
+                <p><?php _e('This page is here to remind of their contribution to this project.')?></p>
             </div></div>
             <div class="yui-gc">
                 <div class="yui-u first rb1"><div class="para donate">
@@ -38,22 +38,18 @@ include '../../donators.inc.php';
                     <?php echo $pp['list']; ?>
                 </div></div>
                 <div class="yui-u"><div class="para donate">
-                    <p>Those organizations and people provided Mageia.Org with
-                        mirroring facilities, hosting, hosting advice, server hardware,
-                        network bandwidth or some facilities:</p>
+                    <p><?php _e('Those organizations and people provided Mageia.Org with mirroring facilities, hosting, hosting advice, server hardware, network bandwidth or some facilities:')?></p>
                     <?php echo html_orgs_list($orgs); ?>
                     
-                    <p>Great places and teams that welcomed us and offerred us some tea, coffee, seats and advice
-                        (mostly in Paris for now):</p>
+                    <p><?php _e('Great places and teams that welcomed us and offerred us some tea, coffee, seats and advice (mostly in Paris for now):')?></p>
                     <ul class="ty-ppl-list twocol">
-                        <li><a href="http://lacantine.org/">La Cantine</a></li>
-                        <li>Le Père Tranquille</li>
-                        <li>Séverine's place</li>
-                        <li><a href="http://twitter.com/happylisou/">&Eacute;lise</a>'s place</a></li>
-                        <li>Starbucks Coffee</li>
+                        <li><?php _e('<a href="http://lacantine.org/">La Cantine</a>')?></li>
+                        <li><?php _e('Le Père Tranquille')?></li>
+                        <li><?php _e('Séverine\'s place')?></li>
+                        <li><?php _e('<a href="http://twitter.com/happylisou/">&Eacute;lise</a>\'s place</a>')?></li>
+                        <li><?php _e('Starbucks Coffee')?></li>
                     </ul>
-                    <p>Thanks a lot to all the people we met, discussed with about this project
-                        and its potential implications: you know who you are!</p>
+                    <p><?php _e('Thanks a lot to all the people we met, discussed with about this project and its potential implications: you know who you are!')?></p>
                     
                 </div></div>
             </div>
