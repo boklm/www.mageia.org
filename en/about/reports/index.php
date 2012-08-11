ae526ef989118c6963cad6a4154df9bbe1a60081
Filip Komar <filip@mageia.org>
2012-08-11 11:30:52 +0000

    reports page switched to lang tr. system
    git-svn-id: svn+ssh://svn.mageia.org/svn/web/www/trunk@1607 ecb8f77e-9c09-4d67-9c5c-b96076beb801

--- en/about/reports/index.php
+++ en/about/reports/index.php
@@ -1,35 +1,34 @@
-<!DOCTYPE html>
+<?php
+define('HLANG', true);
+require '../../../langs.php';
+_lang_load($locale, 'about/reports');
+
+?><!DOCTYPE html>
 <html dir="ltr" lang="en">
 <head>
     <meta charset="utf-8">
-    <title>Mageia activity &amp; financial reports</title>
-    <meta name="description" content="Mageia reports, every year.">
-    <meta name="keywords" content="mageia, reports, activity, financial">
+    <title><?php _e('Mageia activity &amp; financial reports')?></title>
+    <meta name="description" content="<?php _e('Mageia reports, every year.')?>">
+    <meta name="keywords" content="<?php _e('mageia, reports, activity, financial')?>">
     <meta name="author" content="Mageia">
     <link rel="stylesheet" type="text/css" href="/g/style/all.css">
     <?php include '../../../analytics.php'; ?>
 </head>
 <body class="about">
-    <?php include '../../../langs.php'; ?>
-    <h1 id="mgnavt">Activity Reports</h1>
+    <?php echo $hsnav; ?>
+    <h1 id="mgnavt"><?php _e('Activity Reports')?></h1>
     <div id="doc" class="yui-t7">
         <div id="bd" role="main">
             <div class="yui-g">
                 <div class="para values">
-                    <p>Here you will find our various activity and financial reports for every year.</p>
+                    <p><?php _e('Here you will find our various activity and financial reports for every year.')?></p>
 
                     <ul>
-                        <li>2012:
-                            <a href="2012/">financial report</a> (in progress)</a></li>
+                        <li>2012: <?php _e('<a href="2012/">Financial report</a> (in progress).')?></li>
 
-                        <li>2011:
-                            Moral report: <a href="https://wiki.mageia.org/en/Rapport_moral_2011">https://wiki.mageia.org/en/Rapport_moral_2011</a>
-                            (in French &amp; English),
-                            <a href="2011/">financial report</a></li>
+                        <li>2011: <?php _e('<a href="https://wiki.mageia.org/en/Rapport_moral_2011">Moral report</a> (in French &amp; English), <a href="2011/">financial report</a>.')?></li>
 
-                        <li>2010:
-                            Moral report: ,
-                            <a href="2010/">financial report</a></li>
+                        <li>2010: <?php _e('Moral report, <a href="2010/">financial report</a>.')?></li>
                     </ul>
                 </div>
             </div>
