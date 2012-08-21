4ef9238efdd9db5e0b9f9d5f64a9a2f882bb6207
Filip Komar <filip@mageia.org>
2012-08-21 19:27:05 +0000

    constitution page converted to lang translation system
    git-svn-id: svn+ssh://svn.mageia.org/svn/web/www/trunk@1654 ecb8f77e-9c09-4d67-9c5c-b96076beb801

--- en/about/constitution/index.php
+++ en/about/constitution/index.php
@@ -3,44 +3,28 @@
 define('HLANG', true);
 require '../../../langs.php';
 
-$_t = array(
-    'en' => array(
-        'title' => 'Mageia.Org constitution',
-        'svn-source' => 'Original and definitive version (in French) is available from %s.
-            These <em>may</em> be improved. Do not hesitate to mail your patches;
-            check how in the %s.',
-    ),
-    'fr' => array(
-        'title' => 'Statuts de l&rsquo;association Mageia.Org',
-        'svn-source' => 'La version originale est disponible depuis %s.
-            Ces statuts <em>peuvent</em> être améliorés.
-            N&rsquo;hésitez pas à proposer des corrections ou améliorations
-            en suivant les consignes du %s.',
-    )
-);
-
-$_t = i18n::get_strings($_t, $locale, $i18n_fallback_rules);
+_lang_load($locale, "about/constitution");
 
 ?><!DOCTYPE html>
-<html dir="ltr" lang="en">
+<html dir="ltr" lang="<?php echo $locale ?>">
 <head>
     <meta charset="utf-8">
-    <title><?php echo $_t['title']; ?></title>
-    <meta name="description" content="<?php echo $_t['title'];?>">
-    <meta name="keywords" content="mageia.org, association, organization, legal, constitution, statuts">
+    <title><?php _e('Mageia.Org constitution'); ?></title>
+    <meta name="description" content="<?php _e('Mageia.Org constitution');?>">
+    <meta name="keywords" content="<?php _e('mageia.org, association, organization, legal, constitution, statuts'); ?>">
     <meta name="author" content="Mageia">
     <link rel="stylesheet" type="text/css" href="/g/style/all.css">
     <?php include '../../../analytics.php'; ?>
 </head>
 <body class="about constitution">
     <?php echo $hsnav; ?>
-    <h1 id="mgnavt"><?php _e('title')?></h1>
+    <h1 id="mgnavt"><?php _e('Mageia.Org constitution')?></h1>
     <div id="doc4" class="yui-t7">
         <div id="bd" role="main">
             <div class="yui-g">
                 <div class="para preambule">
                     <div id="preamble">
-                    <p><?php echo sprintf($_t['svn-source'],
+                    <p><?php echo sprintf(_t('Original and definitive version (in French) is available from %s. These <em>may</em> be improved. Do not hesitate to mail your patches; check how in the %s.'),
                         '<a href="http://svnweb.mageia.org/org/constitution/">svnweb.mageia.org/org/constitution</a>',
                         '<a href="http://svnweb.mageia.org/org/constitution/README.md?revision=HEAD&view=markup">README</a>')?></p>
                     </div>
