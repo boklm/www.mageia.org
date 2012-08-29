2ecc3f4644dcc9188c53224ca51f2536f73159a2
Romain d'Alverny <rda@mageia.org>
2012-08-29 07:51:24 +0000

    random answer (temp fix)
    git-svn-id: svn+ssh://svn.mageia.org/svn/web/www/trunk@1683 ecb8f77e-9c09-4d67-9c5c-b96076beb801

--- redir.php
+++ redir.php
@@ -5,12 +5,13 @@
 if (false !== strpos($_SERVER['QUERY_STRING'], 'Tenemos')
     || false !== strpos($_SERVER['HTTP_REFERER'], 'blogdeklx1')
 ) {
-    header('HTTP/1.1 503 Service Temporarily Unavailable');
-    header('Status: 503 Service Temporarily Unavailable');
-    header('Retry-After: 7200');
-    /*
-    header('HTTP/1.0 404 Not Found');
-    header('Status: 404 Not Found');
-    */
+    if (rand(0, 2) == 1) {
+        header('HTTP/1.1 503 Service Temporarily Unavailable');
+        header('Status: 503 Service Temporarily Unavailable');
+        header('Retry-After: 7200');
+    } else {
+        header('HTTP/1.0 404 Not Found');
+        header('Status: 404 Not Found');
+    }
     die;
 }
