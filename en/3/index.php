36dd8a4b797d2bdd0728c7fce83d38b16a6a604d
Romain d'Alverny <rda@mageia.org>
2012-10-17 10:36:22 +0000

    M3a2 download page is a bit more verbose, and made more available through the site.
    git-svn-id: svn+ssh://svn.mageia.org/svn/web/www/trunk@1749 ecb8f77e-9c09-4d67-9c5c-b96076beb801

--- en/3/index.php
+++ en/3/index.php
@@ -13,7 +13,7 @@ _lang_load($locale, '3');
     <link rel="stylesheet" href="/g/style/all.css">
     <meta name="description" content="<?php _e('Mageia 3.')?>">
     <style>
-    .para { text-align: left; float: left; width: auto; display: block; border-right: 1px solid #eef; border-bottom: 1px solid #eef; }
+    .para { text-align: left; float: left; width: 600px; display: block; border-right: 1px solid #eef; border-bottom: 1px solid #eef; }
     hr { margin-top: 2em; }
     .unstable-release-warning {
         padding: 1em 1em 1em 2.2em;
@@ -44,7 +44,24 @@ _lang_load($locale, '3');
                 _e('It is only intended for developer use. DO NOT USE THIS IN PRODUCTION OR FOR OFFICIAL REVIEW.'); ?></p>
             <div class="para">
                 <h2><?php _e('Download %s', array('Mageia 3 alpha2')); ?></h2>
-
+                <ul class="dlinfo hl">
+                    <li><?php _e('Up to 167 locales are supported:'); ?>
+                        Deutsch, English, español, français, italiano, português, svenska, nederlands,
+                        polski, dansk, Русский
+                        <?php _e('and so much more!'); ?>
+                        <a href="https://wiki.mageia.org/en/Mageia_2_supported_locales" hreflang="en"><?php _e('See the comprehensive list')?></a>.</li>
+                    <li><?php
+                        _e('Installer DVD and CD ISOs are made of Free Software exclusively.');
+                        _e('As a consequence, <span class="warn">proprietary Wi-Fi and video drivers are not included.');
+                        _e('If you need WiFi or specific video drivers <em>at install time</em>, you should go with below LiveCDs instead.</span>');
+                        _e('You may, if you will, add nonfree software repository <em>after</em> the installation.');
+                    ?></li>
+                    <li><?php _e('Use LiveCDs or LiveDVDs for fresh new installs ONLY, NOT FOR UPGRADE.')?></li>
+                    <li><?php _e('If you are looking for a stable version of Mageia, <a href="%s">go here</a>.', array('/downloads/'))?></li>
+                    <li><?php _e('Enjoy! And <a href="%s">please report bugs</a> if you find some.', array('http://bugs.mageia.org/'))?></li>
+                </ul>
+                <br>
+                <br>
                 <table class="fr-table dlt2" style="width: 580px">
                 <thead>
                     <tr>
@@ -69,19 +86,8 @@ _lang_load($locale, '3');
                         <td><a rel="nofollow" href="../downloads/get/?q=Mageia-3-alpha2-dual-CD.iso">dualarch</a></td>
                         <td><a rel="nofollow" href="../downloads/get/?q=Mageia-3-alpha2-dual-CD.iso&amp;torrent=1">dualarch</a></td>
                     </tr>
-                    <tr><td colspan="4">&nbsp;</td></tr>
-                    <tr>
-                        <th class="name"><?php _e('LiveCD KDE, English-only')?></th>
-                        <td class="size">640MB</td>
-                        <td><a rel="nofollow" href="../downloads/get/?q=Mageia-3-alpha2-LiveCD-KDE4-en-i586-CD.iso">32bit</a></td>
-                        <td><a rel="nofollow" href="../downloads/get/?q=Mageia-3-alpha2-LiveCD-KDE4-en-i586-CD.iso&amp;torrent=1">32bit</a><br>
-                    </tr>
-                    <tr>
-                        <th class="name"><?php _e('LiveCD GNOME, English-only')?></th>
-                        <td class="size">640MB</td>
-                        <td><a rel="nofollow" href="../downloads/get/?q=Mageia-3-alpha2-LiveCD-GNOME-en-i586-CD.iso">32bit</a></td>
-                        <td><a rel="nofollow" href="../downloads/get/?q=Mageia-3-alpha2-LiveCD-GNOME-en-i586-CD.iso&amp;torrent=1">32bit</a></td>
-                    </tr>
+                    <tr><td colspan="4">
+                    </td></tr>
                     <tr><td colspan="4">&nbsp;</td></tr>
                     <tr>
                         <th class="name"><?php _e('LiveDVD KDE')?></th>
@@ -98,6 +104,19 @@ _lang_load($locale, '3');
                             <a rel="nofollow" href="../downloads/get/?q=Mageia-3-alpha2-LiveDVD-GNOME-x86_64-DVD.iso&amp;torrent=1">64bit</a></td>
                         <td><a rel="nofollow" href="../downloads/get/?q=Mageia-3-alpha2-LiveDVD-GNOME-i586-DVD.iso">32bit</a><br>
                             <a rel="nofollow" href="../downloads/get/?q=Mageia-3-alpha2-LiveDVD-GNOME-x86_64-DVD.iso&amp;torrent=1">64bit</a></td>
+                    </tr>
+                    <tr><td colspan="4">&nbsp;</td></tr>
+                    <tr>
+                        <th class="name"><?php _e('LiveCD KDE, English-only')?></th>
+                        <td class="size">640MB</td>
+                        <td><a rel="nofollow" href="../downloads/get/?q=Mageia-3-alpha2-LiveCD-KDE4-en-i586-CD.iso">32bit</a></td>
+                        <td><a rel="nofollow" href="../downloads/get/?q=Mageia-3-alpha2-LiveCD-KDE4-en-i586-CD.iso&amp;torrent=1">32bit</a><br>
+                    </tr>
+                    <tr>
+                        <th class="name"><?php _e('LiveCD GNOME, English-only')?></th>
+                        <td class="size">640MB</td>
+                        <td><a rel="nofollow" href="../downloads/get/?q=Mageia-3-alpha2-LiveCD-GNOME-en-i586-CD.iso">32bit</a></td>
+                        <td><a rel="nofollow" href="../downloads/get/?q=Mageia-3-alpha2-LiveCD-GNOME-en-i586-CD.iso&amp;torrent=1">32bit</a></td>
                     </tr>
                     <tr><td colspan="3">&nbsp;</td></tr>
                     <tr>
