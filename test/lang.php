a94deaa85c859f7637958a422a67244191e6a5e2
Romain d'Alverny <rda@mageia.org>
2012-12-21 14:00:42 +0000

    add test file for browser language
    git-svn-id: svn+ssh://svn.mageia.org/svn/web/www/trunk@1911 ecb8f77e-9c09-4d67-9c5c-b96076beb801

---
+++ test/lang.php
@@ -0,0 +1,9 @@
+<pre><?php
+/**
+*/
+
+include_once '../lib/localeDetection.class.php';
+include_once '../langs.inc.php';
+
+echo 'Accept-Language: ', $_SERVER['HTTP_ACCEPT_LANGUAGE'], "\n\n";
+echo "Relocate `community` with `en` as a default: ", relocate($langs, 'community'), "\n";
