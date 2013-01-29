615f2d4411fabfe8b4ce0c44c309987ed2829450
Romain d'Alverny <rda@mageia.org>
2013-01-29 18:33:54 +0000

    open 2013 financial report
    git-svn-id: svn+ssh://svn.mageia.org/svn/web/www/trunk@1996 ecb8f77e-9c09-4d67-9c5c-b96076beb801

---
+++ en/about/reports/2013/index.php
@@ -0,0 +1,30 @@
+<?php
+/** Mageia.org 2013 fi report. */
+
+$year       = 2013;
+$source_csv = 'mageia-financial-report-2013.csv';
+$page_kw    = 'mageia, financial report, 2013, revenues, expenses';
+
+$js_options_hAxis = '{title: "2013"}';
+
+// year, total revenues, total expenses
+$js_data2_values  = <<<S
+    ['2010', 8801.66, 1277.77],
+    ['2011', 11154.02, 8327.25],
+    ['2012', 10072.08, 10766.69],
+    ['2013', 706.07, 563.87]
+S;
+
+$intro = <<<S
+    <ul>
+        <li>Get the <a href="mageia-financial-report-2012.csv">raw report data</a>
+            (CSV document, licensed under <a href="http://opendatacommons.org/licenses/odbl/1.0/">ODbL 1.0</a>).
+            The rest of this page is built from this data.</li>
+    </ul>
+S;
+
+$last_known_account_title = sprintf('Account status on December 31<sup>st</sup>, %s', $year);
+$last_known_account_title = sprintf('Account status');
+
+include '../template.php';
+
