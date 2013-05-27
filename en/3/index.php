4eb7e4bbca63ae4c1ec516233f51d6f0452c8c45
Filip Komar <filip@mageia.org>
2013-05-27 05:53:13 +0000

    small correction of html structure for dl button
    git-svn-id: svn+ssh://svn.mageia.org/svn/web/www/trunk@2486 ecb8f77e-9c09-4d67-9c5c-b96076beb801

--- en/3/index.php
+++ en/3/index.php
@@ -31,7 +31,7 @@ _lang_load($locale, '3');
             _h('What\'s new?', null, 'h2');
             _h('Too much to include here! See the <a href="https://wiki.mageia.org/en/Mageia_3_Release_Notes">release notes</a> for an extensive exposé.');
 	    ?>
-	    <a href="../downloads/" style="background: #1272B1; color: white; padding: 0.9em; margin: 1em; display: inline-block; text-shadow: 0 1px 6px #000; border-radius: 3px;"><?php _h('Download it right away!');?></a>
+	    <p><a href="../downloads/" style="background: #1272B1; color: white; padding: 0.9em; margin: 1em; display: inline-block; text-shadow: 0 1px 6px #000; border-radius: 3px;"><?php _e('Download it right away!');?></a></p>
             <?php
             _h('Mageia in context', null, 'h2');
             _h('Mageia is both a Community and a Linux Distribution, with Mageia 3 being our third release.');
