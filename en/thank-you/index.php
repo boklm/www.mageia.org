9924efc15079acbb07149c1af9a91b55bf6d3efb
Nicolas Vigier <boklm@mageia.org>
2013-06-01 22:39:17 +0000

    thank-you: add missing echo
    git-svn-id: svn+ssh://svn.mageia.org/svn/web/www/trunk@2534 ecb8f77e-9c09-4d67-9c5c-b96076beb801

--- en/thank-you/index.php
+++ en/thank-you/index.php
@@ -37,7 +37,7 @@ include '../../donators.inc.php';
             ?></p>
             <ul id="donlist" class="ty-ppl-list"></ul>
             <p>
-                <?php sprintf(_t('%s Euros has been donated to Mageia.Org in the last 30 days. <a href="%s">More details</a>.'),
+                <?php echo sprintf(_t('%s Euros has been donated to Mageia.Org in the last 30 days. <a href="%s">More details</a>.'),
                     '<span id="donations_30days"></span>', 'http://treasurer.mageia.org/donations.html');
                 ?>
             </p>
